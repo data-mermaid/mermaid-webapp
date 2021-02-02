1f332a42d1e5813c7cd1181243ddc6c1fe868565
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-02 12:56:52 -0700

    configure plop

---
+++ plopfile.js
@@ -0,0 +1,30 @@
+module.exports = (plop) => {
+  // controller generator
+  plop.setGenerator('controller', {
+    description: 'application controller logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.js',
+        templateFile: 'plop-templates/component.js.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/index.js',
+        templateFile: 'plop-templates/componentIndex.js.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.js',
+        templateFile: 'plop-templates/componentSpec.js.hbs',
+      },
+    ],
+  })
+}
