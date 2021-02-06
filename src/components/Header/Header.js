1ac6deec144f8af8c86f9a593ee7a7ffc6f4fd7d
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-06 12:25:55 -0700

    Auth: configured Auth0 (basic assumptions for now), user menu functionality, logout button, styling to remove dev blocking,

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,39 @@
+import { useAuth0 } from '@auth0/auth0-react'
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import ButtonMenu from '../generic/ButtonMenu'
+import { RowSpaceBetween, RowRight } from '../generic/positioning'
 
 /**
- * Describe your component
+ * Mermaid Header
  */
+
+const Nav = styled.nav`
+  & > * {
+    margin-right: ${(props) => props.theme.spacing.small};
+  }
+`
+
 const Header = () => {
+  const { logout, user } = useAuth0()
+
   return (
-    <>
-      Header{' '}
-      <nav>
-        <Link to="/projects">Projects</Link>
-      </nav>
-    </>
+    <RowSpaceBetween>
+      Header
+      <RowRight>
+        <Nav>
+          <Link to="/projects">Projects</Link>
+          <Link to="/#">Reports</Link>
+          <Link to="/#">Reference</Link>
+          <Link to="/#">Global Dashboard</Link>
+        </Nav>
+        <ButtonMenu
+          label={user.name}
+          items={[{ label: 'Logout', onClick: logout }]}
+        />
+      </RowRight>
+    </RowSpaceBetween>
   )
 }
 
