a3efffb75c3b27b22e0a038de65b7dc382e2deef
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-07 11:25:52 -0700

    extend testing library with renderAuthenticated and renderUnauthenticated functions.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
-// import React from 'react'
-// import { render, screen } from '@testing-library/react'
-// import App from './App'
+import React from 'react'
+import {
+  renderAuthenticated,
+  renderUnauthenticated,
+  screen,
+} from './testUtilities/testingLibraryWithHelpers'
+import App from './App'
 
-test('renders learn react link', () => {
-  //   render(<App />)
-  //   const linkElement = screen.getByText(/learn react/i)
-  //   expect(linkElement).toBeInTheDocument()
+test('App renders the initial screen as expected for a authenticated user', () => {
+  renderAuthenticated(<App />)
+
+  // this assertion is for demo purposes only and will be deleted as the landing pages are built
+  expect(screen.getByText('All projects page placeholder'))
+})
+
+test('App renders the initial screen as expected for a not authenticated user', () => {
+  renderUnauthenticated(<App />)
+
+  // this assertion is for demo purposes only and will be deleted as the landing pages are built
+  expect(screen.queryByText('All projects page placeholder')).toBeNull()
 })
---
+++ src/testUtilities/testingLibraryWithHelpers.js
@@ -0,0 +1,43 @@
+import { Auth0Context } from '@auth0/auth0-react'
+import { render } from '@testing-library/react'
+import { ThemeProvider } from 'styled-components/macro'
+import PropTypes from 'prop-types'
+import React from 'react'
+import theme from '../theme'
+
+const AuthenticatedProviders = ({ children }) => (
+  <Auth0Context.Provider
+    value={{ isAuthenticated: true, user: { name: 'Fake User' } }}
+  >
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </Auth0Context.Provider>
+)
+
+const UnauthenticatedProviders = ({ children }) => (
+  <Auth0Context.Provider
+    value={{ isAuthenticated: false, loginWithRedirect: () => {} }}
+  >
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </Auth0Context.Provider>
+)
+
+AuthenticatedProviders.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+UnauthenticatedProviders.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+const renderAuthenticated = (ui, options) =>
+  render(ui, { wrapper: AuthenticatedProviders, ...options })
+const renderUnauthenticated = (ui, options) =>
+  render(ui, { wrapper: UnauthenticatedProviders, ...options })
+
+const renderOverride = () => {
+  throw new Error(
+    'Please use renderAuthenticated or renderUnauthenticated instead of render.',
+  )
+}
+
+export * from '@testing-library/react'
+export { renderOverride as render, renderAuthenticated, renderUnauthenticated }
