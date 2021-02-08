9180d0b0a1f5b536effb0b84c87fe6a86ea8c9d6
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-08 11:40:38 -0700

    Mock App story for storybook, + storybook config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,15 @@
+import { MemoryRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import theme from '../src/theme'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter>
+      <Story />
+    </MemoryRouter>
+  ),
+]
--- src/components/generic/ButtonMenu/ButtonMenu.stories.js
+++ src/components/generic/ButtonMenu/ButtonMenu.stories.js
@@ -1,6 +1,6 @@
 import { action } from '@storybook/addon-actions'
 import React from 'react'
-import styled from 'styled-components'
+import styled from 'styled-components/macro'
 
 import ButtonMenu from '.'
 
