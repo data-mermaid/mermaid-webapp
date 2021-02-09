33d4e9a6c37326f87a65b5266a8760317543e9d3
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-09 13:37:04 -0700

    Unstyled buttons for reuse

---
+++ src/components/generic/buttons.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const ButtonPrimary = styled.button`
+  background-color: blue;
+`
+
+export const ButtonSecondary = styled.button`
+  background-color: lightgray;
+`
+export const ButtonCallout = styled.button`
+  background-color: green;
+`
+export const ButtonCaution = styled.button`
+  background-color: red;
+`
