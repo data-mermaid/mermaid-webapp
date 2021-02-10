6500dfe6e5e0ee2d7e052527e441679e82d407a1
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-10 10:57:05 -0700

    M1446 baking in project links (overview, collect, admin, data) to SubLayout2 component + routing

--- src/components/SubLayout2/SubLayout2.stories.js
+++ src/components/SubLayout2/SubLayout2.stories.js
@@ -10,7 +10,6 @@ export const basic = () => (
   <SubLayout2
     lowerLeft={<>Lower Left</>}
     lowerRight={<>Lower Right</>}
-    upperLeft={<>Upper Left</>}
     upperRight={<>Upper Right</>}
   />
 )
---
+++ src/components/pages/Admin/Admin.test.js
@@ -0,0 +1,10 @@
+import '@testing-library/jest-dom/extend-expect'
+// import React from 'react'
+// import { renderAuthenticated } from '../../../testUtilities/testingLibraryWithHelpers'
+
+// import Admin from './Admin'
+
+test('Admin component renders with the expected UI elements', () => {
+  // const utilities = renderAuthenticated(<Admin />)
+  // expect(utilities.getByText('I should fail'))
+})
