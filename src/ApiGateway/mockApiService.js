d2d47d304b6906ca098b5272df22fb498a4ce3cb
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-11 15:32:56 -0700

    rearchitect routes to append newly created api gateway service where appropriate.

---
+++ src/ApiGateway/mockApiService.js
@@ -0,0 +1,18 @@
+export default {
+  projects: [
+    {
+      name: 'Karimunjawa National Park',
+      country: 'Fiji',
+      numberOfSites: 23,
+      offlineReady: true,
+      lastUpdatedDate: '01/21/2020',
+    },
+    {
+      name: 'Gita Nada_MPA 2018',
+      country: 'Fiji',
+      numberOfSites: 1,
+      offlineReady: false,
+      lastUpdatedDate: '11/21/2021',
+    },
+  ],
+}
