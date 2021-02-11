846809ef6236d76fb6e6c52fe39d6ef8aa1a7411
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-11 15:33:44 -0700

    Refactor projects page to be more top level

--- src/components/pages/Projects/Projects.stories.js
+++ src/components/pages/Projects/Projects.stories.js
@@ -1,51 +1,11 @@
 import React from 'react'
 
 import Projects from '.'
-import ProjectCard from '../../ProjectCard'
-import ProjectToolBarSection from '../../ProjectToolBarSection'
+import mockApiService from '../../../ApiGateway/mockApiService'
 
 export default {
   title: 'Projects',
   component: Projects,
 }
 
-export const basic = () => (
-  <Projects
-    topRow={<>top row stuff goes here</>}
-    bottomRow={<>All projects page placeholder</>}
-  />
-)
-
-export const populatedMultipleProjects = () => {
-  const projectSamples = [
-    {
-      name: 'Karimunjawa National Park',
-      country: 'Fiji',
-      numberOfSites: 23,
-      offlineReady: true,
-      lastUpdatedDate: '01/21/2020',
-    },
-    {
-      name: 'Gita Nada_MPA 2018',
-      country: 'Fiji',
-      numberOfSites: 1,
-      offlineReady: false,
-      lastUpdatedDate: '11/21/2021',
-    },
-  ]
-
-  const projectList = projectSamples.map(
-    ({ name, country, numberOfSites, offlineReady, lastUpdatedDate }) => (
-      <ProjectCard
-        key={name}
-        name={name}
-        country={country}
-        numberOfSites={numberOfSites}
-        offlineReady={offlineReady}
-        lastUpdatedDate={lastUpdatedDate}
-      />
-    ),
-  )
-
-  return <Projects topRow={<ProjectToolBarSection />} bottomRow={projectList} />
-}
+export const basic = () => <Projects apiService={mockApiService} />
