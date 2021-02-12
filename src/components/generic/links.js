6e0c7d8b443d3f699eea094a054bca9d2f8b7b5b
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-12 12:58:22 -0700

    M1448 refactor custom NavLink names, put in links.js file instead of buttons (they arent buttons)

---
+++ src/components/generic/links.js
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+
+export const NavLinkButtonish = styled(NavLink)`
+  background-color: lightgray;
+  border: solid thin grey;
+`
+export const NavLinkButtonishIcon = styled(NavLinkButtonish)``
+
+export const NavLinkSidebar = styled(NavLink)``
