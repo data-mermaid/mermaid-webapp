1daf09d64921a5fd8c534a5c0733ddc8bfc48aae
Nick Hoang <nickhoang11@gmail.com>
2021-02-16 17:02:56 -0800

    [M1466] Add react switch to simulate offline toggle button. Add sample test.

---
+++ src/components/OfflineToggle/OfflineToggle.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import Switch from 'react-switch'
+
+/**
+ * Describe your component
+ */
+
+const OfflineToggle = () => {
+  const [checked, setChecked] = useState(false)
+
+  const handleChange = (checked) => setChecked(checked)
+
+  return (
+    <Switch
+      id="offline-toggle-switch"
+      aria-label="offline-toggle-switch"
+      onChange={handleChange}
+      checked={checked}
+      onColor="#CC0A00"
+      height={16}
+      width={32}
+      boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+      activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+      uncheckedIcon={false}
+    />
+  )
+}
+
+OfflineToggle.propTypes = {}
+
+export default OfflineToggle
---
+++ src/components/OfflineToggle/OfflineToggle.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import OfflineToggle from '.'
+
+export default {
+  title: 'OfflineToggle',
+  component: OfflineToggle,
+}
+export const basic = () => <OfflineToggle />
