8a8e9a0d9b25a47cf9cc97b7fb2971fb94500f26
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-22 09:03:01 -0700

    M1469 useOnlineStatus hook

---
+++ src/library/useOnlineStatus.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+
+const useOnlineStatus = () => {
+  const [isOnline, setIsOnline] = useState(false)
+  const handleOnline = () => {
+    setIsOnline(true)
+  }
+  const handleOffline = () => {
+    setIsOnline(false)
+  }
+  const cleanup = () => {
+    window.removeEventListener('offline', handleOffline)
+    window.removeEventListener('online', handleOnline)
+  }
+
+  useEffect(() => {
+    setIsOnline(navigator.onLine)
+    window.addEventListener('offline', handleOffline)
+    window.addEventListener('online', handleOnline)
+
+    return cleanup
+  }, [])
+
+  return isOnline
+}
+
+export default useOnlineStatus
