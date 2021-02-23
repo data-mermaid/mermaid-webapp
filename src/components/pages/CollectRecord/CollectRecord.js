8102554c930b044a2054c853bd6404702a8ce95f
Nick Hoang <nickhoang11@gmail.com>
2021-02-22 17:40:28 -0800

    Add input form to Collect Record page.

--- src/components/pages/CollectRecord/CollectRecord.js
+++ src/components/pages/CollectRecord/CollectRecord.js
@@ -2,20 +2,41 @@ import React from 'react'
 // import PropTypes from 'prop-types'
 import SubLayout2 from '../../SubLayout2'
 import CollectingNav from '../../CollectingNav'
+import SelectInputForm from '../../generic/SelectInputForm'
+import { mermaidApiServicePropType } from '../../../ApiServices/useMermaidApi'
 
 /**
  * Describe your component
  */
-const CollectRecord = () => {
+
+// this will be a separate component soon...
+const CollectBody = ({ apiService }) => {
+  const { sites, management_regimes } = apiService
+
+  return (
+    <>
+      <SelectInputForm key="sites" label={'Site'} options={sites} />
+      <SelectInputForm
+        key="management_regimes"
+        label={'Management Regime'}
+        options={management_regimes}
+      />
+    </>
+  )
+}
+
+const CollectRecord = ({ apiService }) => {
   return (
     <SubLayout2
       lowerLeft={<CollectingNav />}
-      lowerRight={<>Collect Record</>}
+      lowerRight={<CollectBody apiService={apiService} />}
       upperRight={<>Sub layout top bar</>}
     />
   )
 }
 
-// CollectRecord.propTypes = {}
+CollectRecord.propTypes = {
+  apiService: mermaidApiServicePropType.isRequired,
+}
 
 export default CollectRecord
