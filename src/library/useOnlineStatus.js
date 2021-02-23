b18bed4106a06f6fc441509d4113397fe23a4fbd
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-22 17:16:15 -0700

    M1476 enable new linter rules (exaustive deps, rules of hooks) and clean up code as prompted

--- src/library/useOnlineStatus.js
+++ src/library/useOnlineStatus.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from 'react'
 const useOnlineStatus = () => {
   const [isOnline, setIsOnline] = useState(false)
 
-  const handleOnline = () => {
-    setIsOnline(true)
-  }
-  const handleOffline = () => {
-    setIsOnline(false)
-  }
-  const cleanup = () => {
-    window.removeEventListener('offline', handleOffline)
-    window.removeEventListener('online', handleOnline)
-  }
-
   useEffect(() => {
+    const handleOnline = () => {
+      setIsOnline(true)
+    }
+    const handleOffline = () => {
+      setIsOnline(false)
+    }
+    const cleanup = () => {
+      window.removeEventListener('offline', handleOffline)
+      window.removeEventListener('online', handleOnline)
+    }
+
     setIsOnline(navigator.onLine)
     window.addEventListener('offline', handleOffline)
     window.addEventListener('online', handleOnline)
