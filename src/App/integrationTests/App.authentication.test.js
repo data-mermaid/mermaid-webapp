eb85544533d405ad575a1c465a274044e92095f1
Melissa Nunes <mnunes@sparkgeo.com>
2021-02-23 16:54:34 -0700

    M1476 add tests. Config for ensuring test isolation.

--- src/App/integrationTests/App.authentication.test.js
+++ src/App/integrationTests/App.authentication.test.js
@@ -1,20 +1,44 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
 import {
-  renderAuthenticated,
-  renderUnauthenticated,
+  fireEvent,
+  renderAuthenticatedOffline,
+  renderAuthenticatedOnline,
+  renderUnauthenticatedOffline,
+  renderUnauthenticatedOnline,
   screen,
 } from '../../testUtilities/testingLibraryWithHelpers'
 import App from '../App'
 
-test('App renders the initial screen as expected for a authenticated user', () => {
-  renderAuthenticated(<App />)
+test('App renders the initial screen as expected for an online and authenticated user', () => {
+  renderAuthenticatedOnline(<App />)
 
   expect(screen.getByRole('heading')).toHaveTextContent('Projects')
+
+  fireEvent.click(screen.getByText('Fake User'))
+
+  // there is a logout button
+  expect(screen.getByText('Logout'))
+})
+test('App renders the initial screen as expected for an offline user who is authenticated when online', () => {
+  renderAuthenticatedOffline(<App />)
+
+  expect(screen.getByRole('heading')).toHaveTextContent('Projects')
+
+  fireEvent.click(screen.getByText('Fake User'))
+
+  // there is not a logout button
+  expect(screen.queryByText('Logout')).toBeNull()
 })
 
-test('App renders the initial screen as expected for a not authenticated user', () => {
-  renderUnauthenticated(<App />)
+test('App renders the initial screen as expected for an online but not authenticated user', () => {
+  renderUnauthenticatedOnline(<App />)
+
+  expect(screen.queryByText('Projects')).toBeNull()
+})
+
+test('App renders the initial screen as expected for an offline user who is not authenticated in an online environment', () => {
+  renderUnauthenticatedOffline(<App />)
 
   expect(screen.queryByText('Projects')).toBeNull()
 })
