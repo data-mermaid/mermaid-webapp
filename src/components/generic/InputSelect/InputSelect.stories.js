f3c5e5074afb3414cab3cad8e5ff3b7cc633b47d
Nick Hoang <nickhoang11@gmail.com>
2021-02-23 16:15:53 -0800

    Add stories.

--- src/components/generic/InputSelect/InputSelect.stories.js
+++ src/components/generic/InputSelect/InputSelect.stories.js
@@ -6,4 +6,30 @@ export default {
   title: 'InputSelect',
   component: InputSelect,
 }
-export const basic = () => <InputSelect />
+export const basic = () => {
+  const sampleSiteOptions = [
+    { name: 'Phone' },
+    { name: 'Table' },
+    { name: 'Desk' },
+    { name: 'Computer' },
+  ]
+
+  return <InputSelect label="Input Type Select" options={sampleSiteOptions} />
+}
+export const selectedValueInInputField = () => {
+  const sampleSiteOptions = [
+    { name: 'Dog' },
+    { name: 'Cat' },
+    { name: 'Mouse' },
+    { name: 'Lion' },
+  ]
+  const sampleSelectedValue = 'Lion'
+
+  return (
+    <InputSelect
+      label="Input Type Select"
+      options={sampleSiteOptions}
+      value={sampleSelectedValue}
+    />
+  )
+}
