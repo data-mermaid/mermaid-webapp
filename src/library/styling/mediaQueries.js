23173c79c5ec9f6ec627c4e024e159045a8655f7
AL Leonard <aleonard@sparkgeo.com>
2021-02-26 15:35:13 -0500

    added responsive media queries

--- src/library/styling/mediaQueries.js
+++ src/library/styling/mediaQueries.js
@@ -5,7 +5,27 @@ const mediaQueryPhoneOnly = (content) => css`
     ${content};
   }
 `
-const mediaQueryHover = (content) => css`
+const mediaQueryForTabletPortraitUp = (content) => css`
+  @media (min-width: 600px) {
+    ${content};
+  }
+`
+const mediaQueryForTabletLandscapeUp = (content) => css`
+  @media (min-width: 900px) {
+    ${content};
+  }
+`
+const mediaQueryForDesktopUp = (content) => css`
+  @media (min-width: 1200px) {
+    ${content};
+  }
+`
+const mediaQueryForBigDesktopUp = (content) => css`
+  @media (min-width: 1800px) {
+    ${content};
+  }
+`
+const hoverState = (content) => css`
   @media (hover: hover) {
     &:hover {
       ${content};
@@ -13,4 +33,11 @@ const mediaQueryHover = (content) => css`
   }
 `
 
-export { mediaQueryPhoneOnly, mediaQueryHover }
+export {
+  mediaQueryPhoneOnly,
+  mediaQueryForBigDesktopUp,
+  mediaQueryForTabletLandscapeUp,
+  mediaQueryForTabletPortraitUp,
+  mediaQueryForDesktopUp,
+  hoverState,
+}
