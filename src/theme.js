0dc79eb3f01a4e78cbfc2bc19c6ee25dd83b23fd
AL Leonard <aleonard@sparkgeo.com>
2021-02-26 11:06:53 -0500

    switch to use the import syntax for the color

--- src/theme.js
+++ src/theme.js
@@ -1,26 +1,26 @@
-const Color = require('color')
+import colorHelper from 'color'
 
-const primary = Color('#1C4C73')
-const secondary = Color('#E6E6E6')
-const callout = Color('#52B434')
-const white = primary.mix(Color('white'), 0.95)
-const black = primary.mix(Color('black'), 0.95)
+const primary = colorHelper('#1C4C73')
+const secondary = colorHelper('#E6E6E6')
+const callout = colorHelper('#52B434')
+const white = primary.mix(colorHelper('white'), 0.95)
+const black = primary.mix(colorHelper('black'), 0.95)
 
 const color = {
   white,
   black,
   primaryColor: primary,
-  primaryHover: primary.mix(Color(white), 0.1),
+  primaryHover: primary.mix(colorHelper(white), 0.1),
   primaryText: white,
-  primaryBorder: primary.mix(Color(black), 0.2),
+  primaryBorder: primary.mix(colorHelper(black), 0.2),
   secondaryColor: secondary,
-  secondaryHover: secondary.mix(Color('white'), 0.2),
+  secondaryHover: secondary.mix(colorHelper('white'), 0.2),
   secondaryText: black,
-  secondaryBorder: secondary.mix(Color(black), 0.2),
+  secondaryBorder: secondary.mix(colorHelper(black), 0.2),
   calloutColor: callout,
-  calloutHover: callout.mix(Color(white), 0.1),
+  calloutHover: callout.mix(colorHelper(white), 0.1),
   calloutText: white,
-  calloutBorder: callout.mix(Color(black), 0.2),
+  calloutBorder: callout.mix(colorHelper(black), 0.2),
   disabledColor: '#D5D5DD',
   disabledText: '#969696',
   disabledBorder: '#969696',
