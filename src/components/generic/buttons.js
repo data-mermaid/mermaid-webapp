163f6bd85df975ed44013bc5e305570460d7fcbe
AL Leonard <aleonard@sparkgeo.com>
2021-03-01 14:20:19 -0500

    fixed commit errors

--- src/components/generic/buttons.js
+++ src/components/generic/buttons.js
@@ -61,8 +61,8 @@ export const ButtonCaution = styled(Button)`
     ${buttonActive};
   }
 `
-// export const ButtonyNavLink = styled(NavLink)`
-//   background-color: lightgray;
-//   border: solid thin grey;
-// `
-// export const ButtonyNavLinkIcon = styled(ButtonyNavLink)``
+export const ButtonyNavLink = styled(NavLink)`
+  background-color: lightgray;
+  border: solid thin grey;
+`
+export const ButtonyNavLinkIcon = styled(ButtonyNavLink)``
