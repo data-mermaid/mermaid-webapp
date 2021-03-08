36847ede55c725eed893cf310ea32be1237ab44c
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-08 11:03:49 -0700

    M1480 make sure each test gets a new mock indexed db instance (for test isolation)

--- src/App/integrationTests/App.authentication.test.js
+++ src/App/integrationTests/App.authentication.test.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
-import mockMermaidDbAccessInstance from '../../testUtilities/mockMermaidDbAccessInstance'
+import { getMockMermaidDbAccessInstance } from '../../testUtilities/mockMermaidDbAccess'
+
 import {
   fireEvent,
   mockMermaidApiAllSuccessful,
@@ -19,7 +20,7 @@ afterAll(() => mockMermaidApiAllSuccessful.close())
 
 test('App renders the initial screen as expected for an online and authenticated user', async () => {
   renderAuthenticatedOnline(
-    <App mermaidDbAccessInstance={mockMermaidDbAccessInstance} />,
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
   )
 
   expect(await screen.findByText('Projects', { selector: 'h1' }))
@@ -32,7 +33,7 @@ test('App renders the initial screen as expected for an online and authenticated
 
 test('App: an online and authenticated user can logout', async () => {
   renderAuthenticatedOnline(
-    <App mermaidDbAccessInstance={mockMermaidDbAccessInstance} />,
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
   )
 
   fireEvent.click(await screen.findByText('FakeFirstNameOnline'))
@@ -42,12 +43,12 @@ test('App: an online and authenticated user can logout', async () => {
 
 test('App renders the initial screen as expected for an offline user who is authenticated when online', async () => {
   renderAuthenticatedOffline(
-    <App mermaidDbAccessInstance={mockMermaidDbAccessInstance} />,
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
   )
 
   expect(await screen.findByText('Projects', { selector: 'h1' }))
 
-  fireEvent.click(screen.getByText('FakeFirstNameOnline'))
+  fireEvent.click(screen.getByText('FakeFirstNameOffline'))
 
   // there is not a logout button
   expect(await waitFor(() => screen.queryByText('Logout'))).toBeNull()
@@ -55,7 +56,7 @@ test('App renders the initial screen as expected for an offline user who is auth
 
 test('App renders the initial screen as expected for an online but not authenticated user', () => {
   renderUnauthenticatedOnline(
-    <App mermaidDbAccessInstance={mockMermaidDbAccessInstance} />,
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
   )
 
   expect(screen.queryByText('Projects')).toBeNull()
@@ -63,7 +64,7 @@ test('App renders the initial screen as expected for an online but not authentic
 
 test('App renders the initial screen as expected for an offline user who is not authenticated in an online environment', () => {
   renderUnauthenticatedOffline(
-    <App mermaidDbAccessInstance={mockMermaidDbAccessInstance} />,
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
   )
 
   expect(screen.queryByText('Projects')).toBeNull()
