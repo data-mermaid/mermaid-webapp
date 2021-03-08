307f4bf4c527adc0d5d18bab22b2ffafaca2053f
Nick Hoang <nickhoang11@gmail.com>
2021-03-08 12:11:51 -0800

    Set up forms component for collect record page.

---
+++ src/components/CollectForms/CollectForms.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import CollectForms from '.'
+
+export default {
+  title: 'CollectForms',
+  component: CollectForms,
+}
+export const basic = () => <CollectForms />
--- src/components/pages/CollectRecord/CollectRecord.js
+++ src/components/pages/CollectRecord/CollectRecord.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react'
-import PropTypes from 'prop-types'
 import { useParams } from 'react-router-dom'
 import SubLayout2 from '../../SubLayout2'
 import NavMenu from '../../NavMenu'
-import InputSelect from '../../generic/InputSelect'
-import InputNumber from '../../generic/InputNumber'
+import CollectForms from '../../CollectForms'
 import { ButtonCallout } from '../../generic/buttons'
 import { RowRight } from '../../generic/positioning'
 import { mermaidApiServicePropType } from '../../../ApiServices/useMermaidApi'
@@ -12,41 +10,6 @@ import { mermaidApiServicePropType } from '../../../ApiServices/useMermaidApi'
 /**
  * Describe your component
  */
-
-// this will be a separate component soon...
-const CollectBody = ({
-  collectRecord,
-  sites,
-  managementRegimes,
-  handleInputChange,
-}) => {
-  const { site, management, depth } = collectRecord
-
-  return (
-    <>
-      <InputSelect
-        name="site"
-        label="Site"
-        options={sites}
-        value={site}
-        onChange={handleInputChange}
-      />
-      <InputSelect
-        name="management"
-        label="Management Regime"
-        value={management}
-        options={managementRegimes}
-        onChange={handleInputChange}
-      />
-      <InputNumber
-        name="depth"
-        label="Depth"
-        value={depth}
-        onChange={handleInputChange}
-      />
-    </>
-  )
-}
 
 const CollectRecord = ({ apiService }) => {
   const { recordId } = useParams()
@@ -73,7 +36,7 @@ const CollectRecord = ({ apiService }) => {
     <SubLayout2
       sidebar={<NavMenu />}
       lowerRight={
-        <CollectBody
+        <CollectForms
           collectRecord={curCollectRecord}
           sites={sites}
           managementRegimes={managementRegimes}
@@ -87,25 +50,6 @@ const CollectRecord = ({ apiService }) => {
       }
     />
   )
-}
-
-CollectBody.propTypes = {
-  collectRecord: PropTypes.shape({
-    site: PropTypes.string,
-    management: PropTypes.string,
-    depth: PropTypes.number,
-  }).isRequired,
-  sites: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-    }),
-  ).isRequired,
-  managementRegimes: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-    }),
-  ).isRequired,
-  handleInputChange: PropTypes.func.isRequired,
 }
 
 CollectRecord.propTypes = {
