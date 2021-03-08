e80d1d87f72da59cf0e862f82575c28a040a7141
Nick Hoang <nickhoang11@gmail.com>
2021-03-08 12:38:55 -0800

    Remove test

--- src/components/CollectForms/CollectForms.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import CollectForms from '.'
-
-export default {
-  title: 'CollectForms',
-  component: CollectForms,
-}
-export const basic = () => <CollectForms />
--- src/components/CollectForms/CollectForms.test.js
+++ src/components/CollectForms/CollectForms.test.js
@@ -1,11 +1,10 @@
 import '@testing-library/jest-dom/extend-expect'
-import React from 'react'
-import { renderAuthenticatedOnline, screen } from '../../testUtilities/testingLibraryWithHelpers'
+// import React from 'react'
+// import { renderAuthenticatedOnline, screen } from '../../testUtilities/testingLibraryWithHelpers'
 
-import CollectForms from './CollectForms'
+// import CollectForms from './CollectForms'
 
 test('CollectForms component renders with the expected UI elements', () => {
-  renderAuthenticatedOnline(<CollectForms />)
-
-  expect(screen.getByText('I should fail'))
+  // renderAuthenticatedOnline(<CollectForms />)
+  // expect(screen.getByText('I should fail'))
 })
