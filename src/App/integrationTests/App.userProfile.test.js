caea5ff5b0640c4e0183b75946dcd4df342ca2c9
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-08 11:15:20 -0700

    M1480 explicit online/offline tests for user profile

---
+++ src/App/integrationTests/App.userProfile.test.js
@@ -0,0 +1,52 @@
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+import { getMockMermaidDbAccessInstance } from '../../testUtilities/mockMermaidDbAccess'
+import {
+  mockMermaidApiAllSuccessful,
+  renderAuthenticatedOffline,
+  renderAuthenticatedOnline,
+  screen,
+  waitFor,
+} from '../../testUtilities/testingLibraryWithHelpers'
+import App from '../App'
+
+beforeAll(() => {
+  mockMermaidApiAllSuccessful.listen()
+})
+afterEach(() => {
+  mockMermaidApiAllSuccessful.resetHandlers()
+})
+afterAll(() => {
+  mockMermaidApiAllSuccessful.close()
+})
+
+test('App renders shows the users name from the API for an online and authenticated user', async () => {
+  renderAuthenticatedOnline(
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
+  )
+
+  // wait for page to load in lieu of being able to test for a loading indicator to have vanished
+  expect(await screen.findByText('Projects', { selector: 'h1' }))
+
+  await waitFor(() =>
+    expect(screen.queryByText('FakeFirstNameOffline')).toBeNull(),
+  )
+  expect(await screen.findByText('FakeFirstNameOnline'))
+})
+
+test('App renders shows the users name from offline storage for an offline user who is authenticated when online', async () => {
+  const thing = renderAuthenticatedOffline(
+    <App mermaidDbAccessInstance={getMockMermaidDbAccessInstance()} />,
+  )
+
+  // wait for page to load in lieu of being able to test for a loading indicator to have vanished
+  expect(
+    await thing.findByText('Projects', {
+      selector: 'h1',
+    }),
+  )
+  await waitFor(() =>
+    expect(thing.queryByText('FakeFirstNameOnline')).toBeNull(),
+  )
+  expect(await thing.findByText('FakeFirstNameOffline'))
+})
