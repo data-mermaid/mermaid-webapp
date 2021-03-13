c168ca6a1b6635478e952d08579c4cecd15f6f5e
Nick Hoang <nickhoang11@gmail.com>
2021-03-12 16:18:07 -0800

    Remove old input components.

--- src/components/generic/InputForm/InputForm.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import InputForm from '.'
-
-export default {
-  title: 'InputForm',
-  component: InputForm,
-}
-export const basic = () => <InputForm />
--- src/components/generic/InputSelect/InputSelect.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import InputSelect from '.'
-
-export default {
-  title: 'InputSelect',
-  component: InputSelect,
-}
-export const basic = () => <InputSelect />
