19a41090d5efd6e5fa0a53d404e0d4f1c0b14ff0
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-14 12:27:40 -0600

    M1494 prep:  benthicLit review collect record page sample info form and basic error validation

--- src/components/SampleInfoForms/SampleInfoForms.js
+++ src/components/SampleInfoForms/SampleInfoForms.js
@@ -1,70 +1,72 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Formik, Form } from 'formik'
-import * as Yup from 'yup'
-import MermaidInput from '../generic/MermaidInput'
-import MermaidSelect from '../generic/MermaidSelect'
+import InputWithLabelAndValidation from '../generic/InputWithLabelAndValidation'
+import InputSelectWithLabelAndValidation from '../generic/InputSelectWithLabelAndValidation'
 import { H2 } from '../generic/text'
+import {
+  managementRegimePropType,
+  sitePropType,
+} from '../../library/mermaidData/useMermaidData'
+import { formikPropType } from '../../library/formikHelpers/formikPropType'
+import getValidationPropsFromFormik from '../../library/formikHelpers/getValidationPropsFromFormik'
 
-const SampleInfoForms = ({ collectRecord, sites, managementRegimes }) => {
-  const { site, management, depth } = collectRecord
-  const siteNameOptions = sites.map((item) => item.name)
-  const managementNameOptions = managementRegimes.map((item) => item.name)
+const SampleInfoForms = ({ formik, sites, managementRegimes }) => {
+  const siteSelectOptions = sites.map((site) => ({
+    label: site.name,
+    value: site.id,
+  }))
+
+  const managementSelectOptions = managementRegimes.map((regime) => ({
+    label: regime.name,
+    value: regime.id,
+  }))
 
   return (
     <>
       <H2>Sample Info</H2>
-      <Formik
-        initialValues={{
-          depth,
-          site,
-          management,
-          sampleDate: '',
-          sampletime: '',
-        }}
-        validationSchema={Yup.object({
-          site: Yup.string()
-            .oneOf(siteNameOptions, 'Invalid site')
-            .required('Site is required'),
-          management: Yup.string()
-            .oneOf(managementNameOptions, 'Invalid management regime')
-            .required('Management Regime is required'),
-          depth: Yup.number().required('Depth is required'),
-        })}
-        // onSubmit={(values) => {}}
-      >
-        <Form>
-          <MermaidSelect label="Site" name="site" options={sites} />
-          <MermaidSelect
-            label="Management"
-            name="management"
-            options={managementRegimes}
-          />
-          <MermaidInput label="Depth" name="depth" type="number" />
-          <MermaidInput label="Sample Date" name="sampleDate" type="date" />
-          <MermaidInput label="Sample Time" name="sampletime" type="time" />
-        </Form>
-      </Formik>
+      <InputSelectWithLabelAndValidation
+        label="Site"
+        id="site"
+        options={siteSelectOptions}
+        {...formik.getFieldProps('site')}
+        {...getValidationPropsFromFormik(formik, 'site')}
+      />
+      <InputSelectWithLabelAndValidation
+        label="Management"
+        id="management"
+        options={managementSelectOptions}
+        {...formik.getFieldProps('management')}
+        {...getValidationPropsFromFormik(formik, 'management')}
+      />
+      <InputWithLabelAndValidation
+        label="Depth"
+        id="depth"
+        type="number"
+        {...formik.getFieldProps('depth')}
+        {...getValidationPropsFromFormik(formik, 'depth')}
+      />
+      <InputWithLabelAndValidation
+        label="Sample Date"
+        id="sampleDate"
+        type="date"
+        {...formik.getFieldProps('sampleDate')}
+        {...getValidationPropsFromFormik(formik, 'sampleDate')}
+      />
+      <InputWithLabelAndValidation
+        label="Sample Time"
+        id="sampleTime"
+        type="time"
+        {...formik.getFieldProps('sampleTime')}
+        {...getValidationPropsFromFormik(formik, 'sampleTime')}
+      />
     </>
   )
 }
 
 SampleInfoForms.propTypes = {
-  collectRecord: PropTypes.shape({
-    site: PropTypes.string,
-    management: PropTypes.string,
-    depth: PropTypes.number,
-  }).isRequired,
-  sites: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-    }),
-  ).isRequired,
-  managementRegimes: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-    }),
-  ).isRequired,
+  sites: PropTypes.arrayOf(sitePropType).isRequired,
+  managementRegimes: PropTypes.arrayOf(managementRegimePropType).isRequired,
+  formik: formikPropType.isRequired,
 }
 
 export default SampleInfoForms
--- src/components/pages/CollectRecord/CollectRecord.js
+++ src/components/pages/CollectRecord/CollectRecord.js
@@ -1,50 +1,83 @@
-import React, { useState } from 'react'
+import { Formik, useFormik } from 'formik'
 import { useParams } from 'react-router-dom'
+import * as Yup from 'yup'
+import React, { useState } from 'react'
+
 import SubLayout2 from '../../SubLayout2'
 import NavMenu from '../../NavMenu'
-import CollectForms from '../../CollectForms'
 import { ButtonCallout } from '../../generic/buttons'
 import { RowRight } from '../../generic/positioning'
 import { mermaidDataPropType } from '../../../library/mermaidData/useMermaidData'
+import SampleInfoForms from '../../SampleInfoForms'
 
 const CollectRecord = ({ mermaidData }) => {
   const { recordId } = useParams()
   const { collectRecords, sites, managementRegimes } = mermaidData
 
-  const filterRecord = (record) =>
+  const initializeCollectRecordBeingEdited = (record) =>
     collectRecords.filter(({ id }) => id === record)[0]
 
-  const [curCollectRecord, setCurCollectRecord] = useState(
-    filterRecord(recordId),
+  const [collectRecordBeingEdited] = useState(
+    initializeCollectRecordBeingEdited(recordId),
   )
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target
+  const validSiteValues = sites.map((site) => site.id)
 
-    const newValue = name === 'depth' ? parseInt(value, 10) : value
+  const validManagementRegimeValues = managementRegimes.map(
+    (regime) => regime.id,
+  )
 
-    setCurCollectRecord({ ...curCollectRecord, [name]: newValue })
+  const formikOptions = {
+    initialValues: {
+      depth: collectRecordBeingEdited.depth,
+      site: collectRecordBeingEdited.site.id,
+      management: collectRecordBeingEdited.management.id,
+      sampleDate: '',
+      sampleTime: '',
+    },
+    enableReinitialize: true,
+    validationSchema: Yup.object({
+      site: Yup.string()
+        .oneOf(validSiteValues, 'Invalid site')
+        .required('Site is required'),
+      management: Yup.string()
+        .oneOf(validManagementRegimeValues, 'Invalid management regime')
+        .required('Management Regime is required'),
+      depth: Yup.number().required('Depth is required'),
+    }),
+    onSubmit: (values) => console.log('here', values),
   }
 
-  const saveRecord = () => {}
-
   return (
-    <SubLayout2
-      sidebar={<NavMenu />}
-      lowerRight={
-        <CollectForms
-          collectRecord={curCollectRecord}
-          sites={sites}
-          managementRegimes={managementRegimes}
-          handleInputChange={handleInputChange}
+    <Formik {...formikOptions}>
+      {(formik) => (
+        <SubLayout2
+          sidebar={<NavMenu />}
+          lowerRight={
+            <form id="benthiclit-form" onSubmit={formik.handleSubmit}>
+              <SampleInfoForms
+                formik={formik}
+                collectRecord={collectRecordBeingEdited}
+                sites={sites}
+                managementRegimes={managementRegimes}
+              />
+              {/* // put transect here */}
+            </form>
+          }
+          upperRight={
+            <RowRight>
+              <ButtonCallout
+                type="submit"
+                onSubmit={formik.handleSubmit}
+                form="benthiclit-form"
+              >
+                Save
+              </ButtonCallout>
+            </RowRight>
+          }
         />
-      }
-      upperRight={
-        <RowRight>
-          <ButtonCallout onClick={saveRecord}>Save</ButtonCallout>
-        </RowRight>
-      }
-    />
+      )}
+    </Formik>
   )
 }
 
