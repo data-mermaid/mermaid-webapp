6a8767ad58f266c3a01cd1b0a5dbcf2c5fc2d0d9
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-14 13:48:09 -0600

    M1494 prep: rename collect record page to specific: EditFishBelt (to avoid a complicated abstraction)

--- src/components/pages/EditfishBelt/EditFishBelt.test.js
+++ src/components/pages/EditfishBelt/EditFishBelt.test.js
@@ -2,9 +2,9 @@ import '@testing-library/jest-dom/extend-expect'
 // import React from 'react'
 // import { renderAuthenticatedOnline, screen } from '../../testUtilities/testingLibraryWithHelpers'
 
-// import CollectRecord from './CollectRecord'
+// import EditFishBelt from './EditFishBelt'
 
-test('CollectRecord component renders with the expected UI elements', () => {
-  // renderAuthenticatedOnline(<CollectRecord />)
+test('EditFishBelt component renders with the expected UI elements', () => {
+  // renderAuthenticatedOnline(<EditFishBelt />)
   // expect(screen.getByText('I should fail'))
 })
