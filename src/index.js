03fd77fdb7948d8fe92328b0a1b06091a4f5002e
Parmvir Thind <parmvirsthind@gmail.com>
2021-03-15 11:00:58 -0700

    remove public_url from redirectUri as it does not accept wildcards on auth0's end

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin + process.env.PUBLIC_URL}
+      redirectUri={window.location.origin}
       audience={process.env.REACT_APP_AUTH0_AUDIENCE}
       scope="read:current_user update:current_user_metadata"
     >
