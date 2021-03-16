3cc7294f485979440f9fa0e0c220d438f593b902
AL Leonard <aleonard@sparkgeo.com>
2021-03-16 11:29:14 -0400

    Initial footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,83 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components/macro'
 import { Link } from 'react-router-dom'
-import { RowSpaceBetween, RowRight, RowLeft } from '../generic/positioning'
 import OfflineToggle from '../OfflineToggle'
-import { IconRefresh } from '../icons'
+import { IconRefresh, IconCheck } from '../icons'
+import { ButtonCallout } from '../generic/buttons'
 
-const IconStyleWrapper = styled.div`
-  color: grey;
+const rightColumn = css`
+  justify-self: end;
+  text-align: end;
+  padding: 0 ${(props) => props.theme.spacing.xsmall} 0 0;
+`
+const StyledFooter = styled('footer')`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  background: ${(props) => props.theme.color.secondaryColor};
+  border-top: solid 1px ${(props) => props.theme.color.border};
+  p,
+  label,
+  nav a,
+  button {
+    font-size: 1.2rem;
+    margin: ${(props) => props.theme.spacing.small} 0;
+  }
+`
+const OfflineToggleWrapper = styled('div')`
+  padding-left: ${(props) => props.theme.spacing.xsmall};
+  label {
+    padding-left: ${(props) => props.theme.spacing.xsmall};
+    display: inline-block;
+  }
+`
+const FooterNav = styled('nav')`
+  a {
+    display: inline-block;
+    padding: 0 ${(props) => props.theme.spacing.xsmall};
+  }
+`
+const UpdateWrapper = styled('div')`
+  ${rightColumn};
+  svg {
+    // weighted alignment adjustment
+    position: relative;
+    top: 2px;
+  }
+  p {
+    display: inline-block;
+  }
+`
+const Copyright = styled.p`
+  ${rightColumn};
 `
 
 const Footer = () => {
   return (
-    <RowSpaceBetween>
-      <RowLeft>
-        <div>© 2021 Mermaid Version v1.0.0</div>
+    <StyledFooter>
+      <OfflineToggleWrapper>
         <OfflineToggle />
-        <IconStyleWrapper>
-          <IconRefresh />
-        </IconStyleWrapper>
-      </RowLeft>
-      <RowRight as="nav">
+        <label htmlFor="offline-toggle-switch">
+          {/* You&apos;re <strong>OFFLINE</strong> */}
+          You&apos;re <strong>ONLINE</strong>
+        </label>
+      </OfflineToggleWrapper>
+      <UpdateWrapper>
+        {/* When it's up to date */}
+        <IconCheck /> <p>MERMAID is up to date</p>
+        {/* When it needs to be updated */}
+        {/* <ButtonCallout>
+          <IconRefresh /> There&apos;s a new Version of MERMAID
+        </ButtonCallout> */}
+      </UpdateWrapper>
+      <FooterNav>
         <Link to="/#">Help</Link>
         <Link to="/#">Terms</Link>
         <Link to="/#">Contact</Link>
         <Link to="/#">Changelog</Link>
         <Link to="/#">Credits</Link>
-      </RowRight>
-    </RowSpaceBetween>
+      </FooterNav>
+      <Copyright>&copy; 2021 Mermaid Version v1.0.0</Copyright>
+    </StyledFooter>
   )
 }
 
