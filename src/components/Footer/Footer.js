48440d040c02686c3e3e396cb84ac4ee5760cb6b
AL Leonard <aleonard@sparkgeo.com>
2021-03-16 11:52:07 -0400

    Made the footer responsive and black

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styled, { css } from 'styled-components/macro'
 import { Link } from 'react-router-dom'
 import OfflineToggle from '../OfflineToggle'
+import { mediaQueryPhoneOnly } from '../../library/styling/mediaQueries'
 import { IconRefresh, IconCheck } from '../icons'
 import { ButtonCallout } from '../generic/buttons'
 
@@ -9,19 +10,35 @@ const rightColumn = css`
   justify-self: end;
   text-align: end;
   padding: 0 ${(props) => props.theme.spacing.xsmall} 0 0;
+  ${mediaQueryPhoneOnly(css`
+    justify-self: start;
+    text-align: start;
+    padding: 0 0 0 ${(props) => props.theme.spacing.xsmall};
+  `)}
 `
 const StyledFooter = styled('footer')`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  background: ${(props) => props.theme.color.secondaryColor};
+  background: ${(props) => props.theme.color.black};
   border-top: solid 1px ${(props) => props.theme.color.border};
   p,
   label,
   nav a,
   button {
+    color: ${(props) => props.theme.color.white};
     font-size: 1.2rem;
     margin: ${(props) => props.theme.spacing.small} 0;
   }
+  ${mediaQueryPhoneOnly(css`
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(4, auto);
+    p,
+    label,
+    nav a,
+    button {
+      margin: ${(props) => props.theme.spacing.medium} 0;
+    }
+  `)}
 `
 const OfflineToggleWrapper = styled('div')`
   padding-left: ${(props) => props.theme.spacing.xsmall};
@@ -39,9 +56,8 @@ const FooterNav = styled('nav')`
 const UpdateWrapper = styled('div')`
   ${rightColumn};
   svg {
-    // weighted alignment adjustment
-    position: relative;
-    top: 2px;
+    color: ${(props) => props.theme.color.white};
+    font-size: 1.2rem;
   }
   p {
     display: inline-block;
