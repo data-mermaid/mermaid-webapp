555246e669a7c772d605e3f023c0073480779509
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-20 11:06:22 -0600

    M74 add a starter table for collect records that includes (properly) sortable columns, and pagination

--- src/components/pages/Collect/Collect.js
+++ src/components/pages/Collect/Collect.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
-import React from 'react'
+import { usePagination, useSortBy, useTable } from 'react-table'
+import React, { useMemo } from 'react'
 
 import useCurrentProjectPath from '../../../library/useCurrentProjectPath'
 import SubLayout2 from '../../SubLayout2'
@@ -8,6 +9,11 @@ import { H3 } from '../../generic/text'
 import { RowSpaceBetween } from '../../generic/positioning'
 
 import AddSampleUnitButton from './AddSampleUnitButton'
+
+import { Table, Tr, Th, Td } from '../../generic/Table/table'
+import { reactTableNaturalSort } from '../../generic/Table/reactTableNaturalSort'
+import PageSizeSelector from '../../generic/Table/PageSizeSelector'
+import PageSelector from '../../generic/Table/PageSelector'
 
 const TopBar = () => (
   <>
@@ -18,32 +24,187 @@ const TopBar = () => (
   </>
 )
 
-const CollectRecordList = ({ mermaidData }) => {
-  const currentProjectPath = useCurrentProjectPath()
+const Collect = ({ mermaidData }) => {
   const { collectRecords } = mermaidData
+  const currentProjectPath = useCurrentProjectPath()
 
-  return (
-    <>
-      {collectRecords.map(({ id, method, data }) => (
-        <div key={id}>
-          <Link to={`${currentProjectPath}/collecting/${data.protocol}/${id}`}>
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Method',
+        accessor: 'method',
+        sortType: (rowA, rowB, columnId) => {
+          // this sort is different, because the data values will be children of react nodes
+          return rowA.original[columnId].props.children.localeCompare(
+            rowB.original[columnId].props.children,
+            'en',
+            { numeric: true, caseFirst: 'upper' },
+          )
+        },
+      },
+      {
+        Header: 'Site',
+        accessor: 'site',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Management',
+        accessor: 'management',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Sample Unit #',
+        accessor: 'sampleUnitNumber',
+        align: 'right',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'size',
+        accessor: 'size',
+        align: 'right',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Depth (m)',
+        accessor: 'depth',
+        align: 'right',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Sample Date',
+        accessor: 'sampleDate',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Observers',
+        accessor: 'observers',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+        sortType: reactTableNaturalSort,
+      },
+      {
+        Header: 'Synced',
+        accessor: 'synced',
+        sortType: reactTableNaturalSort,
+      },
+    ],
+    [],
+  )
+
+  const data = useMemo(
+    () =>
+      collectRecords.map(({ id, method, data: recordData }) => ({
+        method: (
+          <Link
+            to={`${currentProjectPath}/collecting/${recordData.protocol}/${id}`}
+          >
             {method}
           </Link>
-        </div>
-      ))}
+        ),
+        site: mermaidData.getSite(recordData.sample_event.site).name,
+        management: mermaidData.getManagementRegime(
+          recordData.sample_event.management,
+        ).name,
+        sampleUnitNumber: 'wip',
+        size: 'wip',
+        depth: 'wip',
+        sampleDate: 'wip',
+        observers: 'wip',
+        status: 'wip',
+        synced: 'wip',
+      })),
+    [collectRecords, currentProjectPath, mermaidData],
+  )
+  const {
+    canNextPage,
+    canPreviousPage,
+    getTableBodyProps,
+    getTableProps,
+    gotoPage,
+    headerGroups,
+    nextPage,
+    page,
+    pageOptions,
+    prepareRow,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+  } = useTable(
+    {
+      columns,
+      data,
+      initialState: { pageSize: 10 },
+    },
+    useSortBy,
+    usePagination,
+  )
+  const handleRowsNumberChange = (e) => {
+    setPageSize(Number(e.target.value))
+  }
+
+  const table = (
+    <>
+      <Table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            <Tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => (
+                <Th
+                  {...column.getHeaderProps(column.getSortByToggleProps())}
+                  isSorted={column.isSorted}
+                  isSortedDescending={column.isSortedDesc}
+                >
+                  {column.render('Header')}
+                </Th>
+              ))}
+            </Tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {page.map((row) => {
+            prepareRow(row)
+
+            return (
+              <Tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  return (
+                    <Td {...cell.getCellProps()} align={cell.column.align}>
+                      {cell.render('Cell')}
+                    </Td>
+                  )
+                })}
+              </Tr>
+            )
+          })}
+        </tbody>
+      </Table>
+      <RowSpaceBetween>
+        <PageSizeSelector
+          onChange={handleRowsNumberChange}
+          pageSize={pageSize}
+          pageSizeOptions={[10, 50, 100]}
+        />
+        <PageSelector
+          onPreviousClick={previousPage}
+          previousDisabled={!canPreviousPage}
+          onNextClick={nextPage}
+          nextDisabled={!canNextPage}
+          onGoToPage={gotoPage}
+          currentPageIndex={pageIndex}
+          pageCount={pageOptions.length}
+        />
+      </RowSpaceBetween>
     </>
   )
-}
 
-CollectRecordList.propTypes = {
-  mermaidData: mermaidDataPropType.isRequired,
-}
-
-const Collect = ({ mermaidData }) => {
   return (
     <SubLayout2
-      content={<CollectRecordList mermaidData={mermaidData} />}
+      // content={<CollectRecordList mermaidData={mermaidData} />}
       toolbar={<TopBar />}
+      content={table}
     />
   )
 }
--- src/components/pages/Collect/Collect.test.js
+++ src/components/pages/Collect/Collect.test.js
@@ -1,13 +1,96 @@
 import '@testing-library/jest-dom/extend-expect'
-// import React from 'react'
-// import {
-//   renderAuthenticatedOnline,
-//   screen,
-// } from '../../../testUtilities/testingLibraryWithHelpers'
+import userEvent from '@testing-library/user-event'
+import React from 'react'
+import mockMermaidData from '../../../testUtilities/mockMermaidData'
+import {
+  renderAuthenticatedOnline,
+  screen,
+  within,
+} from '../../../testUtilities/testingLibraryWithHelpers'
+import Collect from './Collect'
 
 // import Collect from './Collect'
 
 test('Collect component renders with the expected UI elements', () => {
   // const utilities = renderAuthenticatedOnline(<Collect />)
   // expect(screen.getByText('I should fail'))
+})
+
+test('Collect Records table sorts properly by method column', () => {
+  renderAuthenticatedOnline(<Collect mermaidData={mockMermaidData} />)
+
+  const table = screen.getByRole('table')
+
+  const tableRows = within(table).getAllByRole('row')
+
+  expect(within(tableRows[1]).getByText('Benthic LIT'))
+
+  // click twice to change to descending order
+  userEvent.dblClick(within(table).getByText('Method'))
+
+  const tableRowsAfter = within(table).getAllByRole('row')
+
+  expect(within(tableRowsAfter[1]).getByText('Fish Belt'))
+})
+test('Collect Records table sorts properly by site column', () => {
+  renderAuthenticatedOnline(<Collect mermaidData={mockMermaidData} />)
+
+  const table = screen.getByRole('table')
+
+  const tableRows = within(table).getAllByRole('row')
+
+  expect(within(tableRows[1]).getByText('1203'))
+
+  // click twice to change to descending order
+  userEvent.dblClick(within(table).getByText('Management'))
+
+  const tableRowsAfter = within(table).getAllByRole('row')
+
+  expect(within(tableRowsAfter[1]).getByText('Karang Kapal'))
+})
+test('Collect Records table sorts properly by management column', () => {})
+test('Collect Records table sorts properly by samplu unit # column', () => {})
+test('Collect Records table sorts properly by size column', () => {})
+test('Collect Records table sorts properly by depth column', () => {})
+test('Collect Records table sorts properly by sample date column', () => {})
+test('Collect Records table sorts properly by observers column', () => {})
+test('Collect Records table sorts properly by status column', () => {})
+test('Collect Records table sorts properly by synced column', () => {})
+
+test('Collect Records table changes number of rows visible size when pagination size is changed', () => {
+  renderAuthenticatedOnline(<Collect mermaidData={mockMermaidData} />)
+
+  const table = screen.getByRole('table')
+
+  const tableRows = within(table).getAllByRole('row')
+
+  // 10 + header row
+  expect(tableRows.length).toEqual(11)
+
+  userEvent.selectOptions(screen.getByTestId('page-size-selector'), '50')
+
+  const tableRowsAfter = within(table).getAllByRole('row')
+
+  // 11 + header row
+  expect(tableRowsAfter.length).toEqual(12)
+})
+
+test('Collect Records table change pages when different page is selected ', () => {
+  renderAuthenticatedOnline(<Collect mermaidData={mockMermaidData} />)
+
+  const table = screen.getByRole('table')
+
+  const tableRows = within(table).getAllByRole('row')
+
+  const PageSelector = screen.getByTestId('page-control')
+
+  // 10 + header row
+  expect(tableRows.length).toEqual(11)
+
+  userEvent.click(within(PageSelector).getByText('2'))
+
+  const tableRowsAfter = within(table).getAllByRole('row')
+
+  // 11 + header row
+  expect(tableRowsAfter.length).toEqual(2)
 })
