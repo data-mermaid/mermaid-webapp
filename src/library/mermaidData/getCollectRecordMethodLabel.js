e049524cf32180756c06aaab495969cb86012ce3
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-20 11:59:47 -0600

    M89 remove 'method' from mock collect records

---
+++ src/library/mermaidData/getCollectRecordMethodLabel.js
@@ -0,0 +1,18 @@
+const getCollectRecordMethodLabel = (protocol) => {
+  switch (protocol) {
+    default:
+      return 'Unknown Method'
+    case 'fishbelt':
+      return 'Fish Belt'
+    case 'benthiclit':
+      return 'Benthic LIT'
+    case 'benthicpit':
+      return 'Benthic PIT'
+    case 'habitatcomplexity':
+      return 'Habitat Complexity'
+    case 'bleachingqc':
+      return 'Bleaching'
+  }
+}
+
+export default getCollectRecordMethodLabel
