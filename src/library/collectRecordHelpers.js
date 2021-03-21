fcda286994b4cbea65847b0912f46048d9b45631
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-21 15:06:14 -0600

    M90 M90 update EditFishBelt page to access database gateway and not rely on temporary global app state for data

--- src/library/collectRecordHelpers.js
+++ src/library/collectRecordHelpers.js
@@ -2,23 +2,22 @@ import * as Yup from 'yup'
 import dateFormat from './dateFormat'
 
 const getSampleInfoInitialValues = (collectRecordData, transectType) => ({
-  depth: collectRecordData[transectType].depth || -9999,
-  site: collectRecordData.sample_event.site || '',
-  management: collectRecordData.sample_event.management || '',
-  sample_date: dateFormat(collectRecordData.sample_event.sample_date) || '',
-  sample_time: collectRecordData[transectType].sample_time || '',
+  depth: collectRecordData?.[transectType].depth || -9999,
+  site: collectRecordData?.sample_event.site || '',
+  management: collectRecordData?.sample_event.management || '',
+  sample_date: dateFormat(collectRecordData?.sample_event.sample_date) || '',
+  sample_time: collectRecordData?.[transectType].sample_time || '',
 })
 
 const getTransectInitialValues = (collectRecordData, transectType) => ({
-  transectNumber: collectRecordData[transectType].number || -9999,
-  label: collectRecordData[transectType].label || '',
-  transectLengthSurveyed: collectRecordData[transectType].len_surveyed || -9999,
-  notes: collectRecordData.sample_event.notes || '',
+  transectNumber: collectRecordData?.[transectType].number || -9999,
+  label: collectRecordData?.[transectType].label || '',
+  transectLengthSurveyed:
+    collectRecordData?.[transectType].len_surveyed || -9999,
+  notes: collectRecordData?.sample_event.notes || '',
 })
 
-const getSampleInfoValidationInfo = (mermaidData) => {
-  const { sites, managementRegimes } = mermaidData
-
+const getSampleInfoValidationInfo = ({ sites, managementRegimes }) => {
   const validSiteValues = sites.map((site) => site.id)
 
   const validManagementRegimeValues = managementRegimes.map(
