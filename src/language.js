4ee922128797e498385a09f653f685527b8ac9cb
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-21 15:51:32 -0600

    M90 properly and consistently catch DB errors with user message. Make sure app is ready for DB calls before getting collect record.

--- src/language.js
+++ src/language.js
@@ -1,7 +1,13 @@
 const error = {
-  userProfileUnavailable: 'The user profile is unavailable.',
   appNotAuthenticatedOrReady:
     'Initialization error. Try reloading or reauthenticating',
+  collectRecordUnavailable:
+    'Collect Record data is currently unavailable. Please try again',
+  collectRecordsUnavailable:
+    'Collect Records data is currently unavailable. Please try again',
+  projectsUnavailable:
+    'Projects data is currently unavailable. Please try again',
+  userProfileUnavailable: 'The user profile is unavailable.',
 }
 
 export default { error }
