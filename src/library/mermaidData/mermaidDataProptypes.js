3c39dde50364d11c6a4afb74941372319739a2e2
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-21 15:12:32 -0600

    M90 move mermaid data proptypes to own file

---
+++ src/library/mermaidData/mermaidDataProptypes.js
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types'
+
+export const projectsPropType = PropTypes.arrayOf(
+  PropTypes.shape({
+    name: PropTypes.string,
+    countries: PropTypes.arrayOf(PropTypes.string),
+    num_sites: PropTypes.number,
+    updated_on: PropTypes.string,
+  }),
+)
+export const sitePropType = PropTypes.shape({
+  id: PropTypes.string,
+  name: PropTypes.string,
+  reef_type: PropTypes.string,
+  reef_zone: PropTypes.string,
+  exposure: PropTypes.string,
+})
+export const collectRecordPropType = PropTypes.shape({
+  method: PropTypes.string,
+  site: PropTypes.string,
+  management_regime: PropTypes.string,
+  data: PropTypes.shape({
+    protocol: PropTypes.string,
+  }),
+  depth: PropTypes.number,
+})
+
+export const managementRegimePropType = PropTypes.shape({
+  name: PropTypes.string,
+})
