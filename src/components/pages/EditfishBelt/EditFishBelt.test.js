4f220d036634e3c801a26c4343363a95bccd164e
Melissa Nunes <mnunes@sparkgeo.com>
2021-03-28 12:33:05 -0600

    M72 test expected ui elements of fish belt page, and initial values of form

--- src/components/pages/EditfishBelt/EditFishBelt.test.js
+++ src/components/pages/EditfishBelt/EditFishBelt.test.js
@@ -1,10 +1,80 @@
 import '@testing-library/jest-dom/extend-expect'
-// import React from 'react'
-// import { renderAuthenticatedOnline, screen } from '../../testUtilities/testingLibraryWithHelpers'
+import { Route } from 'react-router-dom'
+import React from 'react'
+import {
+  renderAuthenticatedOnline,
+  screen,
+  waitFor,
+} from '../../../testUtilities/testingLibraryWithHelpers'
+import mockOnlineMermaidDatabaseGatewayInstance from '../../../testUtilities/mockOnlineMermaidDatabaseGatewayInstance'
 
-// import EditFishBelt from './EditFishBelt'
+import EditFishBelt from './EditFishBelt'
 
-test('EditFishBelt component renders with the expected UI elements', () => {
-  // renderAuthenticatedOnline(<EditFishBelt />)
-  // expect(screen.getByText('I should fail'))
+test('EditFishBelt component renders with the expected UI elements', async () => {
+  renderAuthenticatedOnline(
+    <EditFishBelt
+      databaseGatewayInstance={mockOnlineMermaidDatabaseGatewayInstance}
+    />,
+  )
+
+  await waitFor(() =>
+    expect(screen.queryByLabelText('loading indicator')).toBeNull(),
+  )
+
+  expect(
+    screen.getByText('Sample Info', {
+      selector: 'h2',
+    }),
+  )
+  expect(
+    screen.getByText('Transect', {
+      selector: 'h2',
+    }),
+  )
+
+  expect(screen.getByLabelText('Site'))
+  expect(screen.getByLabelText('Management'))
+  expect(screen.getByLabelText('Depth'))
+  expect(screen.getByLabelText('Sample Date'))
+  expect(screen.getByLabelText('Sample Time'))
+  expect(screen.getByLabelText('Transect Number'))
+  expect(screen.getByLabelText('Label'))
+  expect(screen.getByLabelText('Transect Length Surveyed'))
+  expect(screen.getByLabelText('Width'))
+  expect(screen.getByLabelText('Fish Size Bin'))
+  expect(screen.getByLabelText('Reef Slope'))
+  expect(screen.getByLabelText('Notes'))
+})
+
+test('EditFishBelt form inputs are initialized with the correct values', async () => {
+  renderAuthenticatedOnline(
+    <Route path="/projects/:projectId/collecting/fishbelt/:recordId">
+      <EditFishBelt
+        databaseGatewayInstance={mockOnlineMermaidDatabaseGatewayInstance}
+      />
+    </Route>,
+    { initialEntries: ['/projects/fakewhatever/collecting/fishbelt/2'] },
+  )
+
+  await waitFor(() =>
+    expect(screen.queryByLabelText('loading indicator')).toBeNull(),
+  )
+
+  // Site select
+  expect(screen.getByDisplayValue('Karang Kapal'))
+  // Management select
+  expect(screen.getByDisplayValue('Bureta tabu'))
+  expect(screen.getByLabelText('Depth')).toHaveValue(10)
+  expect(screen.getByLabelText('Sample Date')).toHaveValue('2021-03-02')
+  expect(screen.getByLabelText('Sample Time')).toHaveValue('11:55')
+  expect(screen.getByLabelText('Transect Number')).toHaveValue(2)
+  expect(screen.getByLabelText('Label')).toHaveValue('FB-2')
+  expect(screen.getByLabelText('Transect Length Surveyed')).toHaveValue(6)
+  // width select
+  expect(screen.getByDisplayValue('2m'))
+  // fish size bin select
+  expect(screen.getByDisplayValue(5))
+  // reef slope select
+  expect(screen.getByDisplayValue('flat'))
+  expect(screen.getByLabelText('Notes')).toHaveValue('some fish notes')
 })
--- src/library/collectRecordHelpers.js
+++ src/library/collectRecordHelpers.js
@@ -1,21 +1,24 @@
 import * as Yup from 'yup'
 import dateFormat from './dateFormat'
 
-const getSampleInfoInitialValues = (collectRecordData, transectType) => ({
-  depth: collectRecordData?.[transectType].depth ?? '',
-  site: collectRecordData?.sample_event.site ?? '',
-  management: collectRecordData?.sample_event.management ?? '',
-  sample_date: dateFormat(collectRecordData?.sample_event.sample_date) ?? '',
-  sample_time: collectRecordData?.[transectType].sample_time ?? '',
-})
+const getSampleInfoInitialValues = (collectRecordData, transectType) => {
+  // console.log('here', collectRecordData, transectType)
+  return {
+    depth: collectRecordData?.[transectType]?.depth ?? '',
+    site: collectRecordData?.sample_event.site ?? '',
+    management: collectRecordData?.sample_event.management ?? '',
+    sample_date: dateFormat(collectRecordData?.sample_event.sample_date) ?? '',
+    sample_time: collectRecordData?.[transectType]?.sample_time ?? '',
+  }
+}
 
 const getTransectInitialValues = (collectRecordData, transectType) => ({
-  transectNumber: collectRecordData?.[transectType].number ?? '',
-  label: collectRecordData?.[transectType].label ?? '',
-  transectLengthSurveyed: collectRecordData?.[transectType].len_surveyed ?? '',
-  width: collectRecordData?.[transectType].width || '',
-  fishSizeBin: collectRecordData?.[transectType].size_bin || '',
-  reefSlope: collectRecordData?.[transectType].reef_slope || '',
+  transectNumber: collectRecordData?.[transectType]?.number ?? '',
+  label: collectRecordData?.[transectType]?.label ?? '',
+  transectLengthSurveyed: collectRecordData?.[transectType]?.len_surveyed ?? '',
+  width: collectRecordData?.[transectType]?.width || '',
+  fishSizeBin: collectRecordData?.[transectType]?.size_bin || '',
+  reefSlope: collectRecordData?.[transectType]?.reef_slope || '',
   notes: collectRecordData?.sample_event.notes ?? '',
 })
 
