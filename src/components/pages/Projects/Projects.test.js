adbb57b134d0109cede2aacb74a62e68a2b1b3a7
Nick Hoang <nickhoang11@gmail.com>
2021-03-30 17:11:29 -0700

    Add test to render project list count.

--- src/components/pages/Projects/Projects.test.js
+++ src/components/pages/Projects/Projects.test.js
@@ -1,20 +1,40 @@
 import '@testing-library/jest-dom/extend-expect'
-// import React from 'react'
-// import {
-//   renderAuthenticatedOnline,
-//   screen,
-// } from '../../../testUtilities/testingLibraryWithHelpers'
+import React from 'react'
+import mockOnlineDatabaseSwitchboardInstance from '../../../testUtilities/mockOnlineDatabaseSwitchboardInstance'
+import {
+  renderAuthenticatedOnline,
+  screen,
+  waitFor,
+  within,
+} from '../../../testUtilities/testingLibraryWithHelpers'
+import Projects from './Projects'
 
 // import Projects from './Projects'
 
-test('Projects component renders with the expected UI elements', () => {
+test('Projects component renders with the expected UI elements', async () => {
   // const utilities = renderAuthenticatedOnline(<Projects />)
   // expect(screen.getByText('I should fail'))
   // a new ticket will be made for these tests
-  // expect count of projects renderes is the same as the count in mock data
+  renderAuthenticatedOnline(
+    <Projects
+      databaseSwitchboardInstance={mockOnlineDatabaseSwitchboardInstance}
+    />,
+  )
+
+  await waitFor(() =>
+    expect(screen.queryByLabelText('loading indicator')).toBeNull(),
+  )
+
+  // expect count of projects renders is the same as the count in mock data
+  const projectList = screen.getByRole('list')
+
+  const projectListItems = within(projectList).getAllByRole('listitem')
+
+  expect(projectListItems).toHaveLength(5)
+
   // expect filter bar, sort buttons, new project button
 })
 test('A project card hover, shows extra buttons', () => {})
-test('A project card shows  relavent data for a project', () => {
+test('A project card shows  relevant data for a project', () => {
   // test the things that are wired in from data. (title, country, etc)
 })
