f08d7d8a240eedba3a061491d8b494c9fdfe302a
Nick Hoang <nickhoang11@gmail.com>
2021-04-06 11:37:21 -0700

    clean up import userevent.

--- src/components/pages/Projects/Projects.test.js
+++ src/components/pages/Projects/Projects.test.js
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom/extend-expect'
 import React from 'react'
-import userEvent from '@testing-library/user-event'
 import mockOnlineDatabaseSwitchboardInstance from '../../../testUtilities/mockOnlineDatabaseSwitchboardInstance'
 import {
   renderAuthenticatedOnline,
