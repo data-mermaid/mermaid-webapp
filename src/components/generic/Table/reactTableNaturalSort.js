06e87363ae1d9204720b8d7a0dfd3b8c8fee8b6f
Melissa Nunes <mnunes@sparkgeo.com>
2021-04-17 12:36:34 -0600

    M135 update react table sort strategies to accommodate when a cell value is undefined

--- src/components/generic/Table/reactTableNaturalSort.js
+++ src/components/generic/Table/reactTableNaturalSort.js
@@ -1,4 +1,4 @@
-export const reactTableNaturalSort = (rowA, rowB, columnId) => {
+const reactTableNaturalSort = (rowA, rowB, columnId) => {
   const rowACellContents = rowA.original[columnId] ?? ''
   const rowBCellContents = rowB.original[columnId] ?? ''
 
@@ -7,3 +7,17 @@ export const reactTableNaturalSort = (rowA, rowB, columnId) => {
     caseFirst: 'upper',
   })
 }
+
+const reactTableNaturalSortReactNodes = (rowA, rowB, columnId) => {
+  // this sort is different, because the data values will be children of react nodes
+
+  const rowACellContents = rowA.original[columnId].props.children ?? ''
+  const rowBCellContents = rowB.original[columnId].props.children ?? ''
+
+  return rowACellContents.localeCompare(rowBCellContents, 'en', {
+    numeric: true,
+    caseFirst: 'upper',
+  })
+}
+
+export { reactTableNaturalSort, reactTableNaturalSortReactNodes }
