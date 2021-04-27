96a79d1fcb9c0e4b7226671f78499020cb4ce0d4
Nick Hoang <nickhoang11@gmail.com>
2021-04-27 14:32:09 -0700

    Refactor with 2 solutions before finalize this component.

--- src/components/generic/OfflineHide/OfflineHide.js
+++ src/components/generic/OfflineHide/OfflineHide.js
@@ -1,23 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import { useOnlineStatus } from '../../../library/onlineStatusContext'
 
-const OfflineControlContainer = styled.div`
-  display: ${(props) => (props.isAppOnline ? 'inline' : 'none')};
-`
+// const OfflineControlContainer = styled.div`
+//   display: ${(props) => (props.isAppOnline ? 'inline' : 'none')};
+// `
 
 const OfflineHide = ({ children }) => {
   const { isOnline: isAppOnline } = useOnlineStatus()
 
-  return (
-    <OfflineControlContainer isAppOnline={isAppOnline}>{children}</OfflineControlContainer>
-  )
+  // this looks nicer, but not friendly with test. Testing "display: none" doesn't work well with "not.toBeInDocument()"
+  // I want to see how other thoughts are before finalize the solution.
+  // return (
+  //   <OfflineControlContainer isAppOnline={isAppOnline}>
+  //     {children}
+  //   </OfflineControlContainer>
+  // )
 
   // below is other way which looks kinda awful, but don't have to deal with css
-  // const offlineHideContainer = isAppOnline && <>{children}</>
+  const offlineHideContainer = isAppOnline && <>{children}</>
 
-  // return <>{offlineHideContainer}</>
+  return <>{offlineHideContainer}</>
 }
 
 OfflineHide.propTypes = {
