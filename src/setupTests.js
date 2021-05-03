1fc4e2e88586dc08350703c94a464a5b1e7cf6b7
Melissa Nunes <mnunes@sparkgeo.com>
2021-05-03 09:11:20 -0600

    Revert "hopefully fixing github actions tests failing"
    This reverts commit 826be4d7ae47b379b9f77b055307b3ad55c8df87.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,9 +3,6 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom'
-import { configure } from '@testing-library/react'
-
-configure({ asyncUtilTimeout: 20000 })
 
 beforeEach(() => {
   window.sessionStorage.clear()
