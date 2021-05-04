64ab480a74c59a4240f6f6c226c20d4ab21522b4
tienhoah <nickhoang11@gmail.com>
2021-05-04 12:13:55 -0700

    Clean up offline hide.

--- src/components/generic/OfflineHide/OfflineHide.js
+++ src/components/generic/OfflineHide/OfflineHide.js
@@ -1,27 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-// import styled from 'styled-components'
 import { useOnlineStatus } from '../../../library/onlineStatusContext'
-
-// const OfflineControlContainer = styled.div`
-//   display: ${(props) => (props.isAppOnline ? 'inline' : 'none')};
-// `
 
 const OfflineHide = ({ children }) => {
   const { isOnline: isAppOnline } = useOnlineStatus()
 
-  // this looks nicer, but not friendly with test. Testing "display: none" doesn't work well with "not.toBeInDocument()"
-  // I want to see how other thoughts are before finalize the solution.
-  // return (
-  //   <OfflineControlContainer isAppOnline={isAppOnline}>
-  //     {children}
-  //   </OfflineControlContainer>
-  // )
-
-  // below is other way which looks kinda awful, but don't have to deal with css
-  const offlineHideContainer = isAppOnline && <>{children}</>
-
-  return <>{offlineHideContainer}</>
+  return isAppOnline && <>{children}</>
 }
 
 OfflineHide.propTypes = {
