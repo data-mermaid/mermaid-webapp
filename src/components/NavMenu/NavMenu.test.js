e41e5db4f2af63b41b770aac071ebcf5b963cf78
tienhoah <nickhoang11@gmail.com>
2021-05-05 15:05:59 -0700

    Add offline hide to nav menu + tests.

---
+++ src/components/NavMenu/NavMenu.test.js
@@ -0,0 +1,88 @@
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+
+import {
+  mockMermaidApiAllSuccessful,
+  renderAuthenticatedOffline,
+  renderAuthenticatedOnline,
+  screen,
+  waitFor,
+} from '../../testUtilities/testingLibraryWithHelpers'
+import NavMenu from '.'
+
+beforeAll(() => {
+  mockMermaidApiAllSuccessful.listen()
+})
+afterEach(() => {
+  mockMermaidApiAllSuccessful.resetHandlers()
+})
+afterAll(() => {
+  mockMermaidApiAllSuccessful.close()
+})
+
+test('NavMenu component shows project overview, collect, data, admin links when online', () => {
+  renderAuthenticatedOnline(<NavMenu />)
+
+  expect(screen.getByText(/project overview/i)).toBeInTheDocument()
+  expect(screen.getByText(/project health/i)).toBeInTheDocument()
+  expect(screen.getByText('Collect')).toBeInTheDocument()
+  expect(screen.getByText('Collecting')).toBeInTheDocument()
+  expect(screen.getByText(/sites/i)).toBeInTheDocument()
+  expect(screen.getByText(/management regimes/i)).toBeInTheDocument()
+  expect(screen.getByText('Data')).toBeInTheDocument()
+  expect(screen.getByText(/submitted/i)).toBeInTheDocument()
+  expect(screen.getByText(/admin/i)).toBeInTheDocument()
+  expect(screen.getByText(/project info/i)).toBeInTheDocument()
+  expect(screen.getByText(/users/i)).toBeInTheDocument()
+  expect(screen.getByText(/fish families/i)).toBeInTheDocument()
+  expect(screen.getByText(/data sharing/i)).toBeInTheDocument()
+})
+
+test('NavMenu component shows collect links; and hide project overview, data, admin links when offline', async () => {
+  renderAuthenticatedOffline(<NavMenu />)
+
+  await waitFor(() => {
+    expect(screen.queryByText(/project overview/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/project health/i)).not.toBeInTheDocument()
+  })
+
+  expect(screen.getByText('Collect')).toBeInTheDocument()
+  expect(screen.getByText('Collecting')).toBeInTheDocument()
+  expect(screen.getByText('Sites')).toBeInTheDocument()
+  expect(screen.getByText('Management Regimes')).toBeInTheDocument()
+
+  await waitFor(() => {
+    expect(screen.queryByText(/data/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/submitted/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/graphs and maps/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/admin/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/project info/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/users/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/fish families/i)).not.toBeInTheDocument()
+  })
+
+  await waitFor(() => {
+    expect(screen.queryByText(/data sharing/i)).not.toBeInTheDocument()
+  })
+})
