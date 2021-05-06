0254419144b622d0feb925d4b3b7bd704a8d146d
tienhoah <nickhoang11@gmail.com>
2021-05-06 12:48:22 -0700

    Add validate and submit buttons and icons.

--- src/components/pages/collectRecordFormPages/FishBelt/FishBelt.js
+++ src/components/pages/collectRecordFormPages/FishBelt/FishBelt.js
@@ -9,6 +9,7 @@ import {
   getTransectInitialValues,
 } from '../collectRecordFormInitialValues'
 import { ButtonCallout, ButtonCaution } from '../../../generic/buttons'
+import { IconSave, IconCheck, IconUpload } from '../../../icons'
 import { ContentPageLayout } from '../../../Layout'
 import { databaseSwitchboardPropTypes } from '../../../../App/mermaidData/databaseSwitchboard'
 import { ensureTrailingSlash } from '../../../../library/strings/ensureTrailingSlash'
@@ -199,7 +200,16 @@ const FishBelt = ({ databaseSwitchboardInstance, isNewRecord }) => {
 
               <RowRight>
                 <ButtonCallout type="submit" form="fishbelt-form">
+                  <IconSave />
                   Save
+                </ButtonCallout>
+                <ButtonCallout>
+                  <IconCheck />
+                  Validate
+                </ButtonCallout>
+                <ButtonCallout>
+                  <IconUpload />
+                  Submit
                 </ButtonCallout>
               </RowRight>
             </>
