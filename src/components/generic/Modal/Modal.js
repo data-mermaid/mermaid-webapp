b482458472db01327964044979fcfa1ba0a4400c
Melissa Nunes <mnunes@sparkgeo.com>
2021-05-11 12:26:24 -0600

    M181 make modal smarter with title, mainContent, and bottomRowContent props + basic styling

--- src/components/generic/Modal/Modal.js
+++ src/components/generic/Modal/Modal.js
@@ -2,32 +2,50 @@ import Dialog from '@reach/dialog'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components/macro'
+import '@reach/dialog/styles.css'
 
 import { IconClose } from '../../icons'
 
-import '@reach/dialog/styles.css'
+import { RowRight, RowSpaceBetween } from '../positioning'
+import theme from '../../../theme'
 
 const StyledDialog = styled(Dialog)`
   margin-top: 40vh;
 `
+const TitleStyling = styled.span``
 
-const Modal = ({ children, isOpen, onDismiss }) => {
+const ModalRightAlignedButtonSpacing = styled(RowRight)`
+  & > button {
+    margin-left: ${theme.spacing.small};
+  }
+`
+
+const Modal = ({ title, mainContent, isOpen, onDismiss, bottomRowContent }) => {
   return (
     isOpen && (
       <StyledDialog onDismiss={onDismiss} aria-labelledby="dialog-content">
-        <button type="button" className="close-button" onClick={onDismiss}>
-          <IconClose aria-label="close" />
-        </button>
-        <div id="dialog-content">{children}</div>
+        <div id="dialog-content">
+          <RowSpaceBetween>
+            <TitleStyling>{title}</TitleStyling>
+            <button type="button" className="close-button" onClick={onDismiss}>
+              <IconClose aria-label="close" />
+            </button>
+          </RowSpaceBetween>
+          {mainContent}
+          <RowRight>{bottomRowContent}</RowRight>
+        </div>
       </StyledDialog>
     )
   )
 }
 
 Modal.propTypes = {
-  children: PropTypes.node.isRequired,
-  onDismiss: PropTypes.func.isRequired,
+  bottomRowContent: PropTypes.node.isRequired,
   isOpen: PropTypes.bool.isRequired,
+  mainContent: PropTypes.node.isRequired,
+  onDismiss: PropTypes.func.isRequired,
+  title: PropTypes.string.isRequired,
 }
 
 export default Modal
+export { ModalRightAlignedButtonSpacing }
--- src/components/generic/Modal/Modal.stories.js
+++ src/components/generic/Modal/Modal.stories.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
+import { ButtonSecondary } from '../buttons'
+import { RowSpaceBetween, RowLeft } from '../positioning'
 
-import Modal from './Modal'
+import Modal, { ModalRightAlignedButtonSpacing } from './Modal'
 
 export default {
   title: 'Modal',
@@ -10,15 +12,61 @@ export const Basic = () => {
   const [isOpen, setShowDialog] = useState(false)
   const open = () => setShowDialog(true)
   const close = () => setShowDialog(false)
+  const mainContent = <>main content</>
+  const bottomRowContent = (
+    <>
+      <ModalRightAlignedButtonSpacing>
+        <ButtonSecondary type="button">some button</ButtonSecondary>
+        <ButtonSecondary type="button">some other button</ButtonSecondary>
+      </ModalRightAlignedButtonSpacing>
+    </>
+  )
 
   return (
     <>
-      <button type="button" onClick={open}>
+      <ButtonSecondary type="button" onClick={open}>
         Open Dialog
-      </button>
-      <Modal isOpen={isOpen} onDismiss={close}>
-        inner content
-      </Modal>
+      </ButtonSecondary>
+      <Modal
+        isOpen={isOpen}
+        onDismiss={close}
+        title="I'm a title"
+        mainContent={mainContent}
+        bottomRowContent={bottomRowContent}
+      />
+    </>
+  )
+}
+
+export const MultipleButtons = () => {
+  const [isOpen, setShowDialog] = useState(false)
+  const open = () => setShowDialog(true)
+  const close = () => setShowDialog(false)
+  const mainContent = <>main content</>
+  const bottomRowContent = (
+    <RowSpaceBetween>
+      <RowLeft>
+        <ButtonSecondary type="button">button 1</ButtonSecondary>
+      </RowLeft>
+      <ModalRightAlignedButtonSpacing>
+        <ButtonSecondary type="button">button 2</ButtonSecondary>
+        <ButtonSecondary type="button">button 3</ButtonSecondary>
+      </ModalRightAlignedButtonSpacing>
+    </RowSpaceBetween>
+  )
+
+  return (
+    <>
+      <ButtonSecondary type="button" onClick={open}>
+        Open Dialog
+      </ButtonSecondary>
+      <Modal
+        isOpen={isOpen}
+        onDismiss={close}
+        title="I'm a title"
+        mainContent={mainContent}
+        bottomRowContent={bottomRowContent}
+      />
     </>
   )
 }
