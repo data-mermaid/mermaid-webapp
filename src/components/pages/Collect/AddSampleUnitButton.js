2256a454e9608888a0c0dfada9b4bfa7ba28e09b
tienhoah <nickhoang11@gmail.com>
2021-05-17 12:12:19 -0700

    Fix change request for temporary span.

--- src/components/pages/Collect/AddSampleUnitButton.js
+++ src/components/pages/Collect/AddSampleUnitButton.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import useCurrentProjectPath from '../../../library/useCurrentProjectPath'
 import ButtonSecondaryDropdown from '../../generic/ButtonSecondaryDropdown'
 import { Column } from '../../generic/positioning'
@@ -8,17 +8,11 @@ import { IconPlus } from '../../icons'
 
 const CustomNavLink = styled(NavLink)`
   padding: ${(props) => props.theme.spacing.xsmall};
-
-  ${(props) =>
-    props.disabled &&
-    css`
-      pointer-events: none;
-      color: grey;
-    `}
 `
 
 const TemporarySpanStyling = styled.span`
   color: grey;
+  padding: 0.5rem 1rem;
 `
 
 const AddSampleUnitButton = () => {
@@ -35,27 +29,10 @@ const AddSampleUnitButton = () => {
         <CustomNavLink to={`${currentProjectPath}/collecting/fishbelt`}>
           Fish Belt
         </CustomNavLink>
-        <CustomNavLink to={`${currentProjectPath}/collecting/benthiclit`}>
-          <TemporarySpanStyling>Benthic LIT</TemporarySpanStyling>
-        </CustomNavLink>
-        <CustomNavLink
-          to={`${currentProjectPath}/collecting/benthicpit`}
-          disabled
-        >
-          Benthic PIT
-        </CustomNavLink>
-        <CustomNavLink
-          to={`${currentProjectPath}/collecting/habitatcomplexity`}
-          disabled
-        >
-          Habitat Complexity
-        </CustomNavLink>
-        <CustomNavLink
-          to={`${currentProjectPath}/collecting/bleaching`}
-          disabled
-        >
-          Bleaching
-        </CustomNavLink>
+        <TemporarySpanStyling>Benthic LIT</TemporarySpanStyling>
+        <TemporarySpanStyling>Benthic PIT</TemporarySpanStyling>
+        <TemporarySpanStyling>Habitat Complexity</TemporarySpanStyling>
+        <TemporarySpanStyling>Bleaching</TemporarySpanStyling>
       </Column>
     </ButtonSecondaryDropdown>
   )
