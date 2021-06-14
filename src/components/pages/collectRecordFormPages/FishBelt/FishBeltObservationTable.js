ffa5e0cdc7d7fb26a088ba6e1a8a73fd1b0b042b
Melissa Nunes <mnunes@sparkgeo.com>
2021-06-14 13:21:59 -0600

    M231 refactor fishbelt observation reducer to live in own file

--- src/components/pages/collectRecordFormPages/FishBelt/FishBeltObservationTable.js
+++ src/components/pages/collectRecordFormPages/FishBelt/FishBeltObservationTable.js
@@ -9,71 +9,11 @@ import { InputWrapper } from '../../../generic/form'
 
 import InputNumberNoScroll from '../../../InputNumberNoScroll/InputNumberNoScroll'
 import InputNumberWithUnit from '../../../generic/InputNumberWithUnit/InputNumberWithUnit'
+import fishbeltObservationReducer from './fishbeltObservationReducer'
 
-const observationReducer = (state, action) => {
-  switch (action.type) {
-    case 'loadObservationsFromApi':
-      return [...action.payload]
 
-    case 'deleteObservation': {
-      const idOfRemovee = action.payload
-
-      const observationsWithTheRightOneRemoved = state.filter(
-        (observation) => observation.id !== idOfRemovee,
-      )
-
-      return observationsWithTheRightOneRemoved
-    }
-
-    case 'addObservation':
-      return [...state, { id: createUuid(), count: 0, size: 0 }]
-    case 'addNewObservationBelow': {
-      const observationsWithInsertedRow = [...state]
-      const indexToInsertAt = action.payload + 1
-
-      observationsWithInsertedRow.splice(indexToInsertAt, 0, {
-        id: createUuid(),
-        count: 0,
-        size: 0,
-      })
-
-      return observationsWithInsertedRow
-    }
-
-    case 'duplicateLastObservation': {
-      const observationWithNewId = {
-        ...action.payload.observation,
-        id: createUuid(),
-      }
-
-      return [...state, observationWithNewId]
-    }
-    case 'updateCount':
-      return state.map((observation) => {
-        const isObservationToUpdate =
-          observation.id === action.payload.observationId
-
-        return isObservationToUpdate
-          ? { ...observation, count: action.payload.newCount }
-          : observation
-      })
-    case 'updateSize':
-      return state.map((observation) => {
-        const isObservationToUpdate =
-          observation.id === action.payload.observationId
-
-        return isObservationToUpdate
-          ? { ...observation, size: action.payload.newSize }
-          : observation
-      })
-    default:
-      throw new Error("This action isn't supported by the observationReducer")
-  }
-}
-
-const FishBeltObservationTable = ({ collectRecord }) => {
   const [observationsState, observationsDispatch] = useReducer(
-    observationReducer,
+    fishbeltObservationReducer,
     [],
   )
   const haveApiObservationsBeenLoaded = useRef(false)
