813d134a336ae4fe4c01b36db9d2bfe29a59e9de
tienhoah <6288799+tienhoah@users.noreply.github.com>
2021-06-23 09:44:15 -0700

    [M211] Coral Atlas (#173)
    * Add latitude and longitude data and input fields.
    
    * Set up a basic map component for Site Form, able to communicate with Lat/Lng input fields.
    
    * Fix mapbox dependency failing in jest.
    
    * Fix changes request.
    
    * Remove compass in map control.
    
    * Set up a legend slider style and component.
    
    * Implemented Coral Atlas UI setup.
    
    * Set up coral mosaic layer, and its toggle state.
    
    * Add geomorphic and benthic layers and legends.
    
    * Refactor map and coral atlas code.
    
    * M211 co-locate tightly coupled related code
    
    * M211 opted for comment rather than making code self documenting for sake of time lines
    
    * Fix change request with renaming some variables.
    
    * Rename navBar and its set state handle.
    
    Co-authored-by: Melissa Nunes <mnunes@sparkgeo.com>

---
+++ src/components/MermaidMap/LegendDrawer/LegendDrawer.styles.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components'
+import { H3 } from '../../generic/text'
+import theme from '../../../theme'
+
+export const SliderContainer = styled.div`
+  position: absolute;
+  width: 270px;
+  right: ${(props) => (props.isOpen ? '0px' : '-270px')};
+  background: ${(props) => props.isOpen && 'rgba(255, 255, 255, 1)'};
+  top: 8px;
+  z-index: 2;
+  transition: 0.3s ease-out;
+`
+
+export const SliderHandler = styled.div`
+  position: absolute;
+  background: rgba(255, 255, 255, 1);
+  height: 120px;
+  width: 20px;
+  cursor: pointer;
+  top: 10%;
+  left: -20px;
+  border-right: 1px solid;
+  transition: 0.3s ease-out;
+  visibility: visible;
+`
+
+export const SliderHandlerName = styled.span`
+  display: inline-block;
+  position: relative;
+  transform: rotate(-90deg);
+  top: 50px;
+  left: -45px;
+  font-size: 1.5rem;
+`
+
+export const SliderLegendPanel = styled.div`
+  opacity: ${(props) => (props.isOpen ? 1 : 0)};
+  visibility: ${(props) => (props.isOpen ? 'visible' : 'hidden')};
+`
+
+export const LegendHeader = styled(H3)`
+  text-align: center;
+`
+
+export const LegendBody = styled.div`
+  height: 318px;
+  padding: 0 5px;
+  font-size: 1.5rem;
+  overflow-y: scroll;
+`
+
+export const CheckBoxLabel = styled.label`
+  padding: ${theme.spacing.xsmall};
+  width: ${(props) => props.fullWidth && '100%'};
+  display: inline-block;
+  input {
+    margin: 0 ${theme.spacing.xsmall} 0 0;
+    cursor: pointer;
+  }
+`
+
+export const LegendColor = styled.div`
+  height: 15px;
+  width: 50px;
+  border: 1px solid;
+  float: right;
+  margin-top: 2px;
+  background-color: ${(props) => props.bgColor && props.bgColor};
+`
