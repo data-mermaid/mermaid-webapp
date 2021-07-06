5331a151e164a195115a41ae6626ca397fe90983
tienhoah <nickhoang11@gmail.com>
2021-07-05 18:25:52 -0700

    Add new icon, and update project initial value.

--- src/components/pages/Admin/projectRecordInitialFormValue.js
+++ src/components/pages/Admin/projectRecordInitialFormValue.js
@@ -3,6 +3,11 @@ const getProjectInitialValues = (projectRecord) => {
     name: projectRecord?.name ?? '',
     notes: projectRecord?.notes ?? '',
     tags: projectRecord?.tags,
+    data_policy_beltfish: projectRecord?.data_policy_beltfish,
+    data_policy_benthiclit: projectRecord?.data_policy_benthiclit,
+    data_policy_benthicpit: projectRecord?.data_policy_benthicpit,
+    data_policy_bleachingqc: projectRecord?.data_policy_bleachingqc,
+    data_policy_habitatcomplexity: projectRecord?.data_policy_habitatcomplexity,
   }
 }
 
