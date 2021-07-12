742460703e7d83cd0a2fd816616c872433d9df82
Alan Leonard <aleonard@sparkgeo.com>
2021-07-12 15:27:11 -0400

    turned the options into a table

--- src/components/pages/DataSharing/DataSharing.js
+++ src/components/pages/DataSharing/DataSharing.js
@@ -6,57 +6,40 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components'
 
 import { getProjectInitialValues } from '../Admin/projectRecordInitialFormValue'
-import { H2, H4 } from '../../generic/text'
+import { H2 } from '../../generic/text'
 import { ContentPageLayout } from '../../Layout'
 import { ButtonPrimary } from '../../generic/buttons'
 import PageUnavailableOffline from '../PageUnavailableOffline'
 import { useOnlineStatus } from '../../../library/onlineStatusContext'
 import { useDatabaseSwitchboardInstance } from '../../../App/mermaidData/databaseSwitchboard/DatabaseSwitchboardContext'
 import { InputWrapper } from '../../generic/form'
+import {
+  Table,
+  Tr,
+  Th,
+  Td,
+  TableOverflowWrapper,
+} from '../../generic/Table/table'
 import { getDataSharingOptions } from '../../../library/getDataSharingOptions'
 import { IconInfo } from '../../icons'
 import theme from '../../../theme'
 import language from '../../../language'
 import DataSharingInfoModal from '../../DataSharingInfoModal'
 
-const TextStyleWrapper = styled.div`
-  padding: 0 10px;
-  border-left: 10px solid rgb(221, 220, 228);
-  margin-bottom: 20px;
-  margin-right: 20%;
+const DataSharingTable = styled(Table)`
+  td {
+    position: relative;
+    label {
+      position: absolute;
+      top: 0;
+      left: 0;
+      display: grid;
+      place-items: center;
+      width: 100%;
+      height: 100%;
+    }
+  }
 `
-
-const DataSelectGridWrapper = styled.div`
-  display: grid;
-  grid-template-columns: 2fr repeat(3, 1fr);
-  grid-template-rows: repeat(4, 1fr);
-  grid-row-gap: 5px;
-  margin: 20px 0;
-  margin-right: 20%;
-  border: 10px solid rgb(221, 220, 228);
-`
-
-const ItemGridStyle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-color: rgb(221, 220, 228);
-  align-items: center;
-  padding: 10px;
-`
-
-const InputItemGridStyle = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
-
-const RowItemGridStyle = styled.div`
-  display: flex;
-  align-items: center;
-  padding-left: 10px;
-`
-
 const CheckBoxLabel = styled.label`
   display: inline-block;
   input {
@@ -172,96 +155,118 @@ const DataSharing = () => {
       {(formik) => (
         <>
           <InputWrapper>
-            <H4>Data is much more powerful when shared</H4>
-            <TextStyleWrapper>
-              {language.pages.dataSharing.introductionParagraph}
-            </TextStyleWrapper>
+            <h3>Data is much more powerful when shared.</h3>
+            <p>{language.pages.dataSharing.introductionParagraph}</p>
             <ButtonPrimary type="button" onClick={openDataSharingInfoModal}>
-              <IconInfo /> Learn more about how your data is shared
+              <IconInfo /> Learn more about how your data is shared...
             </ButtonPrimary>
-            <DataSelectGridWrapper>
-              <ItemGridStyle />
-              <ItemGridStyle>
-                <ContentWithTooltip
-                  tooltipText={findToolTipDescription('Private')}
-                  ariaLabelledBy="private-tooltip"
-                >
-                  Private
-                </ContentWithTooltip>
-              </ItemGridStyle>
-              <ItemGridStyle>
-                <ContentWithTooltip
-                  tooltipText={findToolTipDescription('Public Summary')}
-                  ariaLabelledBy="public-summary-tooltip"
-                >
-                  Public Summary
-                </ContentWithTooltip>
-              </ItemGridStyle>
-              <ItemGridStyle>
-                <ContentWithTooltip
-                  tooltipText={findToolTipDescription('Public')}
-                  ariaLabelledBy="public-tooltip"
-                >
-                  Public
-                </ContentWithTooltip>
-              </ItemGridStyle>
-              <RowItemGridStyle>Fish Belt</RowItemGridStyle>
-              {dataPolicyOptions.map((item) => (
-                <InputItemGridStyle key={item.value}>
-                  <input
-                    type="radio"
-                    value={item.value}
-                    checked={
-                      formik.getFieldProps('data_policy_beltfish').value ===
-                      item.value
-                    }
-                    onChange={(event) => {
-                      formik.setFieldValue(
-                        'data_policy_beltfish',
-                        parseInt(event.target.value, 10),
-                      )
-                    }}
-                  />
-                </InputItemGridStyle>
-              ))}
-              <RowItemGridStyle>
-                Benthic: PIT, LIT, and Habitat Complexity
-              </RowItemGridStyle>
-              {dataPolicyOptions.map((item) => (
-                <InputItemGridStyle key={item.value}>
-                  <input
-                    type="radio"
-                    value={item.value}
-                    checked={
-                      formik.getFieldProps('data_policy_benthiclit').value ===
-                      item.value
-                    }
-                    onChange={(event) =>
-                      handleBenthicPolicyChange(event, formik)
-                    }
-                  />
-                </InputItemGridStyle>
-              ))}
-              <RowItemGridStyle>Bleaching</RowItemGridStyle>
-              {dataPolicyOptions.map((item) => (
-                <InputItemGridStyle key={item.value}>
-                  <input
-                    type="radio"
-                    value={item.value}
-                    checked={
-                      formik.getFieldProps('data_policy_bleachingqc').value ===
-                      item.value
-                    }
-                    onChange={(event) => {
-                      formik.setFieldValue(
-                        'data_policy_bleachingqc',
-                        parseInt(event.target.value, 10),
-                      )
-                    }}
-                  />
-                </InputItemGridStyle>
-              ))}
-            </DataSelectGridWrapper>
+            <TableOverflowWrapper>
+              <DataSharingTable>
+                <thead>
+                  <Tr>
+                    <Th>&nbsp;</Th>
+                    <Th>
+                      <ContentWithTooltip
+                        tooltipText={findToolTipDescription('Private')}
+                        ariaLabelledBy="private-tooltip"
+                      >
+                        Private
+                      </ContentWithTooltip>
+                    </Th>
+                    <Th>
+                      <ContentWithTooltip
+                        tooltipText={findToolTipDescription('Public Summary')}
+                        ariaLabelledBy="public-summary-tooltip"
+                      >
+                        Public Summary
+                      </ContentWithTooltip>
+                    </Th>
+                    <Th>
+                      <ContentWithTooltip
+                        tooltipText={findToolTipDescription('Public')}
+                        ariaLabelledBy="public-tooltip"
+                      >
+                        Public
+                      </ContentWithTooltip>
+                    </Th>
+                  </Tr>
+                </thead>
+                <tbody>
+                  <Tr>
+                    <Td>Fish Belt</Td>
+                    {dataPolicyOptions.map((item) => (
+                      <Td key={item.value}>
+                        <label htmlFor={`fish-belt${item.value}`}>
+                          <input
+                            type="radio"
+                            value={item.value}
+                            name="fish-belt"
+                            id={`fish-belt${item.value}`}
+                            checked={
+                              formik.getFieldProps('data_policy_beltfish')
+                                .value === item.value
+                            }
+                            onChange={(event) => {
+                              formik.setFieldValue(
+                                'data_policy_beltfish',
+                                parseInt(event.target.value, 10),
+                              )
+                            }}
+                          />
+                        </label>
+                      </Td>
+                    ))}
+                  </Tr>
+                  <Tr>
+                    <Td>Benthic: PIT, LIT, and Habitat Complexity</Td>
+                    {dataPolicyOptions.map((item) => (
+                      <Td key={item.value}>
+                        <label htmlFor={`benthic${item.value}`}>
+                          <input
+                            type="radio"
+                            value={item.value}
+                            name="benthic"
+                            id={`benthic${item.value}`}
+                            checked={
+                              formik.getFieldProps('data_policy_benthiclit')
+                                .value === item.value
+                            }
+                            onChange={(event) =>
+                              handleBenthicPolicyChange(event, formik)
+                            }
+                          />
+                        </label>
+                      </Td>
+                    ))}
+                  </Tr>
+                  <Tr>
+                    <Td>Bleaching</Td>
+                    {dataPolicyOptions.map((item) => (
+                      <Td key={item.value}>
+                        <label htmlFor={`bleaching${item.value}`}>
+                          <input
+                            type="radio"
+                            name="bleaching"
+                            id={`bleaching${item.value}`}
+                            value={item.value}
+                            checked={
+                              formik.getFieldProps('data_policy_bleachingqc')
+                                .value === item.value
+                            }
+                            onChange={(event) => {
+                              formik.setFieldValue(
+                                'data_policy_bleachingqc',
+                                parseInt(event.target.value, 10),
+                              )
+                            }}
+                          />
+                        </label>
+                      </Td>
+                    ))}
+                  </Tr>
+                </tbody>
+              </DataSharingTable>
+            </TableOverflowWrapper>
             <CheckBoxLabel>
               <input id="test-project-toggle" type="checkbox" /> This is a test
               project
