d6a245261818b2955407c7abdc722b14f1647e5b
Melissa Nunes <mnunes@sparkgeo.com>
2021-07-21 16:17:52 -0600

    M214 hydrate app on all reloads, not just initial

--- src/App/initiallyHydrateOfflineStorageWithApiData.js
+++ src/App/initiallyHydrateOfflineStorageWithApiData.js
@@ -5,25 +5,15 @@ export const initiallyHydrateOfflineStorageWithApiData = (dexieInstance) => {
    * Currently we only have fish species, and collect records being loaded into storage here
    * (so that suggesting a new species will work now and will work later with minimal refactor),
    * but the rest will come as we transition away
-   * from using mocked data and mature the archetecture*/
+   * from using mocked data and mature the architecture*/
+
 
   return dexieInstance.transaction(
     'rw',
     dexieInstance.fishSpecies,
-    dexieInstance.hasInitialApiHydrationCompleted,
     dexieInstance.collectRecords,
     async () => {
-      const isAlreadyInitialiazed = !!(
-        await dexieInstance.hasInitialApiHydrationCompleted.toArray()
-      ).length
-
-      if (!isAlreadyInitialiazed) {
-        initiallyHydrateOfflineStorageWithMockData(dexieInstance)
-
-        dexieInstance.hasInitialApiHydrationCompleted.put({
-          id: 'enforceOnlyOneRecordEverStoredAndOverwritten',
-        })
-      }
+      initiallyHydrateOfflineStorageWithMockData(dexieInstance)
     },
   )
 }
