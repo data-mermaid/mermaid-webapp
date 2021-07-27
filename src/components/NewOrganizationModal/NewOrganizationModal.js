b0ea48b50eef53790a96059bea6ffdf1468a3c80
Melissa Nunes <mnunes@sparkgeo.com>
2021-07-27 10:36:22 -0600

    Fixed linting thingy

--- src/components/NewOrganizationModal/NewOrganizationModal.js
+++ src/components/NewOrganizationModal/NewOrganizationModal.js
@@ -37,7 +37,7 @@ const NewOrganizationModal = ({ isOpen, onDismiss, onSubmit }) => {
   const modalContent = (
     <>
       <ModalInputRow>
-        // eslint-disable-next-line jsx-a11y/label-has-associated-control
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
         <label id={`aria-label${id}`} htmlFor={id}>
           New Organization Name
         </label>
