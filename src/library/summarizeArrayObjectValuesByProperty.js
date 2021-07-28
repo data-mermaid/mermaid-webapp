40a729fe4b8d8442c78a5c827ba78462bd4fbd5c
tienhoah <nickhoang11@gmail.com>
2021-07-28 11:41:53 -0700

    Add missing file from previous commit.

--- src/library/summarizeArrayObjectValuesByProperty.js
+++ src/library/summarizeArrayObjectValuesByProperty.js
@@ -1,14 +1,14 @@
 export const summarizeArrayObjectValuesByProperty = (
- arrayOfObjects,
- objectPropertyName,
+  arrayOfObjects,
+  objectPropertyName,
 ) => {
- const summaryReducer = (accumulator, object) => {
-   const property = object[objectPropertyName]
-     ? parseFloat(object[objectPropertyName])
-     : 0
+  const summaryReducer = (accumulator, object) => {
+    const property = object[objectPropertyName]
+      ? parseFloat(object[objectPropertyName])
+      : 0
 
-   return accumulator + property
- }
+    return accumulator + property
+  }
 
- return arrayOfObjects.reduce(summaryReducer, 0)
+  return arrayOfObjects.reduce(summaryReducer, 0)
 }
