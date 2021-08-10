db3ea737e2c57a8f89d1e340373d403bf259bb4a
Alan Leonard <aleonard@sparkgeo.com>
2021-08-10 14:55:15 -0400

    rejig and remove styles for better syntax

--- src/components/generic/InputAutocomplete/InputAutocomplete.styles.js
+++ src/components/generic/InputAutocomplete/InputAutocomplete.styles.js
@@ -18,10 +18,9 @@ export const Menu = styled('ul')`
   color: ${theme.color.textColor};
   z-index: 99;
   top: 4rem;
-`(({ isOpen }) => ({
-  border: isOpen ? null : 'none',
-  outline: isOpen ? null : 'none',
-}))
+  border: ${(props) => (props.isOpen ? null : 'none')};
+  outline: ${(props) => (props.isOpen ? null : 'none')};
+`
 
 export const Item = styled('li')`
   position: relative;
@@ -32,20 +31,4 @@ export const Item = styled('li')`
   padding: ${theme.spacing.buttonPadding};
   white-space: normal;
   word-wrap: normal;
-`(({ isActive, isSelected }) => {
-  const styles = []
-
-  if (isActive) {
-    // this is also hover
-    styles.push({
-      background: `${theme.color.secondaryHover}`,
-    })
-  }
-  if (isSelected) {
-    styles.push({
-      fontWeight: '700',
-    })
-  }
-
-  return styles
-})
+`
