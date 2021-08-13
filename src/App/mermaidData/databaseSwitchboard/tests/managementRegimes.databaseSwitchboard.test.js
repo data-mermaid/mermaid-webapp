9c41534d049b5c5d8b9fea0759811866462ef937
Melissa Nunes <mnunes@sparkgeo.com>
2021-08-13 11:22:31 -0600

    M313 make getting project management regimes only return those related to a project

---
+++ src/App/mermaidData/databaseSwitchboard/tests/managementRegimes.databaseSwitchboard.test.js
@@ -0,0 +1,35 @@
+import { getDatabaseSwitchboardInstanceAuthenticatedOnlineDexieSuccess } from './testHelpers.DatabseSwitchboard'
+
+test('getManagementRegimes only returns profiles for a given project', async () => {
+  const dbSwitchboardInstance = getDatabaseSwitchboardInstanceAuthenticatedOnlineDexieSuccess()
+
+  await dbSwitchboardInstance.dexieInstance.project_managements.bulkPut([
+    { id: 'foo', project: '5' },
+    { id: 'bar', project: '5' },
+    { id: 'baz', project: 'shouldGetFilteredOut' },
+  ])
+  const managementRegimes = await dbSwitchboardInstance.getManagementRegimes(
+    '5',
+  )
+
+  expect(managementRegimes.length).toEqual(2)
+  expect(
+    managementRegimes.find(
+      (profile) => profile.project === 'shouldGetFilteredOut',
+    ),
+  ).toBeUndefined()
+})
+
+test('getManagementRegime only returns profiles for a given project', async () => {
+  const dbSwitchboardInstance = getDatabaseSwitchboardInstanceAuthenticatedOnlineDexieSuccess()
+
+  await dbSwitchboardInstance.dexieInstance.project_managements.bulkPut([
+    { id: 'foo', project: 'shouldGetFilteredOut' },
+  ])
+  const managementRegime = await dbSwitchboardInstance.getManagementRegime({
+    id: 'foo',
+    projectId: '5',
+  })
+
+  expect(managementRegime).toBeUndefined()
+})
