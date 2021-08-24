ad5f19c98aa6e8ed5cc8c5a9f765b37a7f81db3d
Melissa Nunes <mnunes@sparkgeo.com>
2021-08-24 16:31:33 -0600

    M324 decouple getting user profile from db switchboard instance.

--- src/App/mermaidData/useCurrentUser.js
+++ src/App/mermaidData/useCurrentUser.js
@@ -1,16 +1,33 @@
 import { useEffect, useState } from 'react'
 import { toast } from 'react-toastify'
 import language from '../../language'
+import getUserProfile from '../getUserProfile'
 
-export const useCurrentUser = ({ databaseSwitchboardInstance }) => {
+export const useCurrentUser = ({
+  apiBaseUrl,
+  auth0Token,
+  dexieInstance,
+  isMermaidAuthenticated,
+  isOnline,
+}) => {
   const [currentUser, setCurrentUser] = useState()
 
   const _initializeUserOnAuthentication = useEffect(() => {
     let isMounted = true
 
-    if (databaseSwitchboardInstance) {
-      databaseSwitchboardInstance
-        .getUserProfile()
+    if (
+      isMermaidAuthenticated &&
+      apiBaseUrl &&
+      dexieInstance &&
+      isMermaidAuthenticated
+    ) {
+      getUserProfile({
+        apiBaseUrl,
+        auth0Token,
+        dexieInstance,
+        isMermaidAuthenticated,
+        isOnline,
+      })
         .then((user) => {
           if (isMounted && user) {
             setCurrentUser(user)
@@ -24,7 +41,7 @@ export const useCurrentUser = ({ databaseSwitchboardInstance }) => {
     return () => {
       isMounted = false
     }
-  }, [databaseSwitchboardInstance])
+  }, [apiBaseUrl, auth0Token, dexieInstance, isMermaidAuthenticated, isOnline])
 
   return currentUser
 }
