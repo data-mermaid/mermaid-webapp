9980c9831cc43a70af868bae3733372fc513b1f2
tienhoah <nickhoang11@gmail.com>
2021-09-02 16:39:01 -0700

    Fix linting

--- src/components/pages/collectRecordFormPages/SaveValidateSubmitButtonGroup/SaveValidateSubmitButtonGroup.js
+++ src/components/pages/collectRecordFormPages/SaveValidateSubmitButtonGroup/SaveValidateSubmitButtonGroup.js
@@ -1,5 +1,5 @@
 import { useFormikContext } from 'formik'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 
