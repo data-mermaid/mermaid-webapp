5ea5cfa5d5035db04ab672bad106575ef4530ff9
Melissa Nunes <mnunes@sparkgeo.com>
2021-09-23 15:37:28 -0600

    M103 test fix

--- src/App/integrationTests/App.toggleOffline.test.js
+++ src/App/integrationTests/App.toggleOffline.test.js
@@ -4,39 +4,35 @@ import userEvent from '@testing-library/user-event'
 import { initiallyHydrateOfflineStorageWithMockData } from '../../testUtilities/initiallyHydrateOfflineStorageWithMockData'
 import { getMockDexieInstanceAllSuccess } from '../../testUtilities/mockDexie'
 import {
-  renderAuthenticatedOnline,
+  renderAuthenticated,
   screen,
+  within,
 } from '../../testUtilities/testingLibraryWithHelpers'
 import App from '../App'
 
-test('Toggle offline switch on, page turn offline, some navigation links will disappear', async () => {
+test('Toggle offline switch to offline, page turns offline, some navigation links will disappear', async () => {
   const dexieInstance = getMockDexieInstanceAllSuccess()
 
   await initiallyHydrateOfflineStorageWithMockData(dexieInstance)
 
-  renderAuthenticatedOnline(<App dexieInstance={dexieInstance} />, {
+  renderAuthenticated(<App dexieInstance={dexieInstance} />, {
     initialEntries: ['/projects/5/collecting/'],
   })
 
-  // const sideNav = await screen.findByTestId('content-page-side-nav')
+  const sideNav = await screen.findByTestId('content-page-side-nav')
 
   // Trying both test queries below, comment one and uncomment another.
   const offlineToggleSwitchTestId = screen.getByTestId(
     'offline-toggle-switch-test',
   )
-  // const offlineToggleSwitchByLabel = screen.getByLabelText(
-  //   'offline-toggle-switch-label',
-  // )
 
   userEvent.click(offlineToggleSwitchTestId)
 
-  // This shows the page is still ONLINE, because test will not pass, comment out for now.
-  // expect(
-  //   await screen.findByTestId('offline-toggle-switch-label'),
-  // ).toHaveTextContent("You're OFFLINE. Some contents may be out of date.")
+  expect(
+    await screen.findByTestId('offline-toggle-switch-label'),
+  ).toHaveTextContent("You're OFFLINE. Some contents may be out of date.")
 
-  // This submitted nav link should not be in document, because test will not pass, comment out for now.
-  // expect(
-  //   await within(sideNav).findByRole('link', { name: /submitted/i }),
-  // ).not.toBeInTheDocument()
+  expect(
+    within(sideNav).queryByRole('link', { name: 'Submitted' }),
+  ).not.toBeInTheDocument()
 })
--- src/components/OfflineToggle/OfflineToggle.js
+++ src/components/OfflineToggle/OfflineToggle.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useOnlineStatus } from '../../library/onlineStatusContext'
 
-const OfflineToggle = () => {
+const OfflineToggle = (props) => {
   const {
     isAppOnline,
     canUserOverrideOnlineStatus,
@@ -9,17 +9,14 @@ const OfflineToggle = () => {
   } = useOnlineStatus()
 
   return (
-    <label id="offline-toggle-label" htmlFor="offline-toggle-switch">
-      <input
-        id="offline-toggle-switch"
-        data-testid="offline-toggle-switch-test"
-        aria-label="offline-toggle-switch-label"
-        type="checkbox"
-        onChange={toggleUserOnlineStatusOverride}
-        checked={!isAppOnline}
-        disabled={!canUserOverrideOnlineStatus}
-      />
-    </label>
+    <input
+      {...props}
+      data-testid="offline-toggle-switch-test"
+      type="checkbox"
+      onChange={toggleUserOnlineStatusOverride}
+      checked={!isAppOnline}
+      disabled={!canUserOverrideOnlineStatus}
+    />
   )
 }
 
