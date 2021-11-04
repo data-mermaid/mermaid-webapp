f0d27fa0416b3daf270dfbb2c87468004237a5d9
Alan Leonard <aleonard@sparkgeo.com>
2021-11-04 11:59:16 -0400

    missed some comments and props

--- src/components/pages/collectRecordFormPages/FishBelt/FishBelt.js
+++ src/components/pages/collectRecordFormPages/FishBelt/FishBelt.js
@@ -45,11 +45,6 @@ import RecordFormTitle from '../../../RecordFormTitle'
 /*
   Fishbelt component lets a user edit and delete a record as well as create a new record.
 */
-// div {
-//   background: olivedrab;
-//   position: sticky;
-//   top: ${theme.spacing.headerHeight};
-// }
 const CollectRecordToolbarWrapper = styled('div')`
   display: flex;
   justify-content: space-between;
@@ -502,10 +497,12 @@ const FishBelt = ({ isNewRecord, currentUser }) => {
 FishBelt.propTypes = {
   currentUser: currentUserPropType.isRequired,
   isNewRecord: PropTypes.bool,
+  isToolbarSticky: PropTypes.bool,
 }
 
 FishBelt.defaultProps = {
   isNewRecord: true,
+  isToolbarSticky: false,
 }
 
 export default FishBelt
