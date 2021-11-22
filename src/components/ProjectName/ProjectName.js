e66ebf4e61983692d927623bff442324aa2e7adf
Al Leonard <aleonard@sparkgeo.com>
2021-11-22 16:22:48 -0500

    simplified project name, lost home button

--- src/components/ProjectName/ProjectName.js
+++ src/components/ProjectName/ProjectName.js
@@ -2,27 +2,17 @@ import { useParams } from 'react-router-dom'
 import React, { useState, useEffect } from 'react'
 import styled, { css } from 'styled-components/macro'
 import theme from '../../theme'
-import { H2 } from '../generic/text'
 import {
   mediaQueryTabletLandscapeOnly,
   mediaQueryPhoneOnly,
 } from '../../library/styling/mediaQueries'
-import { NavLinkThatLooksLikeButtonIcon } from '../generic/links'
-import { IconHome } from '../icons'
 import { useDatabaseSwitchboardInstance } from '../../App/mermaidData/databaseSwitchboard/DatabaseSwitchboardContext'
 import useIsMounted from '../../library/useIsMounted'
 
 const ProjectNameWrapper = styled('div')`
   background: ${theme.color.white};
-  display: grid;
-  grid-template-columns: auto 1fr;
-  align-items: center;
   h2 {
-    overflow-x: hidden;
-    text-overflow: ellipsis;
-    white-space: nowrap;
-    padding: ${theme.spacing.medium} 0;
-    height: 100%;
+    padding: ${theme.spacing.medium};
     margin: 0;
   }
   a {
@@ -43,7 +33,6 @@ const ProjectNameWrapper = styled('div')`
   ${mediaQueryPhoneOnly(css`
     grid-template-columns: ${props => props.theme.spacing.sideNavWidthPhoneOnly} 1fr;
     h2 {
-      font-size: smaller;
       padding: ${theme.spacing.small} ${theme.spacing.xsmall} ${theme.spacing.small} 0;
     }
     a {
@@ -55,7 +44,6 @@ const ProjectNameWrapper = styled('div')`
     }
   `)}
 `
-// const StyledIconHome = styled(IconHome)``
 const ProjectName = () => {
   const [projectName, setProjectName] = useState('')
   const isMounted = useIsMounted()
@@ -74,10 +62,7 @@ const ProjectName = () => {
 
   return (
     <ProjectNameWrapper>
-      <NavLinkThatLooksLikeButtonIcon to="/">
-        <IconHome />
-      </NavLinkThatLooksLikeButtonIcon>
-      <H2>{projectName}</H2>
+      <h2>{projectName}</h2>
     </ProjectNameWrapper>
   )
 }
