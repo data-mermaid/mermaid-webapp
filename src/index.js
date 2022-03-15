e400d35c7643a75dff3e44014d65f706c005e15c
tienhoah <nickhoang11@gmail.com>
2022-03-15 15:54:05 -0700

    Move maplibre map style to global styles, and add map pop up styles.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './index.css'
-import 'maplibre-gl/dist/maplibre-gl.css'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
--- src/library/styling/globalStyles.js
+++ src/library/styling/globalStyles.js
@@ -6,9 +6,11 @@ import '@fontsource/open-sans'
 import '@fontsource/open-sans/700.css'
 
 const toastifyCss = raw('react-toastify/dist/ReactToastify.css')
+const maplibreglCss = raw('maplibre-gl/dist/maplibre-gl.css')
 
 const GlobalStyle = createGlobalStyle`
     ${toastifyCss}
+    ${maplibreglCss}
     :root {
         font-size: 62.5%;
     }
@@ -27,8 +29,8 @@ const GlobalStyle = createGlobalStyle`
 
     }
     svg {
-        width: ${props => props.theme.typography.defaultIconSize};
-        height: ${props => props.theme.typography.defaultIconSize};
+        width: ${(props) => props.theme.typography.defaultIconSize};
+        height: ${(props) => props.theme.typography.defaultIconSize};
     }
     *,*::before,*::after {
         box-sizing: border-box;
@@ -38,6 +40,32 @@ const GlobalStyle = createGlobalStyle`
         ${hoverState(css`
           text-decoration: none;
         `)}
+    }
+    /* mapbox popup content style */
+    .mapboxgl-popup {
+        border-radius: 6px;
+    }
+
+    .mapboxgl-popup-content {
+        padding: 7px;
+    }
+
+    .mapboxgl-popup-content a {
+        text-decoration: none;
+        font-weight: bold;
+    }
+
+    .mapboxgl-popup-content div {
+        border-top: 1px solid;
+    }
+
+    .mapboxgl-popup-content div p {
+        margin: 0;
+    }
+
+    .mapboxgl-popup-content div p span {
+        margin: 0;
+        font-style: italic;
     }
 `
 
