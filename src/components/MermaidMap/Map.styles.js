4efd3700c2f3c445635c4a141d726795240a5d5d
tienhoah <nickhoang11@gmail.com>
2022-03-15 15:56:19 -0700

    Separate map styles to its own file

---
+++ src/components/MermaidMap/Map.styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components/macro'
+import { InputRow } from '../generic/form'
+
+export const MapInputRow = styled(InputRow)`
+  grid-template-columns: 0.75fr 2.5fr;
+`
+export const MapContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+`
+export const MapWrapper = styled.div`
+  height: 100%;
+  min-height: 70vh;
+`
