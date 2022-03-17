8cebb1997d95a9d993d7bc62e1c1e2ac8a0acf14
tienhoah <nickhoang11@gmail.com>
2022-03-17 11:25:10 -0700

    Debugging map marker click.

--- src/components/ProjectSitesMap/ProjectSitesMap.js
+++ src/components/ProjectSitesMap/ProjectSitesMap.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react'
-import ReactDOM from 'react-dom'
+// import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import maplibregl from 'maplibre-gl'
 import AtlasLegendDrawer from '../AtlasLegendDrawer'
