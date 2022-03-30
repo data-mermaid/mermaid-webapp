b237a36f860d4ec0723cfc982e664bd11a3c3a34
Joe Burkinshaw <1443875+JBurkinshaw@users.noreply.github.com>
2022-03-29 18:25:45 -0700

    Add title to dependency array Add JS doc comment including caveat.

--- src/library/useDocumentTitle.js
+++ src/library/useDocumentTitle.js
@@ -1,6 +1,11 @@
-// useDocumentTitle.js
 import { useEffect } from 'react'
 
+/**
+ * Custom hook to apply a new title to the document with document.title.
+ * Will remember the previous page title which will be re-applied when the component is unmounted.
+ * NOTE: There are some potential pitfalls to this approach which are documented in this comment: https://github.com/data-mermaid/mermaid-webapp/pull/412#issuecomment-1081321094
+ * @param {string} title The title to apply to the document
+ */
 const useDocumentTitle = (title) => {
   useEffect(() => {
     const prevTitle = document.title
@@ -10,7 +15,7 @@ const useDocumentTitle = (title) => {
     return () => {
       document.title = prevTitle
     }
-  })
+  }, [title])
 }
 
 export default useDocumentTitle
