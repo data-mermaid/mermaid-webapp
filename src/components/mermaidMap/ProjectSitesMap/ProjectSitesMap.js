b81220d45efdddf0c2bbd6365640ea96830caf3d
tienhoah <nickhoang11@gmail.com>
2022-04-17 00:51:16 -0700

    Fix linting errors.

--- src/components/mermaidMap/ProjectSitesMap/ProjectSitesMap.js
+++ src/components/mermaidMap/ProjectSitesMap/ProjectSitesMap.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useCallback } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import maplibregl from 'maplibre-gl'
