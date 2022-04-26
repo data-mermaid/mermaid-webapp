9905e2574cc40514c6dad725c35e10178fcf6654
Mel Nunes <nunes.melissa.m@gmail.com>
2022-04-26 13:46:57 -0600

    [M611] fix: make sure null sample time does not show up as 'null', but rather just nothing.

--- src/components/SubmittedFishBeltInfoTable/SubmittedFishBeltInfoTable.js
+++ src/components/SubmittedFishBeltInfoTable/SubmittedFishBeltInfoTable.js
@@ -55,7 +55,7 @@ const SubmittedFishBeltInfoTable = ({ sites, managementRegimes, choices, submitt
       <tbody>
         <TableRowItem title="Site" options={sites} value={site} />
         <TableRowItem title="Management" options={managementRegimes} value={management} />
-        <TableRowItem title="Sample Date Time" value={`${sample_date} ${sample_time}`} />
+        <TableRowItem title="Sample Date Time" value={`${sample_date} ${sample_time || ''}`} />
         <TableRowItem title="Depth" value={depth} />
         <TableRowItem title="Transect Number" value={number} />
         <TableRowItem title="Label" value={label} />
