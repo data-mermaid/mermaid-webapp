515f1acc4eec05904888ef6e30ba76361e8620d7
Mel Nunes <nunes.melissa.m@gmail.com>
2022-04-27 13:18:31 -0600

    [M615] Add LinkLooksLikeButtonSecondary component

--- src/components/generic/buttons.stories.js
+++ src/components/generic/buttons.stories.js
@@ -1,29 +1,31 @@
 import styled from 'styled-components/macro'
 import React from 'react'
-import { ButtonCallout, ButtonCaution, ButtonPrimary, ButtonSecondary } from './buttons'
 
-const ButtonWrapper = styled.div`
-  p,
-  button {
-    margin: 10px;
-    display: block;
-  }
-`
+import {
+  ButtonCallout,
+  ButtonCaution,
+  ButtonPrimary,
+  ButtonSecondary,
+  LinkLooksLikeButtonSecondary,
+} from './buttons'
 
 export default {
   title: 'Generic Buttons',
 }
 
 export const Basic = () => (
-  <ButtonWrapper>
+  <>
     <ButtonPrimary>Button Primary</ButtonPrimary>
     <ButtonSecondary>Button Secondary</ButtonSecondary>
     <ButtonCallout>Button Callout</ButtonCallout>
     <ButtonCaution>Button Caution</ButtonCaution>
+    <LinkLooksLikeButtonSecondary>
+      Link that looks like Button Secondary
+    </LinkLooksLikeButtonSecondary>
     <p>Disabled buttons:</p>
     <ButtonPrimary disabled>Button Primary</ButtonPrimary>
     <ButtonSecondary disabled>Button Secondary</ButtonSecondary>
     <ButtonCallout disabled>Button Callout</ButtonCallout>
     <ButtonCaution disabled>Button Caution</ButtonCaution>
-  </ButtonWrapper>
+  </>
 )
