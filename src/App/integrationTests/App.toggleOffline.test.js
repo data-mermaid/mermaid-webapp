87c8fb8a9fc7cb721187fed3272fb782d923ec47
Joe Burkinshaw <1443875+JBurkinshaw@users.noreply.github.com>
2022-05-03 15:51:05 -0700

    Fix App.toggleOffline.test.js tests Add waitFor() where relevant

--- src/App/integrationTests/App.toggleOffline.test.js
+++ src/App/integrationTests/App.toggleOffline.test.js
@@ -1,59 +1,62 @@
 import '@testing-library/jest-dom/extend-expect'
 import { rest } from 'msw'
 import React from 'react'
-// import userEvent from '@testing-library/user-event'
-// import { initiallyHydrateOfflineStorageWithMockData } from '../../testUtilities/initiallyHydrateOfflineStorageWithMockData'
+import userEvent from '@testing-library/user-event'
+import { initiallyHydrateOfflineStorageWithMockData } from '../../testUtilities/initiallyHydrateOfflineStorageWithMockData'
 import { getMockDexieInstanceAllSuccess } from '../../testUtilities/mockDexie'
 import {
   mockMermaidApiAllSuccessful,
   renderAuthenticated,
-  // waitForElementToBeRemoved,
+  waitForElementToBeRemoved,
   screen,
-  // within,
+  within,
+  waitFor
 } from '../../testUtilities/testingLibraryWithHelpers'
 import App from '../App'
 
 const apiBaseUrl = process.env.REACT_APP_MERMAID_API
 
-// test('Starting ONLINE - Toggle is checked and switched to OFFLINE, some navigation links will disappear. Then navigation links reappear after clicking toggle again', async () => {
-//   const dexieInstance = getMockDexieInstanceAllSuccess()
+test('Starting ONLINE - Toggle is checked and switched to OFFLINE, some navigation links will disappear. Then navigation links reappear after clicking toggle again', async () => {
+  const dexieInstance = getMockDexieInstanceAllSuccess()
 
-//   await initiallyHydrateOfflineStorageWithMockData(dexieInstance)
+  await initiallyHydrateOfflineStorageWithMockData(dexieInstance)
 
-//   renderAuthenticated(<App dexieInstance={dexieInstance} />, {
-//     initialEntries: ['/projects/5/collecting/'],
-//   })
+  renderAuthenticated(<App dexieInstance={dexieInstance} />, {
+    initialEntries: ['/projects/5/collecting/'],
+  })
 
-//   const sideNav = await screen.findByTestId('content-page-side-nav')
+  const sideNav = await screen.findByTestId('content-page-side-nav')
 
-//   const offlineToggleSwitchTestIdBeforeFirstClick = screen.getByTestId('offline-toggle-switch-test')
+  const offlineToggleSwitchTestIdBeforeFirstClick = screen.getByTestId('offline-toggle-switch-test')
 
-//   userEvent.click(offlineToggleSwitchTestIdBeforeFirstClick)
+  userEvent.click(offlineToggleSwitchTestIdBeforeFirstClick)
 
-//   expect(await screen.findByTestId('offline-toggle-switch-label')).toHaveTextContent(
-//     "You're OFFLINE. Some contents may be out of date.",
-//   )
+  expect(await screen.findByTestId('offline-toggle-switch-label')).toHaveTextContent(
+    "You're OFFLINE. Some contents may be out of date.",
+  )
 
-//   expect(within(sideNav).queryByRole('link', { name: 'Submitted' })).not.toBeInTheDocument()
-//   expect(within(sideNav).queryByRole('link', { name: 'Project Info' })).not.toBeInTheDocument()
-//   expect(within(sideNav).queryByRole('link', { name: 'Users' })).not.toBeInTheDocument()
-//   expect(within(sideNav).queryByRole('link', { name: 'Data Sharing' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Submitted' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Project Info' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Users' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Data Sharing' })).not.toBeInTheDocument()
 
-//   const offlineToggleSwitchTestIdAfterFirstClick = screen.getByTestId('offline-toggle-switch-test')
+  const offlineToggleSwitchTestIdAfterFirstClick = screen.getByTestId('offline-toggle-switch-test')
 
-//   userEvent.click(offlineToggleSwitchTestIdAfterFirstClick)
+  userEvent.click(offlineToggleSwitchTestIdAfterFirstClick)
 
-//   await waitForElementToBeRemoved(() => screen.queryByLabelText('project pages loading indicator'))
+  await waitForElementToBeRemoved(() => screen.queryByLabelText('project pages loading indicator'))
 
-//   expect(await screen.findByTestId('offline-toggle-switch-label')).toHaveTextContent(
-//     "You're ONLINE",
-//   )
+  expect(await screen.findByTestId('offline-toggle-switch-label')).toHaveTextContent(
+    "You're ONLINE",
+  )
 
-//   expect(within(sideNav).queryByRole('link', { name: 'Submitted' })).toBeInTheDocument()
-//   expect(within(sideNav).queryByRole('link', { name: 'Project Info' })).toBeInTheDocument()
-//   expect(within(sideNav).queryByRole('link', { name: 'Users' })).toBeInTheDocument()
-//   expect(within(sideNav).queryByRole('link', { name: 'Data Sharing' })).toBeInTheDocument()
-// })
+  waitFor(() => {
+    expect(within(sideNav).queryByRole('link', { name: 'Submitted' })).toBeInTheDocument()
+    expect(within(sideNav).queryByRole('link', { name: 'Project Info' })).toBeInTheDocument()
+    expect(within(sideNav).queryByRole('link', { name: 'Users' })).toBeInTheDocument()
+    expect(within(sideNav).queryByRole('link', { name: 'Data Sharing' })).toBeInTheDocument()
+  })
+})
 
 test('Navigator online - Toggle switch is not checked, and is enabled', async () => {
   jest.spyOn(navigator, 'onLine', 'get').mockReturnValue(true)
@@ -92,8 +95,10 @@ test('Server is unreachable - Toggle switch is not checked, and is enabled', asy
   )
   renderAuthenticated(<App dexieInstance={getMockDexieInstanceAllSuccess()} />)
 
-  const offlineToggleSwitchTestId = await screen.findByTestId('offline-toggle-switch-test')
+  waitFor(() => {
+    const offlineToggleSwitchTestId = screen.findByTestId('offline-toggle-switch-test')
 
-  expect(offlineToggleSwitchTestId).toBeChecked()
-  expect(offlineToggleSwitchTestId).toBeDisabled()
+    expect(offlineToggleSwitchTestId).toBeChecked()
+    expect(offlineToggleSwitchTestId).toBeDisabled()
+  })
 })
