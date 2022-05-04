2f69af6f555fdcc4f34a992fb82447e6c6b69334
tienhoah <nickhoang11@gmail.com>
2022-05-04 14:08:15 -0700

    [M644] Add table data, pagination to table.

--- src/components/pages/UsersAndTransects/UsersAndTransects.js
+++ src/components/pages/UsersAndTransects/UsersAndTransects.js
@@ -1,18 +1,23 @@
 import { toast } from 'react-toastify'
 
 import React, { useState, useMemo, useCallback, useEffect } from 'react'
-import { useTable } from 'react-table'
+import { usePagination, useSortBy, useTable } from 'react-table'
 import { useParams } from 'react-router-dom'
 import { H2 } from '../../generic/text'
 import { getToastArguments } from '../../../library/getToastArguments'
-import { Table, Tr, Th, TableOverflowWrapper } from '../../generic/Table/table'
+import { Table, Tr, Th, Td, TableOverflowWrapper, TableNavigation } from '../../generic/Table/table'
 import { ContentPageLayout } from '../../Layout'
 import PageUnavailableOffline from '../PageUnavailableOffline'
 import { useOnlineStatus } from '../../../library/onlineStatusContext'
 import { useSyncStatus } from '../../../App/mermaidData/syncApiDataIntoOfflineStorage/SyncStatusContext'
 import { useDatabaseSwitchboardInstance } from '../../../App/mermaidData/databaseSwitchboard/DatabaseSwitchboardContext'
 import useIsMounted from '../../../library/useIsMounted'
+import usePersistUserTablePreferences from '../../generic/Table/usePersistUserTablePreferences'
 import language from '../../../language'
+import { reactTableNaturalSort } from '../../generic/Table/reactTableNaturalSort'
+import PageSelector from '../../generic/Table/PageSelector'
+import PageSizeSelector from '../../generic/Table/PageSizeSelector'
+import { useCurrentUser } from '../../../App/CurrentUserContext'
 
 const UsersAndTransects = () => {
   const { isAppOnline } = useOnlineStatus()
@@ -22,13 +27,19 @@ const UsersAndTransects = () => {
   const { isSyncInProgress } = useSyncStatus()
   const isMounted = useIsMounted()
   const [observerProfiles, setObserverProfiles] = useState([])
+  const [submittedRecords, setSubmittedRecords] = useState([])
+  const currentUser = useCurrentUser()
 
   const _getSupportingData = useEffect(() => {
     if (databaseSwitchboardInstance && projectId && !isSyncInProgress) {
-      Promise.all([databaseSwitchboardInstance.getProjectProfiles(projectId)])
-        .then(([projectProfilesResponse]) => {
+      Promise.all([
+        databaseSwitchboardInstance.getProjectProfiles(projectId),
+        databaseSwitchboardInstance.getRecordsForUsersAndTransectsTable(projectId),
+      ])
+        .then(([projectProfilesResponse, submittedRecordsResponse]) => {
           if (isMounted.current) {
             setObserverProfiles(projectProfilesResponse)
+            setSubmittedRecords(submittedRecordsResponse)
             setIsLoading(false)
           }
         })
@@ -44,14 +55,23 @@ const UsersAndTransects = () => {
     })
   }, [observerProfiles])
 
+  const tableCellData = useMemo(
+    () =>
+      submittedRecords.map((record) => ({
+        site: record.site_name,
+        method: record.method,
+      })),
+    [submittedRecords],
+  )
+
   const tableColumns = useMemo(
     () => [
       {
         Header: () => null,
         id: 'Transect Header',
         columns: [
-          { Header: 'Site' },
-          { Header: 'Method' },
+          { Header: 'Site', accessor: 'site', sortType: reactTableNaturalSort },
+          { Header: 'Method', accessor: 'method', sortType: reactTableNaturalSort },
           { Header: 'Submitted Transect Number' },
         ],
       },
@@ -71,27 +91,113 @@ const UsersAndTransects = () => {
     ],
     [getUserColumnHeaders],
   )
-  const { getTableProps, headerGroups } = useTable({
-    columns: tableColumns,
-    data: [],
+
+  const tableDefaultPrefs = useMemo(() => {
+    return {
+      sortBy: [
+        {
+          id: 'site',
+          desc: false,
+        },
+      ],
+    }
+  }, [])
+
+  const [tableUserPrefs, handleSetTableUserPrefs] = usePersistUserTablePreferences({
+    key: `${currentUser.id}-usersAndTransectsTable`,
+    defaultValue: tableDefaultPrefs,
   })
 
+  const {
+    canNextPage,
+    canPreviousPage,
+    getTableBodyProps,
+    getTableProps,
+    gotoPage,
+    headerGroups,
+    nextPage,
+    page,
+    pageOptions,
+    prepareRow,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize, sortBy },
+  } = useTable(
+    {
+      columns: tableColumns,
+      data: tableCellData,
+      initialState: {
+        pageSize: 15,
+        sortBy: tableUserPrefs.sortBy,
+      },
+    },
+    useSortBy,
+    usePagination,
+  )
+
+  const handleRowsNumberChange = (e) => setPageSize(Number(e.target.value))
+
+  const _setSortByPrefs = useEffect(() => {
+    handleSetTableUserPrefs({ propertyKey: 'sortBy', currentValue: sortBy })
+  }, [sortBy, handleSetTableUserPrefs])
+
   const table = (
-    <TableOverflowWrapper>
-      <Table {...getTableProps()}>
-        <thead>
-          {headerGroups.map((headerGroup) => {
-            return (
-              <Tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => (
-                  <Th {...column.getHeaderProps()}>{column.render('Header')}</Th>
-                ))}
-              </Tr>
-            )
-          })}
-        </thead>
-      </Table>
-    </TableOverflowWrapper>
+    <>
+      <TableOverflowWrapper>
+        <Table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => {
+              return (
+                <Tr {...headerGroup.getHeaderGroupProps()}>
+                  {headerGroup.headers.map((column) => (
+                    <Th
+                      {...column.getHeaderProps(column.getSortByToggleProps())}
+                      isSortedDescending={column.isSortedDesc}
+                      sortedIndex={column.sortedIndex}
+                    >
+                      {column.render('Header')}
+                    </Th>
+                  ))}
+                </Tr>
+              )
+            })}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row)
+
+              return (
+                <Tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <Td {...cell.getCellProps()} align={cell.column.align}>
+                        {cell.render('Cell')}
+                      </Td>
+                    )
+                  })}
+                </Tr>
+              )
+            })}
+          </tbody>
+        </Table>
+      </TableOverflowWrapper>
+      <TableNavigation>
+        <PageSizeSelector
+          onChange={handleRowsNumberChange}
+          pageSize={pageSize}
+          pageSizeOptions={[15, 50, 100]}
+        />
+        <PageSelector
+          onPreviousClick={previousPage}
+          previousDisabled={!canPreviousPage}
+          onNextClick={nextPage}
+          nextDisabled={!canNextPage}
+          onGoToPage={gotoPage}
+          currentPageIndex={pageIndex}
+          pageCount={pageOptions.length}
+        />
+      </TableNavigation>
+    </>
   )
 
   const content = isAppOnline ? table : <PageUnavailableOffline />
