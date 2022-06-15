87c187178a0171050d3f48c41e582f2f6810e4a0
tienhoah <nickhoang11@gmail.com>
2022-06-15 14:04:43 -0700

    [M638] Refactor initial form values, and add extra data for benthicpqt.

--- src/components/pages/collectRecordFormPages/collectRecordFormInitialValues.js
+++ src/components/pages/collectRecordFormPages/collectRecordFormInitialValues.js
@@ -8,36 +8,51 @@ const getCollectRecordDataInitialValues = (collectRecord) => {
   }
 }
 
-const getSampleInfoInitialValues = (collectRecord, transectType) => {
-  const collectRecordData = collectRecord?.data
+const getSampleInfoInitialValues = (collectRecord) => {
+  const collectRecordSampleEventData = collectRecord?.data?.sample_event
 
   return {
-    depth: collectRecordData?.[transectType]?.depth ?? '',
-    site: collectRecordData?.sample_event.site ?? '',
-    management: collectRecordData?.sample_event.management ?? '',
-    sample_date: dateFormat(collectRecordData?.sample_event.sample_date) ?? '',
-    sample_time: collectRecordData?.[transectType]?.sample_time ?? '',
+    site: collectRecordSampleEventData?.site ?? '',
+    management: collectRecordSampleEventData?.management ?? '',
+    sample_date: dateFormat(collectRecordSampleEventData?.sample_date) ?? '',
+    notes: collectRecordSampleEventData?.notes ?? '',
   }
 }
 
 const getTransectInitialValues = (collectRecord, transectType) => {
-  const collectRecordData = collectRecord?.data
+  const collectRecordTransectTypeData = collectRecord?.data?.[transectType]
 
   // radios dont complain when initial value is undefined (because they have a value already and checked is the state that changes), but other inputs types do
   // we also dont want 'not reported' (has empty string value) to be selected by default, this is why some of these properties dont default to an ampty string, but others do
   return {
-    number: collectRecordData?.[transectType]?.number ?? '',
-    label: collectRecordData?.[transectType]?.label ?? '',
-    len_surveyed: collectRecordData?.[transectType]?.len_surveyed ?? '',
-    width: collectRecordData?.[transectType]?.width,
-    size_bin: collectRecordData?.[transectType]?.size_bin,
-    reef_slope: collectRecordData?.[transectType]?.reef_slope,
-    notes: collectRecordData?.sample_event.notes ?? '',
-    relative_depth: collectRecordData?.[transectType]?.relative_depth,
-    visibility: collectRecordData?.[transectType]?.visibility,
-    current: collectRecordData?.[transectType]?.current,
-    tide: collectRecordData?.[transectType]?.tide,
+    depth: collectRecordTransectTypeData?.depth ?? '',
+    number: collectRecordTransectTypeData?.number ?? '',
+    label: collectRecordTransectTypeData?.label ?? '',
+    len_surveyed: collectRecordTransectTypeData?.len_surveyed ?? '',
+    width: collectRecordTransectTypeData?.width,
+    sample_time: collectRecordTransectTypeData?.sample_time ?? '',
+    size_bin: collectRecordTransectTypeData?.size_bin,
+    reef_slope: collectRecordTransectTypeData?.reef_slope,
+    relative_depth: collectRecordTransectTypeData?.relative_depth,
+    visibility: collectRecordTransectTypeData?.visibility,
+    current: collectRecordTransectTypeData?.current,
+    tide: collectRecordTransectTypeData?.tide,
   }
 }
 
-export { getCollectRecordDataInitialValues, getSampleInfoInitialValues, getTransectInitialValues }
+const getBenthicPhotoQuadratAdditionalValues = (collectRecord) => {
+  const benthicPhotoQuadratData = collectRecord?.data?.quadrat_transect
+
+  return {
+    quadrat_size: benthicPhotoQuadratData?.quadrat_size ?? '',
+    num_quadrats: benthicPhotoQuadratData?.num_quadrats ?? '',
+    num_points_per_quadrat: benthicPhotoQuadratData?.num_points_per_quadrat ?? '',
+  }
+}
+
+export {
+  getCollectRecordDataInitialValues,
+  getSampleInfoInitialValues,
+  getTransectInitialValues,
+  getBenthicPhotoQuadratAdditionalValues,
+}
