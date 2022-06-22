433d38ba266aee9b19f6461a636e48eda1cf789c
tienhoah <nickhoang11@gmail.com>
2022-06-22 15:13:36 -0700

    [M688] Add mixin function to handle submit benthic photo quadrat.

--- src/App/mermaidData/databaseSwitchboard/BenthicAttributesMixin.js
+++ src/App/mermaidData/databaseSwitchboard/BenthicAttributesMixin.js
@@ -1,8 +1,62 @@
+import { createUuid } from '../../../library/createUuid'
+
 const BenthicAttributesMixin = (Base) =>
   class extends Base {
     getBenthicAttributes = function getBenthicAttributes() {
       if (this._isAuthenticatedAndReady) {
         return this._dexiePerUserDataInstance.benthic_attributes.toArray()
+      }
+
+      return Promise.reject(this._notAuthenticatedAndReadyError)
+    }
+
+    addBenthicAttribute = async function addBenthicAttribute({
+      benthicAttributeParentId,
+      benthicAttributeParentName,
+      newBenthicAttributeName,
+    }) {
+      if (!benthicAttributeParentId || !benthicAttributeParentName || !newBenthicAttributeName) {
+        Promise.reject(new Error('addBenthicAttribute was implemented with missing parameters'))
+      }
+
+      const existingBenthicAttribute = await this.getBenthicAttributes()
+      const existingMatchingBenthicAttribute = existingBenthicAttribute.filter(
+        (benthicAttribute) => benthicAttribute.name === newBenthicAttributeName,
+      )
+      const isProposedBenthicAttributeAlreadyExisting = existingMatchingBenthicAttribute.length > 0
+
+      if (isProposedBenthicAttributeAlreadyExisting) {
+        const benthicAttributeException = {
+          message: 'Benthic attribute already exists',
+          existingBenthicAttribute: existingMatchingBenthicAttribute[0],
+        }
+
+        return Promise.reject(benthicAttributeException)
+      }
+
+      const newBenthicAttributeObject = {
+        id: createUuid(),
+        name: newBenthicAttributeName,
+        parent: benthicAttributeParentId,
+        uiState_pushToApi: true,
+      }
+
+      if (this._isOnlineAuthenticatedAndReady) {
+        const _protectAgainstNetworkStutter =
+          await this._dexiePerUserDataInstance.benthic_attributes.put(newBenthicAttributeObject)
+
+        return this._apiSyncInstance
+          .pushThenPullFishOrBenthicAttributes('benthic_attributes')
+          .then((response) => {
+            const newBenthicAttributeFromApi = response.data.benthic_attributes.updates[0]
+
+            return newBenthicAttributeFromApi
+          })
+      }
+      if (this._isOfflineAuthenticatedAndReady) {
+        return this._dexiePerUserDataInstance.benthic_attributes
+          .put(newBenthicAttributeObject)
+          .then(() => newBenthicAttributeObject)
       }
 
       return Promise.reject(this._notAuthenticatedAndReadyError)
