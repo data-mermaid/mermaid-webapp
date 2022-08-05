cda83512b923acf96ed3ee47f68b227ea1bcd90a
Joe Burkinshaw <1443875+JBurkinshaw@users.noreply.github.com>
2022-08-04 17:38:23 -0700

    Rename and update test file

--- src/library/handleGenericApiErrors.test.js
+++ src/library/handleGenericApiErrors.test.js
@@ -1,17 +1,17 @@
 import { toast } from 'react-toastify'
-import communicateGenericApiErrorsToUser from './communicateGenericApiErrorsToUser'
+import handleGenericApiErrors from './handleGenericApiErrors'
 
-test('communicateGenericApiErrorsToUser throws if an error object with an improper schema is used', () => {
+test('handleGenericApiErrors throws if an error object with an improper schema is used', () => {
   expect(() =>
-    communicateGenericApiErrorsToUser({ error: { foo: 'not the right schema' } }),
+    handleGenericApiErrors({ error: { foo: 'not the right schema' } }),
   ).toThrow()
 })
-test('communicateGenericApiErrorsToUser produces the appropriate toast message if the status is 401', () => {
+test('handleGenericApiErrors produces the appropriate toast message if the status is 401', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({ error: { response: { status: 401 } }, callback })
+  handleGenericApiErrors({ error: { response: { status: 401 } }, callback })
 
   expect(toastSpy).toHaveBeenCalledWith(
     "There is something wrong with the user's credentials. You may want to try logging out and logging back in.",
@@ -22,12 +22,12 @@ test('communicateGenericApiErrorsToUser produces the appropriate toast message i
   )
   expect(callback).not.toHaveBeenCalled()
 })
-test('communicateGenericApiErrorsToUser produces the appropriate toast message if the status is 403', () => {
+test('handleGenericApiErrors produces the appropriate toast message if the status is 403', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({ error: { response: { status: 403 } }, callback })
+  handleGenericApiErrors({ error: { response: { status: 403 } }, callback })
 
   expect(toastSpy).toHaveBeenCalledWith(
     'The current user does not have sufficient permission to do that.',
@@ -38,12 +38,12 @@ test('communicateGenericApiErrorsToUser produces the appropriate toast message i
 
   expect(callback).not.toHaveBeenCalled()
 })
-test('communicateGenericApiErrorsToUser produces the appropriate toast message if the status is 500', () => {
+test('handleGenericApiErrors produces the appropriate toast message if the status is 500', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({ error: { response: { status: 500 } }, callback })
+  handleGenericApiErrors({ error: { response: { status: 500 } }, callback })
 
   expect(toastSpy).toHaveBeenCalledWith('Something went wrong with the server.', {
     toastId: 'Something went wrong with the server.',
@@ -51,12 +51,12 @@ test('communicateGenericApiErrorsToUser produces the appropriate toast message i
 
   expect(callback).not.toHaveBeenCalled()
 })
-test('communicateGenericApiErrorsToUser produces the appropriate toast message if the status is 502', () => {
+test('handleGenericApiErrors produces the appropriate toast message if the status is 502', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({ error: { response: { status: 502 } }, callback })
+  handleGenericApiErrors({ error: { response: { status: 502 } }, callback })
 
   expect(toastSpy).toHaveBeenCalledWith('Something went wrong with the server.', {
     toastId: 'Something went wrong with the server.',
@@ -64,12 +64,12 @@ test('communicateGenericApiErrorsToUser produces the appropriate toast message i
 
   expect(callback).not.toHaveBeenCalled()
 })
-test('communicateGenericApiErrorsToUser produces the appropriate toast message if the status is 503', () => {
+test('handleGenericApiErrors produces the appropriate toast message if the status is 503', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({ error: { response: { status: 503 } }, callback })
+  handleGenericApiErrors({ error: { response: { status: 503 } }, callback })
 
   expect(toastSpy).toHaveBeenCalledWith('Something went wrong with the server.', {
     toastId: 'Something went wrong with the server.',
@@ -77,20 +77,20 @@ test('communicateGenericApiErrorsToUser produces the appropriate toast message i
 
   expect(callback).not.toHaveBeenCalled()
 })
-test('communicateGenericApiErrorsToUser can be extended with a callback function', () => {
+test('handleGenericApiErrors can be extended with a callback function', () => {
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({
+  handleGenericApiErrors({
     error: { response: { status: 'something that wont be handled in util function logic' } },
     callback,
   })
 
   expect(callback).toHaveBeenCalled()
 })
-test('if no callback is provided communicateGenericApiErrorsToUser will produce a generic error toast message f', () => {
+test('if no callback is provided handleGenericApiErrors will produce a generic error toast message f', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
-  communicateGenericApiErrorsToUser({
+  handleGenericApiErrors({
     error: { response: { status: 'something that wont be handled in util function logic' } },
   })
 
@@ -99,12 +99,12 @@ test('if no callback is provided communicateGenericApiErrorsToUser will produce 
   })
 })
 
-test('If a callback is provided, communicateGenericApiErrorsToUser will not produce a generic user message for status 400', () => {
+test('If a callback is provided, handleGenericApiErrors will not produce a generic user message for status 400', () => {
   const toastSpy = jest.spyOn(toast, 'error')
 
   const callback = jest.fn()
 
-  communicateGenericApiErrorsToUser({ error: { response: { status: 400 } }, callback })
+  handleGenericApiErrors({ error: { response: { status: 400 } }, callback })
 
   expect(toastSpy).not.toHaveBeenCalled()
 })
