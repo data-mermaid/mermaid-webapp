cbcaaadddf4bfe26064dcfbdb4a280e78ca13c5d
corina_mac <corina_ww@yahoo.com>
2022-08-24 16:55:47 +0100

    refactor: implemented modal div for copy button

--- src/components/ProjectCard/Modal.js
+++ src/components/ProjectCard/Modal.js
@@ -1,88 +1,82 @@
-// import React, { useRef } from 'react'
-// import styled from 'styled-components'
-// import { MdClose } from 'react-icons/md'
+import React, { useRef } from 'react'
+import styled from 'styled-components'
 
-// const Background = styled.div`
-//   width: 100%;
-//   height: 100%;
-//   background: rgba(0, 0, 0, 0.8);
-//   position: relative;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// `
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 1);
+  position: fixed;
+  display: flex;
+  left: 0px;
+  bottom: 0px;
+  z-index: 1000; // or any value higher than your other elements
+  justify-content: center;
+  align-items: center;
+`
+const ModalWrapper = styled.div`
+  width: 500px;
+  height: 300px;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  color: #000;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  position: relative;
+  z-index: 10;
+  border-radius: 10px;
+`
 
-// const ModalWrapper = styled.div`
-//   width: 500px;
-//   height: 300px;
-//   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-//   background: #fff;
-//   color: #000;
-//   display: grid;
-//   grid-template-columns: 1fr 1fr;
-//   position: relative;
-//   z-index: 10;
-//   border-radius: 10px;
-// `
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  line-height: 1.8;
+  color: #141414;
 
-// const ModalContent = styled.div`
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: center;
-//   align-items: center;
-//   line-height: 1.8;
-//   color: #141414;
+  p {
+    margin-bottom: 1rem;
+  }
 
-//   p {
-//     margin-bottom: 1rem;
-//   }
+  button {
+    padding: 5px 5px;
+    background: #141414;
+    color: #fff;
+    border: none;
+  }
+`
 
-//   button {
-//     padding: 10px 24px;
-//     background: #141414;
-//     color: #fff;
-//     border: none;
-//   }
-// `
+const Modal = ({ showModal, setShowModal }) => {
+  const modalRef = useRef()
 
-// const CloseModalButton = styled(MdClose)`
-//   cursor: pointer;
-//   position: absolute;
-//   top: 20px;
-//   right: 20px;
-//   width: 32px;
-//   height: 32px;
-//   padding: 0;
-//   z-index: 10;
-// `
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false)
+    }
+  }
 
-// const Modal = ({ showModal, setShowModal }) => {
-//   const modalRef = useRef()
+  return (
+    <>
+      {showModal ? (
+        <Background onClick={closeModal} ref={modalRef}>
+          <ModalWrapper showModal={showModal}>
+            <ModalContent>
+              <h1>Are you ready?</h1>
+              <p>Get exclusive access to our next launch.</p>
 
-//   const closeModal = (e) => {
-//     if (modalRef.current === e.target) {
-//       setShowModal(false)
-//     }
-//   }
+              <button
+                type="button"
+                aria-label="Close modal"
+                onClick={() => setShowModal((prev) => !prev)}
+              >
+                Cancel
+              </button>
+            </ModalContent>
+          </ModalWrapper>
+        </Background>
+      ) : null}
+    </>
+  )
+}
 
-//   return (
-//     <>
-//       {showModal ? (
-//         <Background onClick={closeModal} ref={modalRef}>
-//           <ModalWrapper showModal={showModal}>
-//             <ModalContent>
-//               <h1>Are you ready?</h1>
-//               <p>Get exclusive access to our next launch.</p>
-//               <button>Join Now</button>
-//             </ModalContent>
-//             <CloseModalButton
-//               aria-label="Close modal"
-//               onClick={() => setShowModal((prev) => !prev)}
-//             />
-//           </ModalWrapper>
-//         </Background>
-//       ) : null}
-//     </>
-//   )
-// }
-// export default Modal
+export default Modal
--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,7 +1,7 @@
 import { toast } from 'react-toastify'
 import { useHistory } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import React from 'react'
+import React, { useState } from 'react'
 
 import {
   CardWrapper,
@@ -21,7 +21,7 @@ import { IconCopy } from '../icons'
 import { ButtonSecondary } from '../generic/buttons'
 import { removeTimeZoneFromDate } from '../../library/removeTimeZoneFromDate'
 import ProjectCardSummary from './ProjectCardSummary'
-// import Modal from './Modal'
+import Modal from './Modal'
 
 const ProjectCard = ({ project, apiSyncInstance, isOfflineReady, ...restOfProps }) => {
   const { isAppOnline } = useOnlineStatus()
@@ -82,11 +82,11 @@ const ProjectCard = ({ project, apiSyncInstance, isOfflineReady, ...restOfProps 
   //   e.stopPropagation()
   // }
 
-  // const [showModal, setShowModal] = useState(false)
+  const [showModal, setShowModal] = useState(false)
 
-  // const openModal = () => {
-  //   setShowModal((prev) => !prev)
-  // }
+  const openModal = () => {
+    setShowModal((prev) => !prev)
+  }
 
   return (
     <CardWrapper onClick={handleCardClick} {...restOfProps}>
@@ -97,16 +97,11 @@ const ProjectCard = ({ project, apiSyncInstance, isOfflineReady, ...restOfProps 
         </div>
         <ProjectCardHeaderButtonsAndDate onClick={stopEventPropagation}>
           <div>
-            <ButtonSecondary
-              onClick={stopEventPropagation}
-              aria-label="Copy"
-              disabled={!isAppOnline}
-            >
-              {/* <ButtonSecondary onClick={openModal} aria-label="Copy" disabled={!isAppOnline}> */}
+            <ButtonSecondary onClick={openModal} aria-label="Copy" disabled={!isAppOnline}>
               <IconCopy />
               <span>Copy</span>
             </ButtonSecondary>
-            {/* <Modal showModal={showModal} setShowModal={setShowModal} /> */}
+            <Modal showModal={showModal} setShowModal={setShowModal} />
             <CheckBoxLabel
               htmlFor={project.id}
               onClick={stopEventPropagation}
