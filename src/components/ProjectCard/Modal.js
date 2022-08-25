18d1e69221efc6ebd473f320c67788f208669be8
corina_mac <corina_ww@yahoo.com>
2022-08-25 14:56:49 +0100

    refactor: removed unnecessary import

--- src/components/ProjectCard/Modal.js
+++ src/components/ProjectCard/Modal.js
@@ -1,4 +1,3 @@
-import React, { useRef } from 'react'
 import styled from 'styled-components'
 import { useFormik } from 'formik'
 import stopEventPropagation from '../../library/stopEventPropagation'
