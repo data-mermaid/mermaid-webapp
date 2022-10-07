857a29a6e6b5c1c68f853e73da73824ad6ecb2b3
tienhoah <6288799+tienhoah@users.noreply.github.com>
2022-10-06 17:40:29 -0700

    feat: [M764 -> M465] previously M764, wrong merge to M465, suppose to merge to develop
    Add map to Copy Sites Modal

--- src/components/CopySitesModal/CopySitesModal.js
+++ src/components/CopySitesModal/CopySitesModal.js
@@ -21,6 +21,7 @@ import { pluralize } from '../../library/strings/pluralize'
 import language from '../../language'
 import { reactTableNaturalSort } from '../generic/Table/reactTableNaturalSort'
 import PageSelector from '../generic/Table/PageSelector'
+import CopySitesMap from '../mermaidMap/CopySitesMap'
 
 const PaginationWrapper = styled.div`
   display: flex;
@@ -134,6 +135,7 @@ const CopySitesModal = ({ isOpen, onDismiss, addCopiedSitesToSiteTable }) => {
           reef_zone_name,
           exposure_name,
           location,
+          project,
         }) => ({
           id,
           name,
@@ -143,6 +145,7 @@ const CopySitesModal = ({ isOpen, onDismiss, addCopiedSitesToSiteTable }) => {
           reefZone: reef_zone_name,
           exposure: exposure_name,
           location,
+          project,
         }),
       ),
     [siteRecords],
@@ -187,7 +190,6 @@ const CopySitesModal = ({ isOpen, onDismiss, addCopiedSitesToSiteTable }) => {
         pageSize: 5,
         sortBy: tableUserPrefs.sortBy,
       },
-      autoResetSelectedRows: false,
       getRowId: (row) => row.id,
       // Disables requirement to hold shift to enable multi-sort
       isMultiSortEvent: () => true,
@@ -279,6 +281,7 @@ const CopySitesModal = ({ isOpen, onDismiss, addCopiedSitesToSiteTable }) => {
           pageCount={pageOptions.length}
         />
       </PaginationWrapper>
+      <CopySitesMap sitesForMapMarkers={selectedFlatRows.map((r) => r.original)} />
     </>
   )
 
