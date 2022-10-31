c384ff46b7647b2b177c413359364c7a7f35800d
tienhoah <nickhoang11@gmail.com>
2022-10-31 15:30:17 -0700

    Fix change requests.

--- src/library/useScrollCheckError.js
+++ src/library/useScrollCheckError.js
@@ -1,42 +1,43 @@
 import { useState, useEffect } from 'react'
 
-const getElementPosition = (rectObj) => {
+const getErrorPositionRelativeToCurrentlyVisibleViewportArea = (errorElementDOMRectangleObject) => {
   const html = document.documentElement
 
-  let aboveBelow = 'visible'
+  let relativePositionToViewport = 'visible'
 
-  if (rectObj.bottom >= 0 && rectObj.top >= (window.innerHeight || html.clientHeight)) {
-    aboveBelow = 'below'
-  } else if (rectObj.top <= 0 && rectObj.bottom <= (window.innerHeight || html.clientHeight)) {
-    aboveBelow = 'above'
+  if (
+    errorElementDOMRectangleObject.bottom >= 0 &&
+    errorElementDOMRectangleObject.top >= (window.innerHeight || html.clientHeight)
+  ) {
+    relativePositionToViewport = 'below'
+  } else if (
+    errorElementDOMRectangleObject.top <= 0 &&
+    errorElementDOMRectangleObject.bottom <= (window.innerHeight || html.clientHeight)
+  ) {
+    relativePositionToViewport = 'above'
   }
 
-  return aboveBelow
+  return relativePositionToViewport
 }
 
-const isInViewport = (element) => {
-  const rects = []
+const getPositionTypes = (errorElements) =>
+  [...errorElements].map((errorElement) =>
+    getErrorPositionRelativeToCurrentlyVisibleViewportArea(errorElement.getBoundingClientRect()),
+  )
 
-  for (let i = 0; i < element.length; i++) {
-    const rectPosition = getElementPosition(element[i].getBoundingClientRect())
-
-    rects.push(rectPosition)
-  }
-
-  return rects
-}
-
-export const useScrollCheckError = (inputRef) => {
+export const useScrollCheckError = () => {
   const [isErrorAbove, setIsErrorAbove] = useState(false)
   const [isErrorBelow, setIsErrorBelow] = useState(false)
 
   useEffect(() => {
     const handleScroll = () => {
-      const el = document.querySelectorAll('span[type="error"], span[type="warning"]')
-      const positionClass = isInViewport(el)
+      const errorAndWarningElements = document.querySelectorAll(
+        'span[type="error"], span[type="warning"]',
+      )
+      const positionTypes = getPositionTypes(errorAndWarningElements)
 
-      const hasErrorPositionAbove = positionClass.includes('above')
-      const hasErrorPositionBelow = positionClass.includes('below')
+      const hasErrorPositionAbove = positionTypes.includes('above')
+      const hasErrorPositionBelow = positionTypes.includes('below')
 
       setIsErrorAbove(hasErrorPositionAbove)
       setIsErrorBelow(hasErrorPositionBelow)
@@ -47,7 +48,7 @@ export const useScrollCheckError = (inputRef) => {
     return () => {
       window.removeEventListener('scroll', handleScroll)
     }
-  }, [inputRef])
+  }, [])
 
   return { isErrorAbove, isErrorBelow }
 }
