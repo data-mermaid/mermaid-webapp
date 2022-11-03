f008170bd6f94fae132004fd86f0c422531d5cc3
julia-rom <jromanowski@sparkgeo.com>
2022-11-03 13:24:04 -0400

    add placeholder component for BenthicPITForm

---
+++ src/components/pages/collectRecordFormPages/BenthicPITForm/BenthicPITForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+const BenthicPITForm = () => {
+  return (
+    <Header>
+      <h2>Benthic PIT</h2>
+    </Header>
+  )
+}
+
+const Header = styled.div`
+  margin-top: 5em;
+`
+
+export default BenthicPITForm
