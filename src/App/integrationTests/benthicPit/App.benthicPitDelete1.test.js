b5709505c67fbd61b4e50a75b32c519e276a1cb5
Mel Nunes <nunes.melissa.m@gmail.com>
2022-11-14 13:04:24 -0700

    chore: [M474] tests for non observation, non validation benthic pit form
    * chore: [M474] PR suggestions
    
    Co-authored-by: tienhoah <6288799+tienhoah@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: tienhoah <6288799+tienhoah@users.noreply.github.com>
    
    Co-authored-by: tienhoah <6288799+tienhoah@users.noreply.github.com>

---
+++ src/App/integrationTests/benthicPit/App.benthicPitDelete1.test.js
@@ -0,0 +1,56 @@
+import '@testing-library/jest-dom/extend-expect'
+import React from 'react'
+import userEvent from '@testing-library/user-event'
+import {
+  screen,
+  renderAuthenticatedOffline,
+  within,
+} from '../../../testUtilities/testingLibraryWithHelpers'
+import { getMockDexieInstancesAllSuccess } from '../../../testUtilities/mockDexie'
+import { initiallyHydrateOfflineStorageWithMockData } from '../../../testUtilities/initiallyHydrateOfflineStorageWithMockData'
+import App from '../../App'
+
+// test suite cut up into 2 parts for performance reasons
+describe('Offline', () => {
+  test('Delete benthic PIT prompt confirm deletes the record with the proper UI response and messaging', async () => {
+    const { dexiePerUserDataInstance, dexieCurrentUserInstance } = getMockDexieInstancesAllSuccess()
+
+    await initiallyHydrateOfflineStorageWithMockData(dexiePerUserDataInstance)
+
+    renderAuthenticatedOffline(<App dexieCurrentUserInstance={dexieCurrentUserInstance} />, {
+      initialEntries: ['/projects/5/collecting/benthicpit/50'],
+      dexiePerUserDataInstance,
+      dexieCurrentUserInstance,
+    })
+    const deleteButton = await screen.findByText('Delete Record')
+
+    userEvent.click(deleteButton)
+
+    expect(screen.getByText('Are you sure you want to delete this record?'))
+
+    const modal = screen.getByLabelText('Delete Record')
+
+    userEvent.click(
+      within(modal).getByText('Delete Record', {
+        selector: 'button',
+      }),
+    )
+    // shows toast
+    expect(await screen.findByText('Record deleted.'))
+
+    // navigated to collect records table page
+    expect(
+      await screen.findByText('Collecting', {
+        selector: 'h2',
+      }),
+    )
+
+    userEvent.selectOptions(screen.getByTestId('page-size-selector'), '50')
+
+    const table = screen.getByRole('table')
+    const collectRecordLinks = within(table).getAllByRole('link')
+
+    // row length = 16 because 17 mock records, now minus 1
+    expect(collectRecordLinks.length).toEqual(16)
+  })
+})
