a2e08c25b67ab83cf6b8d65db14ee18e0f8bb778
julia-rom <jromanowski@sparkgeo.com>
2022-11-14 09:58:19 -0500

    add additional items to info  table

--- src/components/pages/submittedRecordPages/SubmittedBenthicPIT/SubmittedBenthicPitInfoTable.js
+++ src/components/pages/submittedRecordPages/SubmittedBenthicPIT/SubmittedBenthicPitInfoTable.js
@@ -18,14 +18,14 @@ const SubmittedBenthicPitInfoTable = ({ sites, managementRegimes, choices, submi
     number,
     label,
     len_surveyed,
-    // width,
-    // size_bin,
-    // reef_slope,
-    // visibility,
-    // current,
-    // relative_depth,
-    // tide,
-    // notes,
+    // interval_size,
+    // interval_start,
+    reef_slope,
+    visibility,
+    current,
+    relative_depth,
+    tide,
+    notes,
   } = submittedRecord.benthic_transect
 
   // eslint-disable-next-line no-empty-pattern
@@ -49,14 +49,14 @@ const SubmittedBenthicPitInfoTable = ({ sites, managementRegimes, choices, submi
         <TableRowItem title="Transect Number" value={number} />
         <TableRowItem title="Label" value={label} />
         <TableRowItem title="Transect Length Surveyed" value={len_surveyed} />
-        {/* <TableRowItem title="Width" options={belttransectwidths.data} value={width} />
-        <TableRowItem title="Fish Size Bin" options={fishsizebins.data} value={size_bin} />
-        <TableRowItem title="Reef Slope" options={reefslopes.data} value={reef_slope} />
-        <TableRowItem title="Visibility" options={visibilities.data} value={visibility} />
-        <TableRowItem title="Current" options={currents.data} value={current} />
-        <TableRowItem title="Relative Depth" options={relativedepths.data} value={relative_depth} />
-        <TableRowItem title="Tide" options={tides.data} value={tide} />
-        <TableRowItem title="Notes" value={notes} /> */}
+        {/* <TableRowItem title="Interval Size" value={width} /> */}
+        {/* <TableRowItem title="Interval Start" value={size_bin} /> */}
+        <TableRowItem title="Reef Slope" value={reef_slope} />
+        <TableRowItem title="Visibility" value={visibility} />
+        <TableRowItem title="Current" value={current} />
+        <TableRowItem title="Relative Depth" value={relative_depth} />
+        <TableRowItem title="Tide" value={tide} />
+        <TableRowItem title="Notes" value={notes} />
       </tbody>
     </Table>
   )
