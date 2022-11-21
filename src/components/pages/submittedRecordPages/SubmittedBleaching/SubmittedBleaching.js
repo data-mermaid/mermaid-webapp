eb29b1b24e900c49a9526495efef3947fee7b49f
julia-rom <jromanowski@sparkgeo.com>
2022-11-21 13:02:02 -0500

    api route fix

--- src/components/pages/submittedRecordPages/SubmittedBleaching/SubmittedBleaching.js
+++ src/components/pages/submittedRecordPages/SubmittedBleaching/SubmittedBleaching.js
@@ -59,7 +59,7 @@ const SubmittedBleaching = () => {
         databaseSwitchboardInstance.getSubmittedSampleUnitRecord(
           projectId,
           submittedRecordId,
-          'bleachingtransectmethods',
+          'bleachingquadratcollectionmethods',
         ),
       ]
 
