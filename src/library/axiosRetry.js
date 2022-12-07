76f5e7942bfcab15f17ed7ec7f1ae72699414cdf
Melissa Nunes <mnunes@sparkgeo.com>
2022-12-07 15:37:00 -0700

    Melissa typing Joes ideas

--- src/library/axiosRetry.js
+++ src/library/axiosRetry.js
@@ -1,11 +1,13 @@
 import axios from 'axios'
 import axiosRetry from 'axios-retry'
 
+const axiosInstance = axios.create()
+
 // Apply axios-retry to axios
-axiosRetry(axios, {
+axiosRetry(axiosInstance, {
   retries: 3,
   // Exponential back-off retry delay between requests
   retryDelay: axiosRetry.exponentialDelay,
 })
 
-export default axios
+export default axiosInstance
