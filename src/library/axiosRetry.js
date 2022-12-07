eeaf7930e5cc3f7d53543c7fab0524067c7c01d1
Joe Burkinshaw <1443875+JBurkinshaw@users.noreply.github.com>
2022-12-07 15:21:50 -0800

    Enable retry for all requests This includes POST (/pull and /push).

--- src/library/axiosRetry.js
+++ src/library/axiosRetry.js
@@ -8,6 +8,7 @@ axiosRetry(axiosInstance, {
   retries: 3,
   // Exponential back-off retry delay between requests
   retryDelay: axiosRetry.exponentialDelay,
+  retryCondition: (_error) => true, // retry no matter what (POSTs can also be idempotent in MERMAID)
 })
 
 export default axiosInstance
