b25ba103c9b9c3c0c7619ea76eafd22e693db02c
julia-rom <jromanowski@sparkgeo.com>
2022-12-16 08:07:00 -0500

    add reusable observation table

--- src/components/pages/submittedRecordPages/SubmittedBenthicLit/SubmittedBenthicLitObservationTable.js
+++ src/components/pages/submittedRecordPages/SubmittedBenthicLit/SubmittedBenthicLitObservationTable.js
@@ -4,17 +4,18 @@ import {
   submittedBenthicLitPropType,
 } from '../../../../App/mermaidData/mermaidDataProptypes'
 import { inputOptionsPropTypes } from '../../../../library/miscPropTypes'
-import { SubmittedObservationStickyTable, Tr, Td, Th } from '../../../generic/Table/table'
+import { SubmittedObservationStickyTable, Tr, Td } from '../../../generic/Table/table'
 import {
   TheadItem,
   FormSubTitle,
-  ObservationsSummaryStats,
+  // ObservationsSummaryStats,
   UnderTableRow,
 } from '../SubmittedFormPage.styles'
 import { InputWrapper } from '../../../generic/form'
 import { StyledOverflowWrapper } from '../../collectRecordFormPages/CollectingFormPage.Styles'
 import { getObjectById } from '../../../../library/getObjectById'
 import { getOptions } from '../../../../library/getOptions'
+import BenthicPitLitObservationSummaryStats from '../../../BenthicPitLitObservationSummaryStats'
 
 const SubmittedBenthicLitObservationTable = ({
   benthicAttributeOptions,
@@ -50,18 +51,10 @@ const SubmittedBenthicLitObservationTable = ({
         </SubmittedObservationStickyTable>
       </StyledOverflowWrapper>
       <UnderTableRow>
-        <ObservationsSummaryStats>
-          <tbody>
-            <Tr>
-              <Th>placeholder label</Th>
-              <Td>placeholder item</Td>
-            </Tr>
-            <Tr>
-              <Th>placeholder label</Th>
-              <Td>placeholder item</Td>
-            </Tr>
-          </tbody>
-        </ObservationsSummaryStats>
+        <BenthicPitLitObservationSummaryStats
+          benthicAttributeSelectOptions={benthicAttributeOptions}
+          observations={obs_benthic_lits}
+        />
       </UnderTableRow>
     </InputWrapper>
   )
