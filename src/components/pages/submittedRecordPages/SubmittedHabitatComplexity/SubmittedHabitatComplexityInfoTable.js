04630a978e9ab4b15fc306acdf0c75e96e2573ea
julia-rom <jromanowski@sparkgeo.com>
2022-12-19 09:37:10 -0500

    update unit display on submitted pages

--- src/components/pages/submittedRecordPages/SubmittedHabitatComplexity/SubmittedHabitatComplexityInfoTable.js
+++ src/components/pages/submittedRecordPages/SubmittedHabitatComplexity/SubmittedHabitatComplexityInfoTable.js
@@ -41,11 +41,11 @@ const SubmittedHabitatComplexityInfoTable = ({
         <TableRowItem title="Site" options={sites} value={site} />
         <TableRowItem title="Management" options={managementRegimes} value={management} />
         <TableRowItem title="Sample Date Time" value={`${sample_date} ${sample_time || ''}`} />
-        <TableRowItem title="Depth" value={depth} />
+        <TableRowItem title="Depth (m)" value={depth} />
         <TableRowItem title="Transect Number" value={number} />
         <TableRowItem title="Label" value={label} />
-        <TableRowItem title="Transect Length Surveyed" value={len_surveyed} />
-        <TableRowItem title="Interval Size" value={interval_size} />
+        <TableRowItem title="Transect Length Surveyed (m)" value={len_surveyed} />
+        <TableRowItem title="Interval Size (m)" value={interval_size} />
         <TableRowItem title="Reef Slope" options={reefslopes.data} value={reef_slope} />
         <TableRowItem title="Visibility" options={visibilities.data} value={visibility} />
         <TableRowItem title="Current" options={currents.data} value={current} />
