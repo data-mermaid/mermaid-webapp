3bb55029357bf16f9478624656794ed6dd9979cb
tienhoah <nickhoang11@gmail.com>
2023-02-13 15:01:17 -0800

    Remove tests fixed for secondary name feature.

--- src/components/pages/collectRecordFormPages/FishBeltForm/tests/FishBelt.test.js
+++ src/components/pages/collectRecordFormPages/FishBeltForm/tests/FishBelt.test.js
@@ -136,7 +136,7 @@ test('FishBelt component in EDIT mode - form inputs are initialized with the cor
   // Site select
   expect(screen.getByDisplayValue('Site D'))
   // Management select
-  expect(screen.getByDisplayValue('Management Regimes C [Management Regimes 3]'))
+  expect(screen.getByDisplayValue('Management Regimes C'))
   expect(screen.getByLabelText('Depth')).toHaveValue(10)
   expect(screen.getByLabelText('Sample Date')).toHaveValue('2021-03-02')
   expect(screen.getByLabelText('Sample Time')).toHaveValue('11:55')
