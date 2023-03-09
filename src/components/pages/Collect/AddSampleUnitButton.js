6b2b093c08283e1e63e9b51a6d4d1bd4e6806fd5
julia-rom <jromanowski@sparkgeo.com>
2023-03-09 05:15:00 -0500

    rearrange menu items for export csv and add sample unit

--- src/components/pages/Collect/AddSampleUnitButton.js
+++ src/components/pages/Collect/AddSampleUnitButton.js
@@ -31,19 +31,19 @@ const AddSampleUnitButton = () => {
     <StyledButtonSecondaryDropdown label={label}>
       <Column as="nav" data-testid="new-sample-unit-nav">
         <CustomNavLink to={`${currentProjectPath}/collecting/fishbelt`}>Fish Belt</CustomNavLink>
-        <CustomNavLink to={`${currentProjectPath}/collecting/benthicpqt`}>
-          Benthic Photo Quadrat
-        </CustomNavLink>
         <CustomNavLink to={`${currentProjectPath}/collecting/benthicpit`}>
           Benthic PIT
-        </CustomNavLink>
-        <CustomNavLink to={`${currentProjectPath}/collecting/habitatcomplexity`}>
-          Habitat Complexity
         </CustomNavLink>
         <CustomNavLink to={`${currentProjectPath}/collecting/benthiclit`}>
           Benthic LIT
         </CustomNavLink>
+        <CustomNavLink to={`${currentProjectPath}/collecting/benthicpqt`}>
+          Benthic Photo Quadrat
+        </CustomNavLink>
         <CustomNavLink to={`${currentProjectPath}/collecting/bleachingqc`}>Bleaching</CustomNavLink>
+        <CustomNavLink to={`${currentProjectPath}/collecting/habitatcomplexity`}>
+          Habitat Complexity
+        </CustomNavLink>
       </Column>
     </StyledButtonSecondaryDropdown>
   )
