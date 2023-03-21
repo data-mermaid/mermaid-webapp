558929b176f456e3e92a78a54ade2e69bd730901
tienhoah <nickhoang11@gmail.com>
2023-03-21 14:45:11 -0700

    Remove outdated tests and add new tests for fish size bin.

--- src/testUtilities/mockMermaidData.js
+++ src/testUtilities/mockMermaidData.js
@@ -743,6 +743,12 @@ const choices = {
         updated_on: '2020-03-13T18:34:46.244096Z',
         val: 'AGRRA',
       },
+      {
+        id: '811c8fdd-36a4-4148-be37-2e2bd948e9f8',
+        name: 'WCS India',
+        updated_on: '2021-08-17T17:06:41.054263Z',
+        val: 'WCS India',
+      },
     ],
   },
   reefslopes: {
