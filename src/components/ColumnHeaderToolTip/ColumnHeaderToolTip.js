27975c72c72e095482846f6ba29cb1bfdb983348
julia-rom <jromanowski@sparkgeo.com>
2023-07-20 10:58:47 -0400

    clean up css tooltip

--- src/components/ColumnHeaderToolTip/ColumnHeaderToolTip.js
+++ src/components/ColumnHeaderToolTip/ColumnHeaderToolTip.js
@@ -3,25 +3,10 @@ import PropTypes from 'prop-types'
 import styled from 'styled-components/macro'
 import theme from '../../theme'
 
-// export const Tooltip = styled('p')`
-//   white-space: nowrap;
-//   border-style: dotted;
-//   border-width: 0 0 ${theme.spacing.borderMedium} 0;
-//   position: relative;
-//   display: inline-grid;
-//   place-items: center;
-//   cursor: pointer;
-//   &:hover span,
-//   &:focus span {
-//     transition: ${theme.timing.hoverTransition};
-//     display: block;
-//   }
-// `
 export const TooltipPopup = styled('span')`
   display: block;
   min-width: 26ch;
   width: 100%;
-  //   max-width: 52ch;
   background: ${theme.color.primaryColor};
   color: ${theme.color.white};
   position: absolute;
@@ -29,11 +14,10 @@ export const TooltipPopup = styled('span')`
   clip-path: polygon(0% 0%, 100% 0%, 100% 83%, 53% 83%, 47% 100%, 40% 83%, 0 83%);
   padding: 1em;
   padding-bottom: calc(4rem - 15px);
-  top: -9rem;
+  bottom: 3.5em;
   left: -1em;
   white-space: normal;
   z-index: 100;
-  //   transition: ${theme.timing.hoverTransition};
 `
 
 const ColumnHeaderToolTip = ({ helperText }) => {
