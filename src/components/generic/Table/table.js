8710950649be5a03fb302e4712e3e992c7c86441
julia-rom <jromanowski@sparkgeo.com>
2023-08-09 14:08:15 -0400

    fix pop-over hover bug

--- src/components/generic/Table/table.js
+++ src/components/generic/Table/table.js
@@ -82,6 +82,7 @@ export const Th = styled.th(
     padding: ${theme.spacing.medium};
     background: ${theme.color.white};
     vertical-align: top;
+    // pointer-events: ${props.disabledHover && 'none'};
     &::after {
       content: ${props.isSortingEnabled ? ' \u25b2' : ''};
       font-size: small;
@@ -154,7 +155,7 @@ export const OverviewTr = styled.tr`
       position: absolute;
       background-color: hsl(0 0% 90%);
       mix-blend-mode: multiply;
-      pointer-events: auto;
+      pointer-events: none;
       top: 0;
       left: 0;
       right: 0;
