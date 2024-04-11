59e3c4a629334b9b50afa0eb7260c00ae5ac1d36
julia-rom <jromanowski@sparkgeo.com>
2024-04-11 12:54:23 -0400

    import new language PDFs

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,112 +1,16 @@
 import React from 'react'
-import styled, { css } from 'styled-components/macro'
-
-import { mediaQueryPhoneOnly } from '../../library/styling/mediaQueries'
+import {
+  StyledFooter,
+  StyledToggleLabel,
+  FooterNav,
+  VersionWrapper,
+  CssToggle,
+} from './Footer.styles'
 import { useOnlineStatus } from '../../library/onlineStatusContext'
 import { versionNumber } from '../../version'
-import MermaidDocs from '../../docs/MERMAID User Documentation.pdf'
+import MermaidDocsEN from '../../docs/MERMAID-user-documentation-EN.pdf'
 import OfflineHide from '../generic/OfflineHide'
 import OfflineToggle from '../OfflineToggle'
-import theme from '../../theme'
-
-const smallFooterTextStyle = css`
-  font-size: ${theme.typography.smallFontSize};
-  ${mediaQueryPhoneOnly(css`
-    margin: ${theme.spacing.small} 0;
-  `)}
-`
-
-const VersionWrapper = styled('span')`
-  ${smallFooterTextStyle}
-`
-
-const StyledFooter = styled('footer')`
-  text-align: end;
-  background-color: ${theme.color.footerColor};
-  padding: ${theme.spacing.small};
-  ${mediaQueryPhoneOnly(css`
-    text-align: start;
-    padding-bottom: 5rem;
-  `)}
-  p,
-  label,
-  nav a,
-  button {
-    ${smallFooterTextStyle}
-  }
-`
-const offlineToggleSize = theme.typography.defaultIconSize
-const CssToggle = styled('span')`
-  display: block;
-  width: calc(${offlineToggleSize} * 2);
-  position: relative;
-  &:before,
-  &:after {
-    content: '';
-    transition: 0.3s;
-    position: absolute;
-  }
-  &:before {
-    // container
-    width: calc(${offlineToggleSize} * 2);
-    height: ${offlineToggleSize};
-    left: 0;
-    top: 0;
-    border-radius: 25% / 50%;
-    background: green;
-  }
-  &:after {
-    // toggle
-    width: calc(${offlineToggleSize} - 2px);
-    height: calc(${offlineToggleSize} - 2px);
-    top: 1px;
-    left: 1px;
-    background: oldlace;
-    border-radius: 50%;
-  }
-`
-const StyledToggleLabel = styled('label')`
-  background: rgba(255, 255, 255, 0.5);
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  cursor: pointer;
-  display: flex;
-  gap: 1.5rem;
-  padding: ${theme.spacing.small};
-  margin: 0;
-  input {
-    display: none;
-  }
-  input:checked {
-    ~ span:after {
-      background: ${theme.color.cautionColor};
-      left: calc(${offlineToggleSize} - 1px);
-    }
-    ~ span:before {
-      background: ${theme.color.cautionColor};
-      opacity: 0.5;
-    }
-  }
-  input:disabled {
-    ~ span:before {
-      background: ${theme.color.disabledColor};
-      opacity: 0.5;
-      cursor: not-allowed;
-    }
-    ~ span:after {
-      background: ${theme.color.disabledColor};
-      cursor: not-allowed;
-    }
-  }
-`
-
-const FooterNav = styled('nav')`
-  a {
-    display: inline-block;
-    padding: 0 ${theme.spacing.small};
-  }
-`
 
 const Footer = () => {
   const { isAppOnline } = useOnlineStatus()
@@ -129,8 +33,8 @@ const Footer = () => {
         </span>
       </StyledToggleLabel>
       <FooterNav>
-        <a href={MermaidDocs} target="_blank" rel="noreferrer">
-          Help (PDF)
+        <a href={MermaidDocsEN} target="_blank" rel="noreferrer">
+          Help (PDF) ▲
         </a>
         <OfflineHide>
           <a href="https://datamermaid.org/terms-of-service" target="_blank" rel="noreferrer">
