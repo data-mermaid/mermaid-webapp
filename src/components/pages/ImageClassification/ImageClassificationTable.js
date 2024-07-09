444c14b49d5556f2506790b03e61124d9b85da29
julia-rom <jromanowski@sparkgeo.com>
2024-07-09 11:19:50 -0400

    comment out proptypes

--- src/components/pages/ImageClassification/ImageClassificationTable.js
+++ src/components/pages/ImageClassification/ImageClassificationTable.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 const ImageClassiciationTable = () => {
   return <p>IMAGE CLASSICIATION TABLE</p>
 }
 
-ImageClassiciationTable.propTypes = {}
+// ImageClassiciationTable.propTypes = {}
 
 export default ImageClassiciationTable
