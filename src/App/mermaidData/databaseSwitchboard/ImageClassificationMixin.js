9345142906e9ff379ea565266c0c3b7bb7d9b307
Parmvir Thind <parmvirsthind@gmail.com>
2024-08-21 12:19:21 -0700

    add mixin for image classification. add function to fetch annotations for image.

---
+++ src/App/mermaidData/databaseSwitchboard/ImageClassificationMixin.js
@@ -0,0 +1,22 @@
+import axios from '../../../library/axiosRetry'
+import { getAuthorizationHeaders } from '../../../library/getAuthorizationHeaders'
+
+const ImageClassificationMixin = (Base) =>
+  class extends Base {
+    getAnnotationsForImage = async function getAnnotationsForImage(projectId, imageId) {
+      if (!imageId || !projectId) {
+        Promise.reject(this._operationMissingParameterError)
+      }
+
+      return this._isOnlineAuthenticatedAndReady
+        ? axios
+            .get(
+              `${this._apiBaseUrl}/projects/${projectId}/classification/images/${imageId}`,
+              await getAuthorizationHeaders(this._getAccessToken),
+            )
+            .then((apiResults) => apiResults.data)
+        : Promise.reject(this._notAuthenticatedAndReadyError)
+    }
+  }
+
+export default ImageClassificationMixin
