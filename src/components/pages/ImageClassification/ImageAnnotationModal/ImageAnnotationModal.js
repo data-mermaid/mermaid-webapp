b4f321b7b3eab35e17c127529ab3b7da09cc7fec
Parmvir Thind <parmvirsthind@gmail.com>
2024-08-21 12:20:21 -0700

    fetch annotations for image when modal opens. display loading indicator while fetching.

--- src/components/pages/ImageClassification/ImageAnnotationModal/ImageAnnotationModal.js
+++ src/components/pages/ImageClassification/ImageAnnotationModal/ImageAnnotationModal.js
@@ -1,42 +1,70 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
+import { useParams } from 'react-router-dom'
 import Modal from '../../../generic/Modal/Modal'
 import { IMAGE_CLASSIFICATION_COLORS as COLORS } from '../../../../library/constants/constants'
 import ImageAnnotationModalTable from './ImageAnnotationModalTable'
 import ImageAnnotationModalMap from './ImageAnnotationModalMap'
-import { Footer, Legend, LegendItem, LegendSquare } from './ImageAnnotationModal.styles'
+import {
+  Footer,
+  Legend,
+  LegendItem,
+  LegendSquare,
+  LoadingContainer,
+} from './ImageAnnotationModal.styles'
+import { useDatabaseSwitchboardInstance } from '../../../../App/mermaidData/databaseSwitchboard/DatabaseSwitchboardContext'
+import LoadingIndicator from '../../../LoadingIndicator/LoadingIndicator'
 
-const ImageAnnotationModal = ({ dataToReview, setDataToReview }) => {
+const ImageAnnotationModal = ({ imageId, setImageId }) => {
+  const { databaseSwitchboardInstance } = useDatabaseSwitchboardInstance()
+  const { projectId } = useParams()
+  const [dataToReview, setDataToReview] = useState()
   const [selectedPoints, setSelectedPoints] = useState([])
   const [highlightedPoints, setHighlightedPoints] = useState([])
 
+  useEffect(() => {
+    if (databaseSwitchboardInstance && projectId) {
+      databaseSwitchboardInstance.getAnnotationsForImage(projectId, imageId).then((data) => {
+        console.log('DATA', data)
+        setDataToReview(data)
+      })
+    }
+    // eslint-disable-next-line
+  }, [])
+
   const handleCloseModal = () => {
     // TODO: Save content before closing
-    setDataToReview()
+    setImageId()
   }
 
   return (
     <Modal
-      title={dataToReview.original_image_name}
+      // title={dataToReview.original_image_name}
       isOpen
       onDismiss={handleCloseModal}
       allowCloseWithEscapeKey={false}
       maxWidth="100%"
       mainContent={
-        <div>
-          <ImageAnnotationModalTable
-            points={dataToReview.points}
-            setDataToReview={setDataToReview}
-            setHighlightedPoints={setHighlightedPoints}
-            setSelectedPoints={setSelectedPoints}
-          />
-          <ImageAnnotationModalMap
-            dataToReview={dataToReview}
-            setDataToReview={setDataToReview}
-            highlightedPoints={highlightedPoints}
-            selectedPoints={selectedPoints}
-          />
-        </div>
+        dataToReview ? (
+          <div>
+            <ImageAnnotationModalTable
+              points={dataToReview.points}
+              setDataToReview={setDataToReview}
+              setHighlightedPoints={setHighlightedPoints}
+              setSelectedPoints={setSelectedPoints}
+            />
+            <ImageAnnotationModalMap
+              dataToReview={dataToReview}
+              setDataToReview={setDataToReview}
+              highlightedPoints={highlightedPoints}
+              selectedPoints={selectedPoints}
+            />
+          </div>
+        ) : (
+          <LoadingContainer>
+            <LoadingIndicator />
+          </LoadingContainer>
+        )
       }
       footerContent={
         <Footer>
@@ -66,18 +94,20 @@ const ImageAnnotationModal = ({ dataToReview, setDataToReview }) => {
 }
 
 ImageAnnotationModal.propTypes = {
-  setDataToReview: PropTypes.func.isRequired,
-  dataToReview: PropTypes.shape({
-    original_image_name: PropTypes.string.isRequired,
-    image: PropTypes.string.isRequired,
-    points: PropTypes.arrayOf(
-      PropTypes.shape({
-        row: PropTypes.number.isRequired,
-        column: PropTypes.number.isRequired,
-        annotations: PropTypes.arrayOf(PropTypes.object).isRequired,
-      }),
-    ),
-  }),
+  imageId: PropTypes.string.isRequired,
+  setImageId: PropTypes.func.isRequired,
+  // dataToReview: PropTypes.shape({
+  //   id: PropTypes.string.isRequired,
+  //   original_image_name: PropTypes.string.isRequired,
+  //   image: PropTypes.string.isRequired,
+  //   points: PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       row: PropTypes.number.isRequired,
+  //       column: PropTypes.number.isRequired,
+  //       annotations: PropTypes.arrayOf(PropTypes.object).isRequired,
+  //     }),
+  //   ),
+  // }),
 }
 
 export default ImageAnnotationModal
