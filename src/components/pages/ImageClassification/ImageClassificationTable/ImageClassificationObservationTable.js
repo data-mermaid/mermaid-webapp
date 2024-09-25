d704344757e57c3c765a90457b00b8457e1f10d9
julia-rom <jromanowski@sparkgeo.com>
2024-09-25 17:03:50 -0400

    quadrat value should be imageIndex + 1

--- src/components/pages/ImageClassification/ImageClassificationTable/ImageClassificationObservationTable.js
+++ src/components/pages/ImageClassification/ImageClassificationTable/ImageClassificationObservationTable.js
@@ -159,7 +159,7 @@ const ImageClassificationObservationTable = ({ uploadedFiles, setUploadedFiles }
   }
 
   const distillAnnotationData = useCallback(
-    (items, index) => {
+    (items) => {
       if (!benthicAttributes || !growthForms) {
         return null
       }
@@ -187,7 +187,6 @@ const ImageClassificationObservationTable = ({ uploadedFiles, setUploadedFiles }
         confirmedCount,
         benthicAttributeLabel: benthic_attribute_label,
         growthFormLabel: growth_form_label,
-        quadrat: index + 1,
       }
     },
     [getBenthicAttributeLabel, getGrowthFormLabel, benthicAttributes, growthForms],
@@ -315,7 +314,7 @@ const ImageClassificationObservationTable = ({ uploadedFiles, setUploadedFiles }
             </thead>
 
             <tbody>
-              {distilledImages.map((image) => {
+              {distilledImages.map((image, imageIndex) => {
                 const { file, distilledAnnotationData, numSubRows } = image
 
                 if (numSubRows === 0) {
@@ -356,7 +355,7 @@ const ImageClassificationObservationTable = ({ uploadedFiles, setUploadedFiles }
                         </TdWithHoverText>
                       </>
                     )}
-                    <StyledTd textAlign="right">{annotation?.quadrat}</StyledTd>
+                    <StyledTd textAlign="right">{imageIndex + 1}</StyledTd>
                     <StyledTd>{annotation?.benthicAttributeLabel}</StyledTd>
                     <StyledTd>{annotation?.growthFormLabel || ''}</StyledTd>
                     <StyledTd textAlign="right">{annotation?.confirmedCount}</StyledTd>
