d03f09120b8b75c250ddc6624379739c6df5cf63
julia-rom <jromanowski@sparkgeo.com>
2024-12-20 17:04:41 -0500

    remove decimal enforcement on input

--- src/components/generic/InputNumberNoScrollWithUnit/InputNumberNoScrollWithUnit.js
+++ src/components/generic/InputNumberNoScrollWithUnit/InputNumberNoScrollWithUnit.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components/macro'
 
 import { inputTextareaSelectStyles } from '../form'
@@ -28,42 +28,11 @@ const InnerInput = styled(InputNumberNoScroll)`
   padding: ${theme.spacing.xsmall};
 `
 
-const InputNumberNoScrollWithUnit = ({
-  unit = '',
-  alignUnitsLeft = false,
-  value = '',
-  onChange = () => {},
-  name = '',
-  ...restOfProps
-}) => {
-  const [inputValue, setInputValue] = useState(value || '')
-
-  const handleInputChange = (e) => {
-    let rawValue = e.target.value
-
-    if (unit === 'USD $') {
-      rawValue = rawValue.replace(/[^0-9.]/g, '')
-
-      // Restrict to a valid decimal format: up to two decimal places
-      if (rawValue.includes('.')) {
-        const [integerPart, decimalPart] = rawValue.split('.')
-        if (decimalPart.length > 2) {
-          rawValue = `${integerPart}.${decimalPart.slice(0, 2)}`
-        }
-      }
-    }
-
-    setInputValue(rawValue)
-
-    if (onChange) {
-      onChange({ ...e, target: { ...e.target, value: rawValue, name } })
-    }
-  }
-
+const InputNumberNoScrollWithUnit = ({ unit = '', alignUnitsLeft = false, ...restOfProps }) => {
   return (
     <InputContainer>
       {alignUnitsLeft && <UnitContainer>{unit}</UnitContainer>}
-      <InnerInput {...restOfProps} value={inputValue} onChange={handleInputChange} />
+      <InnerInput {...restOfProps} />
       {!alignUnitsLeft && <UnitContainer>{unit}</UnitContainer>}
     </InputContainer>
   )
@@ -72,9 +41,6 @@ const InputNumberNoScrollWithUnit = ({
 InputNumberNoScrollWithUnit.propTypes = {
   unit: PropTypes.string.isRequired,
   alignUnitsLeft: PropTypes.bool,
-  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  onChange: PropTypes.func,
-  name: PropTypes.string,
 }
 
 export default InputNumberNoScrollWithUnit
