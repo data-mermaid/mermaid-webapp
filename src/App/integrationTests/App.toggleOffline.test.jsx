8f849142d521ab5dfcc575e32fd1947e9c5f7f7f
m n <mnunes@sparkgeo.com>
2025-02-26 11:29:36 -0700

    M621 replace CRA with Vite for non-testing part of app

---
+++ src/App/integrationTests/App.toggleOffline.test.jsx
@@ -0,0 +1,126 @@
+import '@testing-library/jest-dom'
+import { rest } from 'msw'
+import React from 'react'
+
+import { initiallyHydrateOfflineStorageWithMockData } from '../../testUtilities/initiallyHydrateOfflineStorageWithMockData'
+import { getMockDexieInstancesAllSuccess } from '../../testUtilities/mockDexie'
+import {
+  mockMermaidApiAllSuccessful,
+  renderAuthenticated,
+  waitForElementToBeRemoved,
+  screen,
+  within,
+} from '../../testUtilities/testingLibraryWithHelpers'
+import App from '../App'
+
+const apiBaseUrl = import.meta.env.VITE_MERMAID_API
+
+test('Starting ONLINE - Toggle is checked and switched to OFFLINE, some navigation links will disappear. Then navigation links reappear after clicking toggle again', async () => {
+  const { dexiePerUserDataInstance, dexieCurrentUserInstance } = getMockDexieInstancesAllSuccess()
+
+  await initiallyHydrateOfflineStorageWithMockData(dexiePerUserDataInstance)
+
+  const { user } = renderAuthenticated(
+    <App dexieCurrentUserInstance={dexieCurrentUserInstance} />,
+    {
+      initialEntries: ['/projects/5/collecting/'],
+    },
+    { dexieCurrentUserInstance, dexiePerUserDataInstance },
+  )
+
+  const sideNav = await screen.findByTestId('content-page-side-nav')
+
+  const offlineToggleSwitchTestIdBeforeFirstClick = screen.getByTestId('offline-toggle-switch-test')
+
+  await user.click(offlineToggleSwitchTestIdBeforeFirstClick)
+
+  expect(await screen.findByTestId('offline-toggle-switch-label')).toHaveTextContent(
+    "You're OFFLINE. Some contents may be out of date.",
+  )
+
+  expect(within(sideNav).queryByRole('link', { name: 'Submitted' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Project Info' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Users' })).not.toBeInTheDocument()
+  expect(within(sideNav).queryByRole('link', { name: 'Data Sharing' })).not.toBeInTheDocument()
+
+  const offlineToggleSwitchTestIdAfterFirstClick = screen.getByTestId('offline-toggle-switch-test')
+
+  await user.click(offlineToggleSwitchTestIdAfterFirstClick)
+
+  await waitForElementToBeRemoved(() => screen.queryByLabelText('project pages loading indicator'))
+
+  expect(await screen.findByTestId('offline-toggle-switch-label')).toHaveTextContent(
+    "You're ONLINE",
+  )
+
+  expect(within(sideNav).getByRole('link', { name: 'Submitted' })).toBeInTheDocument()
+  expect(within(sideNav).getByRole('link', { name: 'Project Info' })).toBeInTheDocument()
+  expect(within(sideNav).getByRole('link', { name: 'Users' })).toBeInTheDocument()
+  expect(within(sideNav).getByRole('link', { name: 'Data Sharing' })).toBeInTheDocument()
+})
+
+test('Navigator online - Toggle switch is not checked, and is enabled', async () => {
+  jest.spyOn(navigator, 'onLine', 'get').mockReturnValue(true)
+
+  const { dexiePerUserDataInstance, dexieCurrentUserInstance } = getMockDexieInstancesAllSuccess()
+
+  renderAuthenticated(<App dexieCurrentUserInstance={dexieCurrentUserInstance} />, {
+    dexiePerUserDataInstance,
+    dexieCurrentUserInstance,
+  })
+
+  const offlineToggleSwitchTestId = await screen.findByTestId('offline-toggle-switch-test')
+
+  expect(offlineToggleSwitchTestId).not.toBeChecked()
+  expect(offlineToggleSwitchTestId).toBeEnabled()
+})
+
+test('Navigator offline - Toggle switch is checked and disabled', async () => {
+  jest.spyOn(navigator, 'onLine', 'get').mockReturnValue(false)
+
+  const { dexiePerUserDataInstance, dexieCurrentUserInstance } = getMockDexieInstancesAllSuccess()
+
+  renderAuthenticated(<App dexieCurrentUserInstance={dexieCurrentUserInstance} />, {
+    dexiePerUserDataInstance,
+    dexieCurrentUserInstance,
+  })
+
+  const offlineToggleSwitchTestId = await screen.findByTestId('offline-toggle-switch-test')
+
+  expect(offlineToggleSwitchTestId).toBeChecked()
+  expect(offlineToggleSwitchTestId).toBeDisabled()
+})
+
+test('Server is reachable - Toggle switch is not checked, and is enabled', async () => {
+  const { dexiePerUserDataInstance, dexieCurrentUserInstance } = getMockDexieInstancesAllSuccess()
+
+  renderAuthenticated(<App dexieCurrentUserInstance={dexieCurrentUserInstance} />, {
+    dexiePerUserDataInstance,
+    dexieCurrentUserInstance,
+  })
+
+  const offlineToggleSwitchTestId = await screen.findByTestId('offline-toggle-switch-test')
+
+  expect(offlineToggleSwitchTestId).not.toBeChecked()
+  expect(offlineToggleSwitchTestId).toBeEnabled()
+})
+
+test('Server is unreachable - Toggle switch is not checked, and is enabled', async () => {
+  mockMermaidApiAllSuccessful.use(
+    rest.get(`${apiBaseUrl}/health`, (req, res) => {
+      return res.networkError('Custom network error message')
+    }),
+  )
+
+  const { dexiePerUserDataInstance, dexieCurrentUserInstance } = getMockDexieInstancesAllSuccess()
+
+  renderAuthenticated(<App dexieCurrentUserInstance={dexieCurrentUserInstance} />, {
+    dexiePerUserDataInstance,
+    dexieCurrentUserInstance,
+  })
+
+  const offlineToggleSwitchTestId = await screen.findByTestId('offline-toggle-switch-test')
+
+  expect(offlineToggleSwitchTestId).toBeChecked()
+  expect(offlineToggleSwitchTestId).toBeDisabled()
+})
---
+++ src/components/pages/collectRecordFormPages/SaveValidateSubmitButtonGroup/SaveValidateSubmitButtonGroup.jsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import { ButtonCallout } from '../../../generic/buttons'
+import { IconSave, IconCheck, IconUpload } from '../../../icons'
+import { buttonGroupStates } from '../../../../library/buttonGroupStates'
+import OfflineHide from '../../../generic/OfflineHide'
+import theme from '../../../../theme'
+
+const SaveValidateSubmitButtonWrapper = styled('div')`
+  padding-right: ${theme.spacing.xlarge};
+  button {
+    white-space: nowrap;
+  }
+`
+const pointerSize = '1rem'
+const borderSize = '1px'
+const clipPath = `polygon(
+  calc(100% - ${pointerSize}) 0%,
+  100% 50%,
+  calc(100% - ${pointerSize}) 100%,
+  0% 100%,
+  ${pointerSize} 50%,
+  0% 0%)`
+
+const ArrowRightButton = styled(ButtonCallout)`
+  clip-path: ${clipPath};
+  z-index: 1;
+  margin-right: calc(-${pointerSize} + 2px);
+  color: ${theme.color.calloutText};
+  border: none;
+  padding: ${theme.spacing.small} ${theme.spacing.xlarge};
+  position: relative;
+  background: ${theme.color.calloutBorder};
+  &:before {
+    clip-path: ${clipPath};
+    z-index: -1;
+    background: ${theme.color.white};
+    pointer-events: none;
+    content: '';
+    position: absolute;
+    top: ${borderSize};
+    left: ${borderSize};
+    right: ${borderSize};
+    bottom: ${borderSize};
+  }
+  &:not(:disabled):hover {
+    cursor: pointer;
+    background: ${theme.color.calloutBorder};
+    &:before {
+      background: ${theme.color.calloutHover};
+    }
+  }
+  &:disabled {
+    background-color: ${theme.color.disabledColor};
+    color: ${theme.color.disabledTextDark};
+  }
+`
+const SaveValidateSubmitButtonGroup = ({
+  isNewRecord,
+  onSave,
+  onSubmit,
+  onValidate,
+  saveButtonState,
+  submitButtonState,
+  validateButtonState,
+}) => {
+  const getSaveButtonText = () => {
+    if (saveButtonState === buttonGroupStates.saving) {
+      return 'Saving'
+    }
+    if (
+      saveButtonState === buttonGroupStates.saved ||
+      saveButtonState === buttonGroupStates.validating
+    ) {
+      return 'Saved'
+    }
+
+    return 'Save'
+  }
+
+  const getValidateButtonText = () => {
+    if (validateButtonState === buttonGroupStates.validating) {
+      return 'Validating'
+    }
+    if (validateButtonState === buttonGroupStates.validated) {
+      return 'Validated'
+    }
+
+    return 'Validate'
+  }
+
+  const getSubmitButtonText = () =>
+    submitButtonState === buttonGroupStates.submitting ? 'Submitting' : 'Submit'
+
+  const isSaveDisabled =
+    saveButtonState === buttonGroupStates.untouchedEmptyForm ||
+    saveButtonState === buttonGroupStates.saved ||
+    saveButtonState === buttonGroupStates.saving ||
+    validateButtonState === buttonGroupStates.validating
+
+  const isValidateDisabled =
+    saveButtonState === buttonGroupStates.unsaved ||
+    saveButtonState === buttonGroupStates.saving ||
+    validateButtonState === buttonGroupStates.validated ||
+    validateButtonState === buttonGroupStates.validating
+
+  const isSubmitDisabled =
+    submitButtonState === buttonGroupStates.submitting ||
+    validateButtonState === buttonGroupStates.validatable ||
+    validateButtonState === buttonGroupStates.validating ||
+    saveButtonState === buttonGroupStates.unsaved ||
+    saveButtonState === buttonGroupStates.saving
+
+  const saveButton = (
+    <ArrowRightButton type="button" disabled={isSaveDisabled} onClick={onSave}>
+      <IconSave />
+      {getSaveButtonText()}
+    </ArrowRightButton>
+  )
+
+  const validateButton = (
+    <ArrowRightButton onClick={onValidate} disabled={isValidateDisabled}>
+      <IconCheck />
+      {getValidateButtonText()}
+    </ArrowRightButton>
+  )
+
+  const submitButton = (
+    <ArrowRightButton disabled={isSubmitDisabled} onClick={onSubmit}>
+      <IconUpload />
+      {getSubmitButtonText()}
+    </ArrowRightButton>
+  )
+
+  return (
+    <SaveValidateSubmitButtonWrapper data-testid="collect-record-form-buttons">
+      {saveButton}
+      {!isNewRecord && (
+        <OfflineHide>
+          {validateButton}
+          {submitButton}
+        </OfflineHide>
+      )}
+    </SaveValidateSubmitButtonWrapper>
+  )
+}
+
+SaveValidateSubmitButtonGroup.propTypes = {
+  isNewRecord: PropTypes.bool.isRequired,
+  onSave: PropTypes.func.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+  onValidate: PropTypes.func.isRequired,
+  saveButtonState: PropTypes.string.isRequired,
+  submitButtonState: PropTypes.string.isRequired,
+  validateButtonState: PropTypes.string.isRequired,
+}
+
+export default SaveValidateSubmitButtonGroup
