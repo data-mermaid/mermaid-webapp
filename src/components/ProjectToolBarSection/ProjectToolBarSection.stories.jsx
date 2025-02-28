a711ed6bff390b9ab1a00b1d096eecfa54e6a60c
m n <mnunes@sparkgeo.com>
2025-02-27 17:58:06 -0700

    M621 remove stories that no one uses anymore

--- src/components/ProjectToolBarSection/ProjectToolBarSection.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import ProjectToolBarSection from '.'
-
-export default {
-  title: 'ProjectToolBarSection',
-  component: ProjectToolBarSection,
-}
-export const basic = () => <ProjectToolBarSection />
--- src/components/pages/ManagementRegimes/ManagementRegimes.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import ManagementRegimes from '.'
-
-export default {
-  title: 'ManagementRegimes',
-  component: ManagementRegimes,
-}
-export const basic = () => <ManagementRegimes />
