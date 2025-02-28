1cb4c293aa6af2c89203064c08b1afc10eacf249
m n <mnunes@sparkgeo.com>
2025-02-27 17:58:06 -0700

    M621 remove stories that no one uses anymore

--- src/components/generic/InputNumberNoScrollWithUnit/InputNumberNoScrollWithUnit.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import InputNumberNoScrollWithUnit from './InputNumberNoScrollWithUnit'
-
-export default {
-  title: 'InputNumberNoScrollWithUnit',
-  component: InputNumberNoScrollWithUnit,
-}
-
-export const basic = () => <InputNumberNoScrollWithUnit unit="cm" />
--- src/components/pages/ProjectInfo/ProjectInfo.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import ProjectInfo from '.'
-
-export default {
-  title: 'Project Info',
-  component: ProjectInfo,
-}
-export const basic = () => <ProjectInfo />
