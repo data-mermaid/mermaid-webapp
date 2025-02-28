57fb799c0f40e3e70fa10d40425f1921e9994a9f
m n <mnunes@sparkgeo.com>
2025-02-27 17:58:06 -0700

    M621 remove stories that no one uses anymore

--- src/components/pages/Collect/Collect.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Collect from '.'
-
-export default {
-  title: 'Collect',
-  component: Collect,
-}
-export const basic = () => <Collect />
--- src/components/pages/Sites/Sites.stories.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import Sites from '.'
-
-export default {
-  title: 'Sites',
-  component: Sites,
-}
-export const basic = () => <Sites />
