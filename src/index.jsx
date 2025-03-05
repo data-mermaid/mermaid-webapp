021fa60524cb0c56fee83a88c4348712e9cdd927
m n <mnunes@sparkgeo.com>
2025-03-04 17:17:50 -0700

    remove service worker

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import { createRoot } from 'react-dom/client'
 
 import './index.css'
 import 'maplibre-gl/dist/maplibre-gl.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import reportWebVitals from './reportWebVitals'
 
 import { App } from './App'
@@ -53,11 +52,6 @@ root.render(
     </Auth0Provider>
   </React.StrictMode>,
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
