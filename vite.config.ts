21e5c6befd7416299d56e67fdc310eeb84698a38
m n <mnunes@sparkgeo.com>
2025-03-07 16:40:16 -0700

    M621 use vite-plugin-pwa for service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        short_name: 'MERMAID',
+        name: 'MERMAID',
+        icons: [
+          {
+            src: 'mermaid_favicon.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+        ],
+        start_url: '.',
+        display: 'standalone',
+        theme_color: '#000000',
+        background_color: '#ffffff',
+      },
+      workbox: {
+        globPatterns: ['**/*'], //  cache all the imports
+        maximumFileSizeToCacheInBytes: 3000000,
+      },
+
+      includeAssets: ['**/*'], //cache all the static assets in the public folder
+    }),
+  ],
   server: {
     port: 3000,
   },
@@ -14,5 +41,8 @@ export default defineConfig({
   build: {
     outDir: 'build',
     emptyOutDir: true,
+  },
+  define: {
+    'process.env': process.env,
   },
 })
