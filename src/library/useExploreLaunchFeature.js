61521176989df2fccfbdf2257cf0267b285a76da
tienhoah <nickhoang11@gmail.com>
2025-04-15 15:47:14 -0700

    Fix default value for explore launch feature and update fallback dashboard link

--- src/library/useExploreLaunchFeature.js
+++ src/library/useExploreLaunchFeature.js
@@ -1,14 +1,14 @@
 import { getCurrentUserOptionalFeature } from './getCurrentUserOptionalFeature'
 
 export const useExploreLaunchFeature = ({ currentUser }) => {
-  const { enabled: isExploreLaunchEnabledForUser = true } = getCurrentUserOptionalFeature(
+  const { enabled: isExploreLaunchEnabledForUser = false } = getCurrentUserOptionalFeature(
     currentUser,
     'explore_launch',
   )
 
   const mermaidExploreLink = isExploreLaunchEnabledForUser
     ? import.meta.env.VITE_MERMAID_DASHBOARD_LINK
-    : `https://dev.dashboard3.datamermaid.org`
+    : `https://dev-dashboard.datamermaid.org`
 
   return {
     mermaidExploreLink,
