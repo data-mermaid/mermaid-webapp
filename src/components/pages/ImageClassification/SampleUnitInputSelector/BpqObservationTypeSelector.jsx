952507371e3d59c550f79af7a9a63d3562e6af92
Stacy <hurrendor@gmail.com>
2025-06-04 11:49:17 -0700

    M1536 Add GTM and tag ids

--- src/components/pages/ImageClassification/SampleUnitInputSelector/BpqObservationTypeSelector.jsx
+++ src/components/pages/ImageClassification/SampleUnitInputSelector/BpqObservationTypeSelector.jsx
@@ -1,42 +1,35 @@
-import React from "react";
-import { ButtonPrimary, ButtonSecondary } from "../../../generic/buttons";
-import { H3 } from "../../../generic/text";
-import { IconPen, IconSparkles } from "../../../icons";
-import PropTypes from "prop-types";
+import React from 'react'
+import { ButtonPrimary, ButtonSecondary } from '../../../generic/buttons'
+import { H3 } from '../../../generic/text'
+import { IconPen, IconSparkles } from '../../../icons'
+import PropTypes from 'prop-types'
 import {
   ButtonContainer,
   ButtonText,
   OfflineText,
   SelectorContainer,
   TextContainer,
-} from "./BpqObservationTypeSelector.styles";
-import { Trans, useTranslation } from "react-i18next";
+} from './BpqObservationTypeSelector.styles'
+import { Trans, useTranslation } from 'react-i18next'
 
-const BpqObservationTypeSelector = ({
-  setIsImageClassificationSelected,
-  isAppOnline,
-}) => {
-  const { t } = useTranslation();
+const BpqObservationTypeSelector = ({ setIsImageClassificationSelected, isAppOnline }) => {
+  const { t } = useTranslation()
   const handleSampleUnitChange = (type) => () => {
-    setIsImageClassificationSelected(type);
-  };
+    setIsImageClassificationSelected(type)
+  }
 
   return (
     <SelectorContainer>
       <TextContainer>
         <H3 htmlFor="image-classification-selection">
-          {t("image_classification.feature_introduction.title")}
+          {t('image_classification.feature_introduction.title')}
         </H3>
         <p>
           <Trans
             i18nKey="image_classification.feature_introduction.description"
             components={{
               a: (
-                <a
-                  href="https://coralnet.ucsd.edu"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <a href="https://coralnet.ucsd.edu" target="_blank" rel="noopener noreferrer">
                   CoralNet
                 </a>
               ),
@@ -46,36 +39,31 @@ const BpqObservationTypeSelector = ({
       </TextContainer>
       <ButtonContainer>
         <div>
-          {!isAppOnline && (
-            <OfflineText>{t("offline.unavailable_offline")}</OfflineText>
-          )}
+          {!isAppOnline && <OfflineText>{t('offline.unavailable_offline')}</OfflineText>}
           <ButtonPrimary
             type="button"
+            id="gtm-image-classification-selection"
             onClick={handleSampleUnitChange(true)}
             disabled={!isAppOnline}
           >
             <IconSparkles />
-            <ButtonText>
-              {t("image_classification.feature_introduction.use_ai_button")}
-            </ButtonText>
+            <ButtonText>{t('image_classification.feature_introduction.use_ai_button')}</ButtonText>
           </ButtonPrimary>
         </div>
         <ButtonSecondary type="button" onClick={handleSampleUnitChange(false)}>
           <IconPen />
           <ButtonText>
-            {t(
-              "image_classification.feature_introduction.use_manual_observations_button"
-            )}
+            {t('image_classification.feature_introduction.use_manual_observations_button')}
           </ButtonText>
         </ButtonSecondary>
       </ButtonContainer>
     </SelectorContainer>
-  );
-};
+  )
+}
 
 BpqObservationTypeSelector.propTypes = {
   setIsImageClassificationSelected: PropTypes.func.isRequired,
   isAppOnline: PropTypes.bool.isRequired,
-};
+}
 
-export default BpqObservationTypeSelector;
+export default BpqObservationTypeSelector
