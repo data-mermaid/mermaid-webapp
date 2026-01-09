b217feed803ebfed00d98fcf832ddad3dff04543
Stacy <hurrendor@gmail.com>
2026-01-09 12:17:53 -0800

    M1755 One more aria update

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -107,6 +107,9 @@ const ProjectCard = ({ project, isOfflineReady, addProjectToProjectsPage, ...res
   }
 
   const handleCardKeyDown = (e) => {
+    if (e.target !== e.currentTarget) {
+      return
+    }
     if (e.key === 'Enter' || e.key === ' ') {
       e.preventDefault()
       handleCardClick()
