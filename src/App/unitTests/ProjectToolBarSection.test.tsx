615e5f4221b2eeb053f8069b140a4adaeaa70c38
Stacy <hurrendor@gmail.com>
2026-01-14 13:04:24 -0800

    M1764 Update unit tests for feature code

--- src/App/unitTests/ProjectToolBarSection.test.tsx
+++ src/App/unitTests/ProjectToolBarSection.test.tsx
@@ -6,46 +6,48 @@ import { getMockDexieInstancesAllSuccess } from '../../testUtilities/mockDexie'
 import { initiallyHydrateOfflineStorageWithMockData } from '../../testUtilities/initiallyHydrateOfflineStorageWithMockData'
 import { renderAuthenticatedOnline } from '../../testUtilities/testingLibraryWithHelpers'
 
+// enable when project_demo feature is enabled
+
 describe('ProjectToolBarSection', () => {
-  describe('A demo project does not exist', () => {
-    it('users can add a demo project from the dropdown', async () => {
-      const { dexiePerUserDataInstance } = getMockDexieInstancesAllSuccess()
-      await initiallyHydrateOfflineStorageWithMockData(dexiePerUserDataInstance)
-
-      renderAuthenticatedOnline(
-        <ProjectToolBarSection
-          setProjectFilter={jest.fn()}
-          projectSortKey=""
-          setProjectSortKey={jest.fn()}
-          setIsProjectSortAsc={jest.fn()}
-          addProjectToProjectsPage={jest.fn()}
-          handleExploreButtonClick={jest.fn()}
-          userHasDemoProject={false}
-        />,
-        {
-          dexiePerUserDataInstance,
-          isSyncInProgressOverride: true,
-        },
-      )
-
-      const dropdownButton = screen.getByTestId('dropdown-icon-button')
-      expect(dropdownButton).toBeInTheDocument()
-      await userEvent.click(dropdownButton)
-
-      const addDemoButton = screen.getByTestId('add-demo-project-button')
-      expect(addDemoButton).toBeInTheDocument()
-
-      // todo: mock out handling XMLHTTPRequest
-      // userEvent.click(addDemoButton)
-      // expect(await screen.findByTestId('loading-indicator')).toBeInTheDocument()
-
-      // expect - URL will navigate to 'Sites'
-      // todo: mock out properly
-      // ${apiBaseUrl}/projects/create_demo/
-      // expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
-      // expect(mockedUseNavigate).toHaveBeenCalledWith(expect.stringMatching(/\/sites$/))
-    })
-  })
+  // describe('A demo project does not exist', () => {
+  //   it('users can add a demo project from the dropdown', async () => {
+  //     const { dexiePerUserDataInstance } = getMockDexieInstancesAllSuccess()
+  //     await initiallyHydrateOfflineStorageWithMockData(dexiePerUserDataInstance)
+  //
+  //     renderAuthenticatedOnline(
+  //       <ProjectToolBarSection
+  //         setProjectFilter={jest.fn()}
+  //         projectSortKey=""
+  //         setProjectSortKey={jest.fn()}
+  //         setIsProjectSortAsc={jest.fn()}
+  //         addProjectToProjectsPage={jest.fn()}
+  //         handleExploreButtonClick={jest.fn()}
+  //         userHasDemoProject={false}
+  //       />,
+  //       {
+  //         dexiePerUserDataInstance,
+  //         isSyncInProgressOverride: true,
+  //       },
+  //     )
+  //
+  //     const dropdownButton = screen.getByTestId('dropdown-icon-button')
+  //     expect(dropdownButton).toBeInTheDocument()
+  //     await userEvent.click(dropdownButton)
+  //
+  //     const addDemoButton = screen.getByTestId('add-demo-project-button')
+  //     expect(addDemoButton).toBeInTheDocument()
+  //
+  //     // todo: mock out handling XMLHTTPRequest
+  //     // userEvent.click(addDemoButton)
+  //     // expect(await screen.findByTestId('loading-indicator')).toBeInTheDocument()
+  //
+  //     // expect - URL will navigate to 'Sites'
+  //     // todo: mock out properly
+  //     // ${apiBaseUrl}/projects/create_demo/
+  //     // expect(mockedUseNavigate).toHaveBeenCalledTimes(1)
+  //     // expect(mockedUseNavigate).toHaveBeenCalledWith(expect.stringMatching(/\/sites$/))
+  //   })
+  // })
   describe('A demo project exists', () => {
     it('the demo project dropdown will not render', async () => {
       const { dexiePerUserDataInstance } = getMockDexieInstancesAllSuccess()
