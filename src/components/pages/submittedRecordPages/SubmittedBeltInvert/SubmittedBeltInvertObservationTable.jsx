7f8f30b575820c127d563051a3ab95ea2e16dbf0
Gio W <gio_wan@live.ca>
2026-06-25 11:37:58 -0600

    fix [1997] also apply conditional size rendering to submitted

--- src/components/pages/submittedRecordPages/SubmittedBeltInvert/SubmittedBeltInvertObservationTable.jsx
+++ src/components/pages/submittedRecordPages/SubmittedBeltInvert/SubmittedBeltInvertObservationTable.jsx
@@ -54,11 +54,13 @@ const SubmittedBeltInvertObservationTable = ({
     return roundToOneDecimal(observationDensities.get(item.id))
   }
 
+  const hasSizeData = obs_belt_inverts.some((item) => item.size)
+
   const observationBeltInverts = obs_belt_inverts.map((item, index) => (
     <Tr key={item.id}>
       <Td $align="center">{index + 1}</Td>
       <Td $align="left">{getInvertName(item.invert_attribute)}</Td>
-      <Td $align="left">{item.size}</Td>
+      {hasSizeData && <Td $align="left">{item.size}</Td>}
       <Td $align="right">{item.count}</Td>
       <Td $align="right">{item.notes}</Td>
       <Td $align="right">{getInvertDensity(item)}</Td>
@@ -74,7 +76,7 @@ const SubmittedBeltInvertObservationTable = ({
             <Tr>
               <TheadItem> </TheadItem>
               <TheadItem $align="left">{t('observations.macroinvertebrate_name')}</TheadItem>
-              <TheadItem $align="left">{t('size_cm')}</TheadItem>
+              {hasSizeData && <TheadItem $align="left">{t('size_cm')}</TheadItem>}
               <TheadItem $align="right">{t('count')}</TheadItem>
               <TheadItem $align="right">{t('notes')}</TheadItem>
               <TheadItem $align="right">{`${t('density')} (${t(
